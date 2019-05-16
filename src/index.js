0683e8e5837fd4c19f847dce89a1c3dac459e4b0
o0 <igor.alexeenko@htmlacademy.ru>
2019-05-17 00:10:24 +0300

    Подключает store через connect к приложению

--- src/index.js
+++ src/index.js
@@ -1,21 +1,30 @@
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
+
 import App from "./components/app/app.jsx";
 import questions from "./mocks/questions";
+import {reducer} from "./reducer";
+
 
 const gameSettings = {
   gameTime: 5,
   errorCount: 3,
 };
 
+
 const init = (gameQuestions) => {
   const {errorCount, gameTime} = gameSettings;
+  const store = createStore(reducer);
 
-  ReactDOM.render(<App
-    errorCount={errorCount}
-    gameTime={gameTime}
-    questions={gameQuestions}
-  />,
+  ReactDOM.render(<Provider store={store}>
+    <App
+      errorCount={errorCount}
+      gameTime={gameTime}
+      questions={gameQuestions}
+    />
+  </Provider>,
   document.querySelector(`.main`));
 };
 
