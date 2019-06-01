3139afe4f8c4dd5ef2290781c43ab4c25fef98fc
kicumkicum <kicumkicum@gmail.com>
2019-06-02 00:57:27 +0300

    :nine: :one: Пытается оживить тесты. Пока безуспешно. WIP

--- src/components/app/app.test.js
+++ src/components/app/app.test.js
@@ -1,7 +1,7 @@
-import React from 'react';
+import * as React from 'react';
 import renderer from 'react-test-renderer';
 
-import {App} from './app.jsx';
+import {App} from './app.tsx';
 
 const mock = {
   questions: [
