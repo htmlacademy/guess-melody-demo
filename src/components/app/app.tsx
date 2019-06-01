08499c177ba29accbdb8e49b3a8c8ce1b4fbd325
kicumkicum <kicumkicum@gmail.com>
2019-06-02 00:42:33 +0300

    :nine: :one: Исправляет ts-особенный синтаксис импорта

--- src/components/app/app.tsx
+++ src/components/app/app.tsx
@@ -1,6 +1,6 @@
-import React, {Component} from "react";
+import * as React from "react";
 import {connect} from "react-redux";
-import PropTypes from "prop-types";
+import * as PropTypes from "prop-types";
 import {getStep} from "../../reducer/game/selectors";
 import {getQuestions} from "../../reducer/data/selectors";
 
@@ -10,7 +10,7 @@ const Type = {
 };
 
 
-class App extends Component {
+class App extends React.Component {
   render() {
     const {
       questions,
