5d3bcaaadb82d450e929c2677c435afe78f41306
kicumkicum <kicumkicum@gmail.com>
2019-06-02 03:13:24 +0300

    :nine: :four: Переименовывает все компоненты и хоки `*.js[x]` в `*.ts[x]`

--- src/components/audio-player/audio-player.tsx
+++ src/components/audio-player/audio-player.tsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 
+
 export default class AudioPlayer extends PureComponent {
   render() {
     const {isLoading, isPlaying, renderAudio, onPlayButtonClick} = this.props;
