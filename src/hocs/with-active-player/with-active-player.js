2e961e6b87e8be58264f3ae86dde59ddbcdbb914
Sasha Sushko <sushko@outlook.com>
2019-11-04 17:13:53 +0300

    6.1 Добавит HOC withActivePlayer

---
+++ src/hocs/with-active-player/with-active-player.js
@@ -0,0 +1,37 @@
+import React, {PureComponent} from 'react';
+import AudioPlayer from "../../components/audio-player/audio-player.jsx";
+
+const withActivePlayer = (Component) => {
+  class WithActivePlayer extends PureComponent {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        activePlayer: -1,
+      };
+    }
+
+    render() {
+      const {activePlayer} = this.state;
+
+      return <Component
+        {...this.props}
+        renderPlayer={(it, i) => {
+          return <AudioPlayer
+            src={it.src}
+            isPlaying={i === activePlayer}
+            onPlayButtonClick={() => this.setState((prevState) => ({
+              activePlayer: prevState.activePlayer === i ? -1 : i
+            }))}
+          />;
+        }}
+      />;
+    }
+  }
+
+  WithActivePlayer.propTypes = {};
+
+  return WithActivePlayer;
+};
+
+export default withActivePlayer;
