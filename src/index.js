5b660e5335b618702fe9e7a5849187ca92d85ff2
Sasha Sushko <sushko@outlook.com>
2019-11-19 12:42:36 +0300

    7.1 Установим нужные пакеты и подключим их в проект

--- src/index.js
+++ src/index.js
@@ -1,11 +1,14 @@
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
 import React from "react";
 import ReactDOM from "react-dom";
+import thunk from "redux-thunk";
+import {compose} from "recompose";
 
 import App from "./components/app/app.jsx";
-import questions from "./mocks/questions";
-import {reducer} from "./reducer";
+import {createAPI} from './api';
+import reducer from "./reducer";
+import {Operation} from "./reducer/data/data";
 import withScreenSwitch from "./hocs/with-screen-switch/with-screen-switch";
 
 const gameSettings = {
@@ -15,24 +18,29 @@ const gameSettings = {
 const AppWrapped = withScreenSwitch(App);
 
 
-const init = (gameQuestions) => {
+const init = () => {
   const {errorCount, gameTime} = gameSettings;
+  const api = createAPI((...args) => store.dispatch(...args));
+
   /* eslint-disable no-underscore-dangle */
   const store = createStore(
       reducer,
-      window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+      compose(
+          applyMiddleware(thunk.withExtraArgument(api)),
+          window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+      )
   );
   /* eslint-enable */
+
+  store.dispatch(Operation.loadQuestions());
 
   ReactDOM.render(<Provider store={store}>
     <AppWrapped
       maxMistakes={errorCount}
       gameTime={gameTime}
-      questions={gameQuestions}
-      questionsLength={gameQuestions.length}
     />
   </Provider>,
   document.querySelector(`#root`));
 };
 
-init(questions);
+init();
