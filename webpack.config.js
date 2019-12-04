5d49261081bf85c4c3b4b67abcda5423de31fa14
Sasha Sushko <sushko@outlook.com>
2019-12-04 23:23:02 +0300

    9.8 Исправит точку входа на TS-файл

--- webpack.config.js
+++ webpack.config.js
@@ -1,7 +1,7 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
+  entry: './src/index.tsx',
   output: {
     filename: 'index.js',
     path: path.join(__dirname, 'public')
