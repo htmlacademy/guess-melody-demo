2b867451d0d3d479c82b70162adae65b9f2147e2
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-03 15:47:31 +0300

    1.2 Установит и настроит webpack

---
+++ webpack.config.js
@@ -0,0 +1,27 @@
+const path = require(`path`);
+
+module.exports = {
+  entry: `./src/index.js`,
+  output: {
+    filename: `bundle.js`,
+    path: path.join(__dirname, `public`)
+  },
+  devServer: {
+    contentBase: path.join(__dirname, `public`),
+    open: false,
+    inline: false,
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: `babel-loader`,
+        },
+      }
+    ],
+  },
+  devtool: `source-map`,
+};
