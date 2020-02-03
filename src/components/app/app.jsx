7ace2951f475871f1b6a405c7273e13014019607
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-03 16:02:43 +0300

    1.3 Установит React и создаст первые компоненты

---
+++ src/components/app/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+
+
+const App = () => {
+  return (
+    <WelcomeScreen />
+  );
+};
+
+
+export default App;
