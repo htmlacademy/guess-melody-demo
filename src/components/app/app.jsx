cb9c091ae4f1263e095066e294461294dcf136db
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-03 16:04:45 +0300

    1.4 Добавит пропсы в компонент

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,9 +2,12 @@ import React from "react";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
 
-const App = () => {
+const App = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {errorsCount} = props;
+
   return (
-    <WelcomeScreen />
+    <WelcomeScreen errorsCount={errorsCount} />
   );
 };
 
