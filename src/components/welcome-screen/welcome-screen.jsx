f0117f36d3f3549c780c08f274e6728a83ca9596
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-03 15:36:26 +0300

    Module 1
    1.1 Установит и настроит eslint
    
    1.2 Установит и настроит webpack
    
    1.3 Установит React и создаст первые компоненты
    
    1.4 Добавит пропсы в компонент

---
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -0,0 +1,26 @@
+import React from "react";
+
+
+const WelcomeScreen = (props) => {
+  // eslint-disable-next-line react/prop-types
+  const {errorsCount} = props;
+
+  return (
+    <section className="welcome">
+      <div className="welcome__logo">
+        <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
+      </div>
+      <button className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
+      <h2 className="welcome__rules-title">Правила игры</h2>
+      <p className="welcome__text">Правила просты:</p>
+      <ul className="welcome__rules-list">
+        <li>Нужно ответить на все вопросы.</li>
+        <li>Можно допустить {errorsCount} ошибки.</li>
+      </ul>
+      <p className="welcome__text">Удачи!</p>
+    </section>
+  );
+};
+
+
+export default WelcomeScreen;
