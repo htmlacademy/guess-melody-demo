d50dc141c92b8b29742606a8e935d051d395fdc2
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-05 16:18:33 +0300

    2.4 Установит Enzyme, добавит обработчик клика для кнопки и обновит снепшот тесты

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,12 +2,16 @@ import React from "react";
 import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
 
+const welcomeButtonHandler = () => {};
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <WelcomeScreen
+      errorsCount={errorsCount}
+      onWelcomeButtonClick={welcomeButtonHandler}
+    />
   );
 };
 
--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -3,14 +3,19 @@ import PropTypes from "prop-types";
 
 
 const WelcomeScreen = (props) => {
-  const {errorsCount} = props;
+  const {errorsCount, onWelcomeButtonClick} = props;
 
   return (
     <section className="welcome">
       <div className="welcome__logo">
         <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
       </div>
-      <button className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
+      <button
+        onClick={onWelcomeButtonClick}
+        className="welcome__button"
+      >
+        <span className="visually-hidden">Начать игру</span>
+      </button>
       <h2 className="welcome__rules-title">Правила игры</h2>
       <p className="welcome__text">Правила просты:</p>
       <ul className="welcome__rules-list">
@@ -24,6 +29,7 @@ const WelcomeScreen = (props) => {
 
 WelcomeScreen.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  onWelcomeButtonClick: PropTypes.func.isRequired,
 };
 
 
