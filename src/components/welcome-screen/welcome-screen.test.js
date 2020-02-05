b410c0772f04689d864c9f0b895403937942b236
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-05 16:07:08 +0300

    2.3 Добавит снепшот тесты

---
+++ src/components/welcome-screen/welcome-screen.test.js
@@ -0,0 +1,13 @@
+import React from "react";
+import renderer from "react-test-renderer";
+import WelcomeScreen from "./welcome-screen.jsx";
+
+it(`Should WelcomeScreen render correctly`, () => {
+  const tree = renderer
+    .create(<WelcomeScreen
+      errorsCount={3}
+    />)
+    .toJSON();
+
+  expect(tree).toMatchSnapshot();
+});
