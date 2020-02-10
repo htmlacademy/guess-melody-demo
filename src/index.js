200307b507ee619e56500bf5b66766e68a1d58a1
Sasha Sushko <sushko@outlook.com>
2020-02-10 17:56:50 +0300

    3.5 Подключит моки для разработки в приложение

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app.jsx";
+import questions from "./mocks/questions.js";
 
 const Settings = {
   ERRORS_COUNT: 3
@@ -9,6 +10,7 @@ const Settings = {
 ReactDOM.render(
     <App
       errorsCount={Settings.ERRORS_COUNT}
+      questions={questions}
     />,
     document.querySelector(`#root`)
 );
