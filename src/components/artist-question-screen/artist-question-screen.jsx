337d26a8f86a9796155c97684e9a3f972b51cbda
Sasha Sushko <sushko@outlook.com>
2020-02-10 18:10:03 +0300

    3.13 Добавит по аналогии компонент с вопросом по артистам

---
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -0,0 +1,25 @@
+import React from "react";
+import PropTypes from "prop-types";
+import {GameType} from "../../const.js";
+
+const ArtistQuestionScreen = (props) => {
+  return (
+  );
+};
+
+ArtistQuestionScreen.propTypes = {
+  onAnswer: PropTypes.func.isRequired,
+  question: PropTypes.shape({
+    answers: PropTypes.arrayOf(PropTypes.shape({
+      artist: PropTypes.string.isRequired,
+      picture: PropTypes.string.isRequired,
+    })).isRequired,
+    song: PropTypes.shape({
+      artist: PropTypes.string.isRequired,
+      src: PropTypes.string.isRequired,
+    }).isRequired,
+    type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
+  }).isRequired,
+};
+
+export default ArtistQuestionScreen;
