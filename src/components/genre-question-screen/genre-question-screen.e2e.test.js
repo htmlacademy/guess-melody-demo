d002be6a3a431abd25645878e6520f847aec6d7b
Sasha Sushko <sushko@outlook.com>
2020-02-10 18:08:31 +0300

    3.12 Добавит e2e-тесты на компонент вопроса по жанру

---
+++ src/components/genre-question-screen/genre-question-screen.e2e.test.js
@@ -0,0 +1,75 @@
+import React from "react";
+import {configure, shallow} from "enzyme";
+import Adapter from "enzyme-adapter-react-16";
+import GenreQuestionScreen from "./genre-question-screen.jsx";
+
+configure({adapter: new Adapter()});
+
+const mock = {
+  question: {
+    type: `genre`,
+    genre: `rock`,
+    answers: [
+      {
+        src: `path`,
+        genre: `rock`,
+      },
+      {
+        src: `path`,
+        genre: `jazz`,
+      },
+      {
+        src: `path`,
+        genre: `jazz`,
+      },
+      {
+        src: `path`,
+        genre: `blues`,
+      },
+    ],
+  },
+};
+
+it(`When user answers genre question form is not sent`, () => {
+  const {question} = mock;
+  const onAnswer = jest.fn();
+  const genreQuestion = shallow(<GenreQuestionScreen
+    onAnswer={onAnswer}
+    question={question}
+  />);
+
+  const form = genreQuestion.find(`form`);
+  const formSendPrevention = jest.fn();
+  form.simulate(`submit`, {
+    preventDefault: formSendPrevention,
+  });
+
+  expect(onAnswer).toHaveBeenCalledTimes(1);
+  expect(formSendPrevention).toHaveBeenCalledTimes(1);
+});
+
+it(`User answer passed to callback is consistent with "userAnswer" prop`, () => {
+  const {question} = mock;
+  const onAnswer = jest.fn((...args) => [...args]);
+  const userAnswer = [false, true, false, false];
+
+  const genreQuestion = shallow(<GenreQuestionScreen
+    onAnswer={onAnswer}
+    question={question}
+  />);
+
+  const form = genreQuestion.find(`form`);
+  const inputTwo = genreQuestion.find(`input`).at(1);
+
+  inputTwo.simulate(`change`, {target: {checked: true}});
+  form.simulate(`submit`, {preventDefault() {}});
+
+  expect(onAnswer).toHaveBeenCalledTimes(1);
+
+  expect(onAnswer.mock.calls[0][0]).toMatchObject(question);
+  expect(onAnswer.mock.calls[0][1]).toMatchObject(userAnswer);
+
+  expect(
+      genreQuestion.find(`input`).map((it) => it.prop(`checked`))
+  ).toEqual(userAnswer);
+});
