b48ad12397ce41f46c84347446b9bdfd6e156609
Sasha Sushko <sushko@outlook.com>
2020-02-10 18:11:03 +0300

    3.15 Опишет логику отображения ответов в компоненте с вопросами по артистам

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -43,6 +43,21 @@ const ArtistQuestionScreen = (props) => {
         </div>
 
         <form className="game__artist">
+          {answers.map((answer, i) => (
+            <div key={answer.artist} className="artist">
+              <input className="artist__input visually-hidden" type="radio" name="answer" value={`answer-${i}`} id={`answer-${i}`}
+                onChange={(evt) => {
+                  evt.preventDefault();
+                  onAnswer(question, answer);
+                }}
+              />
+              <label className="artist__name" htmlFor={`answer-${i}`}>
+                <img className="artist__picture" src={answer.picture} alt={answer.artist} />
+                {answer.artist}
+              </label>
+            </div>
+          ))}
+        </form>
       </section>
     </section>
   );
