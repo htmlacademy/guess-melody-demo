251687f5230a2c6c7ef16b7e771f05ee8e964ce5
Sasha Sushko <sushko@outlook.com>
2020-02-10 18:19:57 +0300

    3.23 Опишет логику отображения игровых экранов

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -32,6 +32,33 @@ class App extends PureComponent {
         />
       );
     }
+
+    if (question) {
+      switch (question.type) {
+        case GameType.ARTIST:
+          return (
+            <ArtistQuestionScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+        case GameType.GENRE:
+          return (
+            <GenreQuestionScreen
+              question={question}
+              onAnswer={() => {
+                this.setState((prevState) => ({
+                  step: prevState.step + 1,
+                }));
+              }}
+            />
+          );
+      }
+    }
   }
 
   render() {
