f4f72d4cc9f33362ad27b642f42d03216b816cf5
Sasha Sushko <sushko@outlook.com>
2020-02-10 17:54:43 +0300

    3.4 Опишет моки для разработки для вопроса по артисту

--- src/mocks/questions.js
+++ src/mocks/questions.js
@@ -1,3 +1,5 @@
+const AVATAR_URL = `https://api.adorable.io/avatars/128`;
+
 export default [
   {
     type: `genre`,
@@ -14,6 +16,22 @@ export default [
     }, {
       src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
       genre: `rock`,
+    }],
+  }, {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    },
+    answers: [{
+      picture: `${AVATAR_URL}/${Math.random()}`,
+      artist: `John Snow`,
+    }, {
+      picture: `${AVATAR_URL}/${Math.random()}`,
+      artist: `Jack Daniels`,
+    }, {
+      picture: `${AVATAR_URL}/${Math.random()}`,
+      artist: `Jim Beam`,
     }],
   }
 ];
