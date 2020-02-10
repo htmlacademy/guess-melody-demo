4f8d3c1727310942d7d9cd03df82c9dd79100f5b
Sasha Sushko <sushko@outlook.com>
2020-02-10 18:10:45 +0300

    3.14 Опишет метод render компонента с вопросами по артистам

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -3,7 +3,48 @@ import PropTypes from "prop-types";
 import {GameType} from "../../const.js";
 
 const ArtistQuestionScreen = (props) => {
+  const {onAnswer, question} = props;
+  const {
+    answers,
+    song,
+  } = question;
+
   return (
+    <section className="game game--artist">
+      <header className="game__header">
+        <a className="game__back" href="#">
+          <span className="visually-hidden">Сыграть ещё раз</span>
+          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+        </a>
+
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370"
+            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
+        </svg>
+
+        <div className="game__mistakes">
+          <div className="wrong" />
+          <div className="wrong" />
+          <div className="wrong" />
+        </div>
+      </header>
+
+      <section className="game__screen">
+        <h2 className="game__title">Кто исполняет эту песню?</h2>
+        <div className="game__track">
+          <div className="track">
+            <button className="track__button track__button--play" type="button" />
+            <div className="track__status">
+              <audio
+                src={song.src}
+              />
+            </div>
+          </div>
+        </div>
+
+        <form className="game__artist">
+      </section>
+    </section>
   );
 };
 
