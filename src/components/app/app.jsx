b544e2b9ad0197651e61a702670532e33ee7a3a8
Sasha Sushko <sushko@outlook.com>
2020-02-10 18:17:56 +0300

    3.20 Опишет пути

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,6 +1,10 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen.jsx";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen.jsx";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen.jsx";
+import {GameType} from "../../const.js";
 
 class App extends PureComponent {
   constructor(props) {
@@ -10,10 +14,27 @@ class App extends PureComponent {
   render() {
     const {questions} = this.props;
 
-    return <WelcomeScreen
-      errorsCount={errorsCount}
-      onWelcomeButtonClick={() => {}}
-    />;
+    return (
+      <BrowserRouter>
+        <Switch>
+          <Route exact path="/">
+            {this._renderGameScreen()}
+          </Route>
+          <Route exact path="/artist">
+            <ArtistQuestionScreen
+              question={questions[1]}
+              onAnswer={() => {}}
+            />
+          </Route>
+          <Route exact path="/genre">
+            <GenreQuestionScreen
+              question={questions[0]}
+              onAnswer={() => {}}
+            />
+          </Route>
+        </Switch>
+      </BrowserRouter>
+    );
   }
 }
 
