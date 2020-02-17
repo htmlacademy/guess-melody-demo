f60486639ddb2ba8a6e0e2b31ac53d20034a0048
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-17 18:42:03 +0300

    4.2 Добавит новый компонент AudioPlayer и подключит к игровым экранам

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const.js";
+import AudioPlayer from "../audio-player/audio-player.jsx";
 
 
 const ArtistQuestionScreen = (props) => {
@@ -15,12 +16,10 @@ const ArtistQuestionScreen = (props) => {
       <h2 className="game__title">Кто исполняет эту песню?</h2>
       <div className="game__track">
         <div className="track">
-          <button className="track__button track__button--play" type="button" />
-          <div className="track__status">
-            <audio
-              src={song.src}
-            />
-          </div>
+          <AudioPlayer
+            isPlaying={true}
+            src={song.src}
+          />
         </div>
       </div>
 
