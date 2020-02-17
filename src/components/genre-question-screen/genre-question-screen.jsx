102e2a5eacf1be27c38ebe3da1862b5af652527f
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-17 18:42:03 +0300

    4.2 Добавит новый компонент AudioPlayer и подключит к игровым экранам

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,6 +1,7 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const.js";
+import AudioPlayer from "../audio-player/audio-player.jsx";
 
 
 class GenreQuestionScreen extends PureComponent {
@@ -32,12 +33,10 @@ class GenreQuestionScreen extends PureComponent {
         >
           {answers.map((answer, i) => (
             <div key={`${i}-${answer.src}`} className="track">
-              <button className="track__button track__button--play" type="button"/>
-              <div className="track__status">
-                <audio
-                  src={answer.src}
-                />
-              </div>
+              <AudioPlayer
+                isPlaying={i === 0}
+                src={answer.src}
+              />
               <div className="game__answer">
                 <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${i}`}
                   id={`answer-${i}`}
