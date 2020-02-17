9036052902069fa39dd8684895e023a70de1085b
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-17 19:57:21 +0300

    4.4 Выносит контроль за состоянием плеера в родителя

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,47 +1,59 @@
-import React from "react";
+import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const.js";
 import AudioPlayer from "../audio-player/audio-player.jsx";
 
 
-const ArtistQuestionScreen = (props) => {
-  const {onAnswer, question} = props;
-  const {
-    answers,
-    song,
-  } = question;
+class ArtistQuestionScreen extends PureComponent {
+  constructor(props) {
+    super(props);
 
-  return (
-    <section className="game__screen">
-      <h2 className="game__title">Кто исполняет эту песню?</h2>
-      <div className="game__track">
-        <div className="track">
-          <AudioPlayer
-            isPlaying={true}
-            src={song.src}
-          />
-        </div>
-      </div>
+    this.state = {
+      isPlaying: true,
+    };
+  }
 
-      <form className="game__artist">
-        {answers.map((answer, i) => (
-          <div key={answer.artist} className="artist">
-            <input className="artist__input visually-hidden" type="radio" name="answer" value={`answer-${i}`} id={`answer-${i}`}
-              onChange={(evt) => {
-                evt.preventDefault();
-                onAnswer(question, answer);
-              }}
+  render() {
+    const {isPlaying} = this.state;
+    const {onAnswer, question} = this.props;
+    const {
+      answers,
+      song,
+    } = question;
+
+    return (
+      <section className="game__screen">
+        <h2 className="game__title">Кто исполняет эту песню?</h2>
+        <div className="game__track">
+          <div className="track">
+            <AudioPlayer
+              isPlaying={isPlaying}
+              src={song.src}
+              onPlayButtonClick={() => this.setState({isPlaying: !isPlaying})}
             />
-            <label className="artist__name" htmlFor={`answer-${i}`}>
-              <img className="artist__picture" src={answer.picture} alt={answer.artist} />
-              {answer.artist}
-            </label>
           </div>
-        ))}
-      </form>
-    </section>
-  );
-};
+        </div>
+
+        <form className="game__artist">
+          {answers.map((answer, i) => (
+            <div key={answer.artist} className="artist">
+              <input className="artist__input visually-hidden" type="radio" name="answer" value={`answer-${i}`} id={`answer-${i}`}
+                onChange={(evt) => {
+                  evt.preventDefault();
+                  onAnswer(question, answer);
+                }}
+              />
+              <label className="artist__name" htmlFor={`answer-${i}`}>
+                <img className="artist__picture" src={answer.picture} alt={answer.artist} />
+                {answer.artist}
+              </label>
+            </div>
+          ))}
+        </form>
+      </section>
+    );
+  }
+}
 
 
 ArtistQuestionScreen.propTypes = {
--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -9,13 +9,14 @@ class GenreQuestionScreen extends PureComponent {
     super(props);
 
     this.state = {
+      activePlayer: 0,
       answers: [false, false, false, false],
     };
   }
 
   render() {
     const {onAnswer, question} = this.props;
-    const {answers: userAnswers} = this.state;
+    const {answers: userAnswers, activePlayer} = this.state;
     const {
       answers,
       genre,
@@ -34,7 +35,12 @@ class GenreQuestionScreen extends PureComponent {
           {answers.map((answer, i) => (
             <div key={`${i}-${answer.src}`} className="track">
               <AudioPlayer
-                isPlaying={i === 0}
+                onPlayButtonClick={() => {
+                  this.setState({
+                    activePlayer: activePlayer === i ? -1 : i,
+                  });
+                }}
+                isPlaying={i === activePlayer}
                 src={answer.src}
               />
               <div className="game__answer">
