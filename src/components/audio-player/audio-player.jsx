c36f24f2c2f5c6201cd3dbd78647c636cd5ab8ed
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-17 19:57:21 +0300

    4.4 Выносит контроль за состоянием плеера в родителя

--- src/components/audio-player/audio-player.jsx
+++ src/components/audio-player/audio-player.jsx
@@ -52,6 +52,7 @@ export default class AudioPlayer extends PureComponent {
 
   render() {
     const {isLoading, isPlaying} = this.state;
+    const {onPlayButtonClick} = this.props;
 
     return (
       <Fragment>
@@ -59,7 +60,10 @@ export default class AudioPlayer extends PureComponent {
           className={`track__button track__button--${isPlaying ? `pause` : `play`}`}
           type="button"
           disabled={isLoading}
-          onClick={() => this.setState({isPlaying: !this.state.isPlaying})}
+          onClick={() => {
+            this.setState({isPlaying: !this.state.isPlaying});
+            onPlayButtonClick();
+          }}
         />
         <div className="track__status">
           <audio
@@ -83,5 +87,6 @@ export default class AudioPlayer extends PureComponent {
 
 AudioPlayer.propTypes = {
   isPlaying: PropTypes.bool.isRequired,
+  onPlayButtonClick: PropTypes.func.isRequired,
   src: PropTypes.string.isRequired,
 };
--- src/components/audio-player/audio-player.test.js
+++ src/components/audio-player/audio-player.test.js
@@ -14,6 +14,7 @@ it(`AudioPlayer is rendered correctly`, () => {
 
   const tree = renderer.create(<AudioPlayer
     isPlaying={false}
+    onPlayButtonClick={() => {}}
     src={song.src}
   />, {
     createNodeMock: () => {
