4c63e1a2ffbbe10fec5de8dacbc0028639bc9934
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-20 16:33:36 +0300

    5.1 Установит redux, react-redux и подготовит начальную инфраструктуру (reducer, action, store)

---
+++ src/reducer.test.js
@@ -0,0 +1,56 @@
+import {reducer, ActionType} from "./reducer.js";
+
+it(`Reducer without additional parameters should return initial state`, () => {
+  expect(reducer(void 0, {})).toEqual({
+    step: -1,
+    mistakes: 0,
+  });
+});
+
+it(`Reducer should increment current step by a given value`, () => {
+  expect(reducer({
+    step: -1,
+    mistakes: 0,
+  }, {
+    type: ActionType.INCREMENT_STEP,
+    payload: 1,
+  })).toEqual({
+    step: 0,
+    mistakes: 0,
+  });
+
+  expect(reducer({
+    step: -1,
+    mistakes: 0,
+  }, {
+    type: ActionType.INCREMENT_STEP,
+    payload: 0,
+  })).toEqual({
+    step: -1,
+    mistakes: 0,
+  });
+});
+
+it(`Reducer should increment number of mistakes by a given value`, () => {
+  expect(reducer({
+    step: -1,
+    mistakes: 0,
+  }, {
+    type: ActionType.INCREMENT_MISTAKES,
+    payload: 1,
+  })).toEqual({
+    step: -1,
+    mistakes: 1,
+  });
+
+  expect(reducer({
+    step: -1,
+    mistakes: 0,
+  }, {
+    type: ActionType.INCREMENT_MISTAKES,
+    payload: 0,
+  })).toEqual({
+    step: -1,
+    mistakes: 0,
+  });
+});
