d3bb5cdf5d8658de2c38c0d19182b0ff715353e8
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-20 16:33:36 +0300

    5.1 Установит redux, react-redux и подготовит начальную инфраструктуру (reducer, action, store)

--- src/index.js
+++ src/index.js
@@ -1,16 +1,24 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import {createStore} from "redux";
+import {Provider} from "react-redux";
 import App from "./components/app/app.jsx";
 import questions from "./mocks/questions.js";
+import {reducer} from "./reducer.js";
+
 
 const Settings = {
   ERRORS_COUNT: 3
 };
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App
-      errorsCount={Settings.ERRORS_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsCount={Settings.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
---
+++ src/utils.js
@@ -0,0 +1,3 @@
+export const extend = (a, b) => {
+  return Object.assign({}, a, b);
+};
