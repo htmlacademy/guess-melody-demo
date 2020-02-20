2cc377cae581de9766541d74101f1025347cd556
Igor Antonov <antonov.igor.khv@gmail.com>
2020-02-20 16:33:36 +0300

    5.1 Установит redux, react-redux и подготовит начальную инфраструктуру (reducer, action, store)

---
+++ src/reducer.js
@@ -0,0 +1,30 @@
+import {extend} from "./utils.js";
+
+const initialState = {
+  mistakes: 0,
+  step: -1,
+};
+
+const ActionType = {
+  INCREMENT_MISTAKES: `INCREMENT_MISTAKES`,
+  INCREMENT_STEP: `INCREMENT_STEP`,
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.INCREMENT_STEP:
+      return extend(state, {
+        step: state.step + action.payload,
+      });
+
+    case ActionType.INCREMENT_MISTAKES:
+      return extend(state, {
+        mistakes: state.mistakes + action.payload,
+      });
+  }
+
+  return state;
+};
+
+
+export {reducer, ActionType};
