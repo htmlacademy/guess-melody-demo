2ffc2f1e0982b18f3b250421462ab5c2e0768757
Sasha Sushko <sushko@outlook.com>
2020-02-24 17:53:57 +0300

    6.3 Вынесет состояние из компонента Жанра в HOC
    У нас остался последний компонент с состоянием. Так же унесём его в HOC.
    
    Обратите внимание, что нам пришлось переписать тест, потому что интерфейс компонента изменился.

--- src/components/genre-question-screen/genre-question-screen.e2e.test.js
+++ src/components/genre-question-screen/genre-question-screen.e2e.test.js
@@ -37,6 +37,7 @@ it(`When user answers genre question form is not sent`, () => {
     onAnswer={onAnswer}
     question={question}
     renderPlayer={() => {}}
+    userAnswers={[false, false, false, false]}
   />);
 
   const form = genreQuestion.find(`form`);
@@ -58,6 +59,8 @@ it(`User answer passed to callback is consistent with "userAnswer" prop`, () => 
     onAnswer={onAnswer}
     question={question}
     renderPlayer={() => {}}
+    onChange={() => {}}
+    userAnswers={userAnswer}
   />);
 
   const form = genreQuestion.find(`form`);
@@ -68,8 +71,7 @@ it(`User answer passed to callback is consistent with "userAnswer" prop`, () => 
 
   expect(onAnswer).toHaveBeenCalledTimes(1);
 
-  expect(onAnswer.mock.calls[0][0]).toMatchObject(question);
-  expect(onAnswer.mock.calls[0][1]).toMatchObject(userAnswer);
+  expect(onAnswer.mock.calls[0][0]).toEqual(void 0);
 
   expect(
       genreQuestion.find(`input`).map((it) => it.prop(`checked`))
