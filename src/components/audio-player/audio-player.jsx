bd6a91d5025375e0d88e39802354fe45d14a4332
Sasha Sushko <sushko@outlook.com>
2020-02-24 17:38:22 +0300

    6.1 Вынесет состояние плеера в HOC
    С этого модуля начинаем плотно соблюдать критерий Б16. Поэтому состояние плеера из компонента переносим в HOC with-audio.js.
    
    Попутно мы изменили интерфейс компонента, поэтому необходимо переписать тест на него. Так же напишем тест на HOC, потому что он изменяет разметку.

--- src/components/audio-player/audio-player.jsx
+++ src/components/audio-player/audio-player.jsx
@@ -1,58 +1,10 @@
-import React, {PureComponent, Fragment, createRef} from "react";
+import React, {PureComponent, Fragment} from "react";
 import PropTypes from "prop-types";
 
 
 export default class AudioPlayer extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this._audioRef = createRef();
-
-    this.state = {
-      progress: 0,
-      isLoading: true,
-      isPlaying: props.isPlaying,
-    };
-  }
-
-  componentDidMount() {
-    const {src} = this.props;
-    const audio = this._audioRef.current;
-
-    audio.src = src;
-
-    audio.oncanplaythrough = () => this.setState({
-      isLoading: false,
-    });
-
-    audio.onplay = () => {
-      this.setState({
-        isPlaying: true,
-      });
-    };
-
-    audio.onpause = () => this.setState({
-      isPlaying: false,
-    });
-
-    audio.ontimeupdate = () => this.setState({
-      progress: audio.currentTime
-    });
-  }
-
-  componentWillUnmount() {
-    const audio = this._audioRef.current;
-
-    audio.oncanplaythrough = null;
-    audio.onplay = null;
-    audio.onpause = null;
-    audio.ontimeupdate = null;
-    audio.src = ``;
-  }
-
   render() {
-    const {isLoading, isPlaying} = this.state;
-    const {onPlayButtonClick} = this.props;
+    const {isLoading, isPlaying, onPlayButtonClick, children} = this.props;
 
     return (
       <Fragment>
@@ -60,33 +12,22 @@ export default class AudioPlayer extends PureComponent {
           className={`track__button track__button--${isPlaying ? `pause` : `play`}`}
           type="button"
           disabled={isLoading}
-          onClick={() => {
-            this.setState({isPlaying: !this.state.isPlaying});
-            onPlayButtonClick();
-          }}
+          onClick={() => onPlayButtonClick()}
         />
         <div className="track__status">
-          <audio
-            ref={this._audioRef}
-          />
+          {children}
         </div>
       </Fragment>
     );
   }
-
-  componentDidUpdate() {
-    const audio = this._audioRef.current;
-
-    if (this.props.isPlaying) {
-      audio.play();
-    } else {
-      audio.pause();
-    }
-  }
 }
 
 AudioPlayer.propTypes = {
+  isLoading: PropTypes.bool.isRequired,
   isPlaying: PropTypes.bool.isRequired,
   onPlayButtonClick: PropTypes.func.isRequired,
-  src: PropTypes.string.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.arrayOf(PropTypes.node),
+    PropTypes.node
+  ]).isRequired,
 };
