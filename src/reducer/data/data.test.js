c892ce8743faae6b9679ecbe6e72226d828dd799
Sasha Sushko <sushko@outlook.com>
2020-03-03 09:21:13 +0300

    7.1 Декомпозирует редьюсер
    Разделим большой редьюсер на несколько поменьше, которые будут отвечать только за часть хранилища.
    
    Такие части в программировании называются неймспейсами (namespace).
    
    Затем с помощью метода combineReducers библиотеки Redux соберём эти маленькие части обратно в большой редьюсер.

---
+++ src/reducer/data/data.test.js
@@ -0,0 +1,55 @@
+import {reducer, ActionType} from "./data.js";
+
+const questions = [
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [{
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `blues`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `jazz`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }],
+  }, {
+    type: `artist`,
+    song: {
+      artist: `Jim Beam`,
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+    },
+    answers: [{
+      picture: `https://api.adorable.io/avatars/128/A`,
+      artist: `John Snow`,
+    }, {
+      picture: `https://api.adorable.io/avatars/128/AB`,
+      artist: `Jack Daniels`,
+    }, {
+      picture: `https://api.adorable.io/avatars/128/AC`,
+      artist: `Jim Beam`,
+    }],
+  },
+];
+
+
+it(`Reducer without additional parameters should return initial state`, () => {
+  expect(reducer(void 0, {})).toEqual({
+    questions: [],
+  });
+});
+
+it(`Reducer should update questions by load questions`, () => {
+  expect(reducer({
+    questions: [],
+  }, {
+    type: ActionType.LOAD_QUESTIONS,
+    payload: questions,
+  })).toEqual({
+    questions,
+  });
+});
---
+++ src/reducer/user/user.test.js
@@ -0,0 +1,60 @@
+import {reducer, ActionCreator, ActionType, AuthorizationStatus} from "./user.js";
+
+
+it(`Reducer without additional parameters should return initial state`, () => {
+  expect(reducer(void 0, {})).toEqual({
+    authorizationStatus: AuthorizationStatus.NO_AUTH,
+  });
+});
+
+it(`Reducer should change authorizationStatus by a given value`, () => {
+  expect(reducer({
+    authorizationStatus: AuthorizationStatus.NO_AUTH,
+  }, {
+    type: ActionType.REQUIRED_AUTHORIZATION,
+    payload: AuthorizationStatus.AUTH,
+  })).toEqual({
+    authorizationStatus: AuthorizationStatus.AUTH,
+  });
+
+  expect(reducer({
+    authorizationStatus: AuthorizationStatus.AUTH,
+  }, {
+    type: ActionType.REQUIRED_AUTHORIZATION,
+    payload: AuthorizationStatus.NO_AUTH,
+  })).toEqual({
+    authorizationStatus: AuthorizationStatus.NO_AUTH,
+  });
+
+  expect(reducer({
+    authorizationStatus: AuthorizationStatus.AUTH,
+  }, {
+    type: ActionType.REQUIRED_AUTHORIZATION,
+    payload: AuthorizationStatus.AUTH,
+  })).toEqual({
+    authorizationStatus: AuthorizationStatus.AUTH,
+  });
+
+  expect(reducer({
+    authorizationStatus: AuthorizationStatus.NO_AUTH,
+  }, {
+    type: ActionType.REQUIRED_AUTHORIZATION,
+    payload: AuthorizationStatus.NO_AUTH,
+  })).toEqual({
+    authorizationStatus: AuthorizationStatus.NO_AUTH,
+  });
+});
+
+describe(`Action creators work correctly`, () => {
+  it(`Action creator for require authorization returns correct action`, () => {
+    expect(ActionCreator.requireAuthorization(AuthorizationStatus.NO_AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.NO_AUTH,
+    });
+
+    expect(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)).toEqual({
+      type: ActionType.REQUIRED_AUTHORIZATION,
+      payload: AuthorizationStatus.AUTH,
+    });
+  });
+});
