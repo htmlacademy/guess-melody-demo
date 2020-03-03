c813417384084d8cc14cb58880e30c580f9dd12f
Sasha Sushko <sushko@outlook.com>
2020-03-03 10:07:55 +0300

    7.6 Подключит декомпозированные редьюсеры и Thunk в приложение
    Redux Thunk — это middleware, поэтому для её подключения мы используем метод applyMiddleware из самой Redux.
    
    Важно: поддержку Redux DevTools мы отключили временно, позже мы её вернём.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,20 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
 import App from "./components/app/app.jsx";
-import {reducer} from "./reducer.js";
+import reducer from "./reducer/reducer.js";
+import {Operation as DataOperation} from "./reducer/data/data.js";
+import {Operation as UserOperation, ActionCreator, AuthorizationStatus} from "./reducer/user/user.js";
+import {createAPI} from "./api.js";
 
+
+const api = createAPI(() => {});
 
 const store = createStore(
     reducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    applyMiddleware(thunk.withExtraArgument(api))
 );
 
 ReactDOM.render(
