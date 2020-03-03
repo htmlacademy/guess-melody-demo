b72ab86500a659cafb32b4d5d5228545347dfa89
Sasha Sushko <sushko@outlook.com>
2020-03-03 10:29:42 +0300

    7.10 Добавит экран авторизации в логику игры
    У нас почти всё готово. Осталось подключить компонент авторизации в логику _renderGameScreen и поддержать изменения в тестах.
    
    И чтобы два раза не ходить, подправим игровой экран — используем в нём наши селекторы. Ведь теперь они легко переиспользуются во всём приложении.
    
    Готово!

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -55,13 +55,24 @@ class App extends PureComponent {
     }
 
     if (step >= questions.length) {
-      return (
-        <WinScreen
-          questionsCount={questions.length}
-          mistakesCount={mistakes}
-          onReplayButtonClick={resetGame}
-        />
-      );
+      if (authorizationStatus === AuthorizationStatus.AUTH) {
+        return (
+          <WinScreen
+            questionsCount={questions.length}
+            mistakesCount={mistakes}
+            onReplayButtonClick={resetGame}
+          />
+        );
+      } else if (authorizationStatus === AuthorizationStatus.NO_AUTH) {
+        return (
+          <AuthScreen
+            onReplayButtonClick={resetGame}
+            onSubmit={login}
+          />
+        );
+      }
+
+      return null;
     }
 
     if (question) {
