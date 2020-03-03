d3d51cc942c9cf8c934d5243305272a69a3adb30
Sasha Sushko <sushko@outlook.com>
2020-03-03 09:44:30 +0300

    7.4 Добавит модуль работы с API
    Для работы с сетевыми запросами мы будем использовать библиотеку Axios, поэтому первым делом установим её.
    
    Так как работа непосредственно с запросами будет происходить в action с помощью Redux Thunk, в модуле API нам достаточно описать создание экземпляра API и логику перехватчиков.
    
    Перехватчики нам потребутся, чтобы понимать, авторизован пользователь или нет. И на основе этих выводов показывать ему «секретную» страницу или форму входа соответственно.
    
    Перехватчик успешного обращения (onSuccess) у нас просто проксирует объект запроса, а вот перехватчик провального обращения (onFail) при ошибке «401 пользователь не авторизован» будет вызывать колбэк onUnauthorized, который содержит в себе логику по работе с неавторизованным пользователем, её мы опишем позднее.

---
+++ src/api.js
@@ -0,0 +1,35 @@
+import axios from "axios";
+
+const Error = {
+  UNAUTHORIZED: 401
+};
+
+export const createAPI = (onUnauthorized) => {
+  const api = axios.create({
+    baseURL: `https://htmlacademy-react-3.appspot.com/guess-melody`,
+    timeout: 1000 * 5,
+    withCredentials: true,
+  });
+
+  const onSuccess = (response) => {
+    return response;
+  };
+
+  const onFail = (err) => {
+    const {response} = err;
+
+    if (response.status === Error.UNAUTHORIZED) {
+      onUnauthorized();
+
+      // Бросаем ошибку, потому что нам важно прервать цепочку промисов после запроса авторизации.
+      // Запрос авторизации - это особый случай и важно дать понять приложению, что запрос был неудачным.
+      throw err;
+    }
+
+    throw err;
+  };
+
+  api.interceptors.response.use(onSuccess, onFail);
+
+  return api;
+};
