e4de9767adb8070ca7594ac02dacfbc814641b18
Sasha Sushko <sushko@outlook.com>
2020-03-05 17:41:13 +0300

    8.4 Перелинкует экраны приложения
    Теперь, когда за навигацию будет отвечать роутер, мы можем выкинуть наш функции и заменить их компонентом Link.
    
    Не стоит забывать о тестах. Во-первых, в E2E-тесты нужно добавить роутер, как зависимость. Во-вторых, после замены кнопок на ссылки нужно обновить снепшоты.

--- src/components/game-over-screen/game-over-screen.test.js
+++ src/components/game-over-screen/game-over-screen.test.js
@@ -1,12 +1,21 @@
 import React from "react";
 import renderer from "react-test-renderer";
+import {Router} from "react-router-dom";
 import GameOverScreen from "./game-over-screen.jsx";
+import history from "../../history.js";
+
 
 it(`Should GameOverScreen render correctly`, () => {
   const tree = renderer
-    .create(<GameOverScreen
-      onReplayButtonClick={() => {}}
-    />)
+    .create(
+        <Router
+          history={history}
+        >
+          <GameOverScreen
+            onReplayButtonClick={() => {}}
+          />
+        </Router>
+    )
     .toJSON();
 
   expect(tree).toMatchSnapshot();
