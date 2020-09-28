640f79ae66eb3463dd90582a1847ce1e54fed64f
Igor Antonov <antonov.igor.khv@gmail.com>
2020-09-28 12:57:55 +0300

    1.5. Валидация props
    Добавим валидацию `prop` у всех компонент. Для этого установим пакет `prop-types` и определим правила валидации. Уберём комментарии, которые указывали на игнорирование проверки. После описания правил валидации выполним `npm test` и убедимся, что ошибок нет.
    
    Подробная информация о PropTypes приведена в разделе «Материалы».

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,9 +1,9 @@
 import React from "react";
+import PropTypes from "prop-types";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
 
 
 const App = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {errorsCount} = props;
 
   return (
@@ -11,5 +11,8 @@ const App = (props) => {
   );
 };
 
+App.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
+};
 
 export default App;
--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -1,8 +1,7 @@
 import React from "react";
-
+import PropTypes from "prop-types";
 
 const WelcomeScreen = (props) => {
-  // eslint-disable-next-line react/prop-types
   const {errorsCount} = props;
 
   return (
@@ -22,5 +21,8 @@ const WelcomeScreen = (props) => {
   );
 };
 
+WelcomeScreen.propTypes = {
+  errorsCount: PropTypes.number.isRequired,
+};
 
 export default WelcomeScreen;
