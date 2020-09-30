0ef609d3e4f6d21c6494c15ccb4d46e4a21630db
Igor Antonov <antonov.igor.khv@gmail.com>
2020-09-30 18:38:49 +0300

    2.3. Добавим маршрутизацию
    На этом шаге добавим маршрутизацию в приложение. Для этого в модуле компонента `App` подключим несколько компонентов из пакета `react-router-dom` (`Switch`, `Route`, `BrowserRouter`), а затем обновим код самого компонента: для каждого маршрута будет рендерится соответствующий компонент.
    
    Компонент `Route` принимает prop `exact`, который позволяет задать точное соответствие маршруту и `location.pathname`.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,13 +1,40 @@
 import React from "react";
 import PropTypes from "prop-types";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
+import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen";
+import GenreQuestionScreen from "../genre-question-screen/genre-question-screen";
+import AuthScreen from "../auth-screen/auth-screen";
+import GameOverScreen from "../game-over-screen/game-over-screen";
+import WinScreen from "../win-screen/win-screen";
 
 
 const App = (props) => {
   const {errorsCount} = props;
 
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/">
+          <WelcomeScreen errorsCount={errorsCount} />
+        </Route>
+        <Route exact path="/dev-artist">
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path="/dev-genre">
+          <GenreQuestionScreen />
+        </Route>
+        <Route exact path="/login">
+          <AuthScreen />
+        </Route>
+        <Route exact path="/result">
+          <WinScreen />
+        </Route>
+        <Route exact path="/lose">
+          <GameOverScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 };
 
