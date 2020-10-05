e4ced391393397bcf747c763c9309437dd7d9d9d
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-05 13:27:04 +0300

    3.9. Создадим новый компонент GameScreen.
    В этом компоненте будет сосредоточена логика переключния игровых экранов. Компонент должен рендериться по маршруту `/game`.
    
    Пока мы опишем заготовку для компонента. Нам потребуется стейт, значит создадим  class-компонент.
    
    В стейте будем хранить индекс очередной игры.

---
+++ src/components/game-screen/game-screen.jsx
@@ -0,0 +1,13 @@
+import {PureComponent} from "react";
+
+class GameScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      step: 0,
+    };
+  }
+}
+
+export default GameScreen;
