c38a31d25b78f81f42eed071ba0d6fdbcb435c57
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-05 13:07:13 +0300

    3.3. Подключим моки.
    Подключим и  передадим моки в компонент `App`.

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./components/app/app";
+import questions from "./mocks/questions";
 
 const Settings = {
   ERRORS_COUNT: 3
@@ -9,6 +10,7 @@ const Settings = {
 ReactDOM.render(
     <App
       errorsCount={Settings.ERRORS_COUNT}
+      questions={questions}
     />,
     document.querySelector(`#root`)
 );
