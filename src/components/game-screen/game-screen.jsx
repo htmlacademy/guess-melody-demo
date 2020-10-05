3909c3211611bd6e90b081ff08b0628a27db2377
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-05 13:32:14 +0300

    3.10. Опишем метод `render` у компонента GameScreen
     В методе `render` мы должны написать код, который будет рендерить игру на основе мок. Напомним, вариантов игр два: «Угадай исполнителя», «Угадай жанр».
    
    Если игр нет (кончились), то нужно вернуть пользователя на стартовую страницу — «WelcomeScreen».
    
    Обратите внимание на передачу обработчиков в компоненты `ArtistQuestionScreen` и `GenreQuestionScreen`. Код обработчика `onAnswer` мы определяем тут же. В нём будет происходить изменение стейта.

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -1,4 +1,9 @@
-import {PureComponent} from "react";
+import React, {PureComponent} from "react";
+import PropTypes from 'prop-types';
+import {Redirect} from 'react-router-dom';
+import {GameType} from '../../const';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 
 class GameScreen extends PureComponent {
   constructor(props) {
@@ -8,6 +13,50 @@ class GameScreen extends PureComponent {
       step: 0,
     };
   }
+
+  render() {
+    const {questions} = this.props;
+    const {step} = this.state;
+    const question = questions[step];
+
+    if (step >= questions.length || !question) {
+      return (
+        <Redirect to="/" />
+      );
+    }
+
+    switch (question.type) {
+      case GameType.ARTIST:
+        return (
+          <ArtistQuestionScreen
+            question={question}
+            onAnswer={() => {
+              this.setState((prevState) => ({
+                step: prevState.step + 1,
+              }));
+            }}
+          />
+        );
+      case GameType.GENRE:
+        return (
+          <GenreQuestionScreen
+            question={question}
+            onAnswer={() => {
+              this.setState((prevState) => ({
+                step: prevState.step + 1,
+              }));
+            }}
+          />
+        );
+    }
+
+    return <Redirect to="/" />;
+  }
+
 }
+
+GameScreen.propTypes = {
+  questions: PropTypes.array.isRequired,
+};
 
 export default GameScreen;
