2b3db319ce1d3f4ebea3cadea1965d00606e1bfd
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-05 13:14:51 +0300

    3.4. Рефакторим компонент GenreQuestionScreen.
    В компонент `GenreQuestionScreen` нам потребуется `state`. В нём мы будем хранить информацию о правильном ответе на очередной вопрос. Поэтому обновим компонент и перепишем его в class-components.
    
    Создадим модуль `const.js` для хранения различных констант. Пока опишем в нём константу `GameType` (перечисление) для хранения типа игры.

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,78 +1,51 @@
-import React from "react";
+import React, {PureComponent} from "react";
+import PropTypes from "prop-types";
+import {GameType} from "../../const";
 
-const GenreQuestionScreen = () => {
-  return (
-    <section className="game game--genre">
-      <header className="game__header">
-        <a className="game__back" href="#">
-          <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
-        </a>
+class GenreQuestionScreen extends PureComponent {
+  constructor(props) {
+    super(props);
+  }
 
-        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-          <circle className="timer__line" cx="390" cy="390" r="370"
-            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
-        </svg>
+  render() {
+    return (
+      <section className="game game--genre">
+        <header className="game__header">
+          <a className="game__back" href="#">
+            <span className="visually-hidden">Сыграть ещё раз</span>
+            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
+          </a>
 
-        <div className="game__mistakes">
-          <div className="wrong"/>
-          <div className="wrong"/>
-          <div className="wrong"/>
-        </div>
-      </header>
+          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+            <circle className="timer__line" cx="390" cy="390" r="370"
+              style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
+          </svg>
 
-      <section className="game__screen">
-        <h2 className="game__title">Выберите треки</h2>
-        <form className="game__tracks">
-          <div className="track">
-            <button className="track__button track__button--play" type="button"></button>
-            <div className="track__status">
-              <audio></audio>
-            </div>
-            <div className="game__answer">
-              <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-1" />
-              <label className="game__check" htmlFor="answer-1">Отметить</label>
-            </div>
+          <div className="game__mistakes">
+            <div className="wrong"/>
+            <div className="wrong"/>
+            <div className="wrong"/>
           </div>
+        </header>
 
-          <div className="track">
-            <button className="track__button track__button--play" type="button"></button>
-            <div className="track__status">
-              <audio></audio>
-            </div>
-            <div className="game__answer">
-              <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-2" />
-              <label className="game__check" htmlFor="answer-2">Отметить</label>
-            </div>
-          </div>
-
-          <div className="track">
-            <button className="track__button track__button--pause" type="button"></button>
-            <div className="track__status">
-              <audio></audio>
-            </div>
-            <div className="game__answer">
-              <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-3" />
-              <label className="game__check" htmlFor="answer-3">Отметить</label>
-            </div>
-          </div>
-
-          <div className="track">
-            <button className="track__button track__button--play" type="button"></button>
-            <div className="track__status">
-              <audio></audio>
-            </div>
-            <div className="game__answer">
-              <input className="game__input visually-hidden" type="checkbox" name="answer" value="answer-1" id="answer-4" />
-              <label className="game__check" htmlFor="answer-4">Отметить</label>
-            </div>
-          </div>
-
-          <button className="game__submit button" type="submit">Ответить</button>
-        </form>
+        <section className="game__screen">
+          <h2 className="game__title">Выберите треки</h2>
+        </section>
       </section>
-    </section>
-  );
+    );
+  }
+}
+
+GenreQuestionScreen.propTypes = {
+  onAnswer: PropTypes.func.isRequired,
+  question: PropTypes.shape({
+    answers: PropTypes.arrayOf(PropTypes.shape({
+      src: PropTypes.string.isRequired,
+      genre: PropTypes.string.isRequired,
+    })).isRequired,
+    genre: PropTypes.string.isRequired,
+    type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
+  }).isRequired,
 };
 
 export default GenreQuestionScreen;
