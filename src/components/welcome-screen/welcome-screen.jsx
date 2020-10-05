f96baf3119186c1114db3cb7f7f4e65917be65f2
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-05 13:35:47 +0300

    3.11. Добавим действие для кнопки запуска игры
    У нас есть компонент, который отвечает за принятие решения, компонент какой игры следует рендерить. Теперь добавим функциональности в компонент `WelcomeScreen`. Научим кнопку «Play» запускать игру.
    
    Реализую запуска будем делать снаружи. Внутри компонента лишь получим prop с колбэк-функцией.

--- src/components/welcome-screen/welcome-screen.jsx
+++ src/components/welcome-screen/welcome-screen.jsx
@@ -2,14 +2,21 @@ import React from "react";
 import PropTypes from "prop-types";
 
 const WelcomeScreen = (props) => {
-  const {errorsCount} = props;
+  const {errorsCount, onPlayButtonClick} = props;
 
   return (
     <section className="welcome">
       <div className="welcome__logo">
         <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
       </div>
-      <button className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
+      <button
+        className="welcome__button"
+        onClick={onPlayButtonClick}
+      >
+        <span className="visually-hidden">
+          Начать игру
+        </span>
+      </button>
       <h2 className="welcome__rules-title">Правила игры</h2>
       <p className="welcome__text">Правила просты:</p>
       <ul className="welcome__rules-list">
@@ -23,6 +30,7 @@ const WelcomeScreen = (props) => {
 
 WelcomeScreen.propTypes = {
   errorsCount: PropTypes.number.isRequired,
+  onPlayButtonClick: PropTypes.func.isRequired,
 };
 
 export default WelcomeScreen;
