bb838a7305eb490576984d28b0352eb21cc74622
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-06 18:13:56 +0300

    4.4. Добавим поддержку тега <audio />
    Задачу воспроизведения мы решили при помощи объекта `Audio`, но добиться
    того же самого результата можно другим способом — при помощи тега
    `<audio>`. React позволяет это сделать за счёт применения механизма
    рефов (ref). С его помощью можно получить доступ к DOM-элементам,
    которые были созданы во время рендеринга.
    
    Избавимся в компоненте `AudioPlayer` от применения объекта `Audio` и решим
    задачу воспроизведения при помощи тега `<audio>`. Код от этого поменяется не
    сильно.
    
    В конструкторе компонента создадим новую ссылку (`createRef`) и сохраним
    в приватном поле компонента. Она нам потребуется для установки связи с
    <audio>. Затем обновим метод `componentDidMount`. Вместо объекта Audio
    будем взаимодействовать с DOM-элементом (<audio>).
    
    Чтобы связать `ref` с компонентом `audio`, передадим ему в пропе `ref`,
    созданную в конструкторе ссылку.

--- src/components/audio-player/audio-player.jsx
+++ src/components/audio-player/audio-player.jsx
@@ -1,10 +1,12 @@
-import React, {PureComponent, Fragment} from "react";
+import React, {PureComponent, Fragment, createRef} from "react";
 import PropTypes from "prop-types";
 
 
 export default class AudioPlayer extends PureComponent {
   constructor(props) {
     super(props);
+
+    this._audioRef = createRef();
 
     this.state = {
       isLoading: true,
@@ -14,29 +16,31 @@ export default class AudioPlayer extends PureComponent {
 
   componentDidMount() {
     const {src} = this.props;
+    const audio = this._audioRef.current;
 
-    this._audio = new Audio(src);
+    audio.src = src;
 
-    this._audio.oncanplaythrough = () => this.setState({
+    audio.oncanplaythrough = () => this.setState({
       isLoading: false,
     });
 
-    this._audio.onplay = () => {
+    audio.onplay = () => {
       this.setState({
         isPlaying: true,
       });
     };
 
-    this._audio.onpause = () => this.setState({
+    audio.onpause = () => this.setState({
       isPlaying: false,
     });
   }
 
   componentWillUnmount() {
-    this._audio.oncanplaythrough = null;
-    this._audio.onplay = null;
-    this._audio.onpause = null;
-    this._audio = null;
+    const audio = this._audioRef.current;
+
+    audio.oncanplaythrough = null;
+    audio.onplay = null;
+    audio.onpause = null;
   }
 
   render() {
@@ -51,17 +55,21 @@ export default class AudioPlayer extends PureComponent {
           onClick={() => this.setState({isPlaying: !this.state.isPlaying})}
         />
         <div className="track__status">
-          <audio />
+          <audio
+            ref={this._audioRef}
+          />
         </div>
       </Fragment>
     );
   }
 
   componentDidUpdate() {
+    const audio = this._audioRef.current;
+
     if (this.state.isPlaying) {
-      this._audio.play();
+      audio.play();
     } else {
-      this._audio.pause();
+      audio.pause();
     }
   }
 }
