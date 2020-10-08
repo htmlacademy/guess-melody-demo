19f6bdb909991432381aaf57655d4c2781ba883e
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-08 15:49:40 +0300

    4.11. Подключит HOC в GameScreen
    Воспользуемся HOC `withActivePlayer` в компоненте `GameScreen`. Подключим
    HOC, создадим обёртки (`GenreQuestionScreenWrapped`, `ArtistQuestionScreenWrapped`)
    и воспользуемся ими в методе `render`.

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -5,6 +5,11 @@ import {GameType} from '../../const';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 
+import withAudioPlayer from "../../hocs/with-audio-player/with-audio-player";
+
+const GenreQuestionScreenWrapped = withAudioPlayer(GenreQuestionScreen);
+const ArtistQuestionScreenWrapped = withAudioPlayer(ArtistQuestionScreen);
+
 class GameScreen extends PureComponent {
   constructor(props) {
     super(props);
@@ -28,7 +33,7 @@ class GameScreen extends PureComponent {
     switch (question.type) {
       case GameType.ARTIST:
         return (
-          <ArtistQuestionScreen
+          <ArtistQuestionScreenWrapped
             question={question}
             onAnswer={() => {
               this.setState((prevState) => ({
@@ -39,7 +44,7 @@ class GameScreen extends PureComponent {
         );
       case GameType.GENRE:
         return (
-          <GenreQuestionScreen
+          <GenreQuestionScreenWrapped
             question={question}
             onAnswer={() => {
               this.setState((prevState) => ({
