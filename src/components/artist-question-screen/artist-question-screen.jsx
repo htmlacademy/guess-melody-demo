6b5254bb4cf4d4a8fcd7d9518ec20528387c40fb
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-08 15:46:33 +0300

    4.9. Переведём в функциональный компонент. WIP
    Мы отказались от стейта в компоненте `ArtistQuestionScreen`. Раз так, то
    сконвертируем его в функциональный компонент.

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,65 +1,62 @@
-import React, {PureComponent} from "react";
+import React from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const";
 
-class ArtistQuestionScreen extends PureComponent {
-  render() {
-    const {onAnswer, question, renderPlayer} = this.props;
-    const {
-      answers,
-      song,
-    } = question;
+const ArtistQuestionScreen = (props) => {
+  const {onAnswer, question, renderPlayer} = props;
+  const {
+    answers,
+    song,
+  } = question;
 
-    return (
-      <section className="game game--artist">
-        <header className="game__header">
-          <a className="game__back" href="#">
-            <span className="visually-hidden">Сыграть ещё раз</span>
-            <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-          </a>
+  return (
+    <section className="game game--artist">
+      <header className="game__header">
+        <a className="game__back" href="#">
+          <span className="visually-hidden">Сыграть ещё раз</span>
+          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+        </a>
 
-          <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
-            <circle className="timer__line" cx="390" cy="390" r="370"
-              style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
-          </svg>
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370"
+            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
+        </svg>
 
-          <div className="game__mistakes">
-            <div className="wrong" />
-            <div className="wrong" />
-            <div className="wrong" />
+        <div className="game__mistakes">
+          <div className="wrong" />
+          <div className="wrong" />
+          <div className="wrong" />
+        </div>
+      </header>
+
+      <section className="game__screen">
+        <h2 className="game__title">Кто исполняет эту песню?</h2>
+        <div className="game__track">
+          <div className="track">
+            {renderPlayer(song.src, 0)}
           </div>
-        </header>
+        </div>
 
-        <section className="game__screen">
-          <h2 className="game__title">Кто исполняет эту песню?</h2>
-          <div className="game__track">
-            <div className="track">
-              {renderPlayer(song.src, 0)}
+        <form className="game__artist">
+          {answers.map((answer, i) => (
+            <div key={answer.artist} className="artist">
+              <input className="artist__input visually-hidden" type="radio" name="answer" value={`answer-${i}`} id={`answer-${i}`}
+                onChange={(evt) => {
+                  evt.preventDefault();
+                  onAnswer(question, answer);
+                }}
+              />
+              <label className="artist__name" htmlFor={`answer-${i}`}>
+                <img className="artist__picture" src={answer.picture} alt={answer.artist} />
+                {answer.artist}
+              </label>
             </div>
-          </div>
-
-          <form className="game__artist">
-            {answers.map((answer, i) => (
-              <div key={answer.artist} className="artist">
-                <input className="artist__input visually-hidden" type="radio" name="answer" value={`answer-${i}`} id={`answer-${i}`}
-                  onChange={(evt) => {
-                    evt.preventDefault();
-                    onAnswer(question, answer);
-                  }}
-                />
-                <label className="artist__name" htmlFor={`answer-${i}`}>
-                  <img className="artist__picture" src={answer.picture} alt={answer.artist} />
-                  {answer.artist}
-                </label>
-              </div>
-            ))}
-          </form>
-        </section>
+          ))}
+        </form>
       </section>
-    );
-  }
-}
-
+    </section>
+  );
+};
 
 ArtistQuestionScreen.propTypes = {
   onAnswer: PropTypes.func.isRequired,
