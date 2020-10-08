d06b46b1f500d4de5cd599ff22450ee71cb18dbb
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-08 15:39:06 +0300

    4.8. Уберём подключение AudioPlayer из ArtistQuestionScreen. WIP.
    Стейт в компоненте `ArtistQuestionScreen` больше не нужен. Ровным счётом
    как и подключение `AudioPlayer`. Выполним небольшой рефакторинг и для
    отрисовки плеера воспользуемся пропом `renderPlayer`.

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -1,20 +1,10 @@
 import React, {PureComponent} from "react";
 import PropTypes from "prop-types";
 import {GameType} from "../../const";
-import AudioPlayer from "../audio-player/audio-player";
 
 class ArtistQuestionScreen extends PureComponent {
-  constructor(props) {
-    super(props);
-
-    this.state = {
-      isPlaying: true,
-    };
-  }
-
   render() {
-    const {isPlaying} = this.state;
-    const {onAnswer, question} = this.props;
+    const {onAnswer, question, renderPlayer} = this.props;
     const {
       answers,
       song,
@@ -44,11 +34,7 @@ class ArtistQuestionScreen extends PureComponent {
           <h2 className="game__title">Кто исполняет эту песню?</h2>
           <div className="game__track">
             <div className="track">
-              <AudioPlayer
-                isPlaying={isPlaying}
-                src={song.src}
-                onPlayButtonClick={() => this.setState({isPlaying: !isPlaying})}
-              />
+              {renderPlayer(song.src, 0)}
             </div>
           </div>
 
@@ -88,6 +74,7 @@ ArtistQuestionScreen.propTypes = {
     }).isRequired,
     type: PropTypes.oneOf([GameType.ARTIST, GameType.GENRE]).isRequired,
   }).isRequired,
+  renderPlayer: PropTypes.func.isRequired,
 };
 
 export default ArtistQuestionScreen;
