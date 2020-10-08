3c59e5dda19de64bd982b9e5e71a87484fe35e4f
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-08 15:19:40 +0300

    4.7. Удалим служебные маршруты
    Рефакторинг. Перед тем как воспользоваться HOC, созданным в прошлом шаге,
    удалим служебные маршруты `/dev-artist` и `/dev-genre`. Они нам больше не
    потребуются.
    
    Если их сейчас не удалить, то придётся воспользоваться HOC `withAudioPlayer` и
    в `App`. В этом нет необходимости, тем более переключение экранов уже реализовано.

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -2,8 +2,6 @@ import React from "react";
 import PropTypes from "prop-types";
 import {Switch, Route, BrowserRouter} from "react-router-dom";
 import WelcomeScreen from "../welcome-screen/welcome-screen";
-import ArtistQuestionScreen from "../artist-question-screen/artist-question-screen";
-import GenreQuestionScreen from "../genre-question-screen/genre-question-screen";
 import AuthScreen from "../auth-screen/auth-screen";
 import GameOverScreen from "../game-over-screen/game-over-screen";
 import WinScreen from "../win-screen/win-screen";
@@ -12,7 +10,6 @@ import GameScreen from "../game-screen/game-screen";
 
 const App = (props) => {
   const {errorsCount, questions} = props;
-  const [firstQuestion, secondQuestion] = questions;
 
   return (
     <BrowserRouter>
@@ -26,18 +23,6 @@ const App = (props) => {
             />
           )}
         />
-        <Route exact path="/dev-artist">
-          <ArtistQuestionScreen
-            question={secondQuestion}
-            onAnswer={() => {}}
-          />
-        </Route>
-        <Route exact path="/dev-genre">
-          <GenreQuestionScreen
-            question={firstQuestion}
-            onAnswer={() => {}}
-          />
-        </Route>
         <Route exact path="/login">
           <AuthScreen />
         </Route>
