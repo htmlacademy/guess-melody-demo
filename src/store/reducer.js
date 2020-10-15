280173d366387a385800471b820ac0210e2f589d
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-15 16:44:14 +0300

    5.15. Добавит проверку на максимальное количество ошибок

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,19 +1,30 @@
 import {extend} from "../utils";
 import {ActionType} from "./action";
+import questions from "../mocks/questions";
+import {MAX_MISTAKE_COUNT} from "../const";
 
 const initialState = {
   mistakes: 0,
   step: 0,
+  questions,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case ActionType.INCREMENT_STEP:
+      let nextStep = state.step + action.payload;
+
       return extend(state, {
-        step: state.step + action.payload,
+        step: nextStep,
       });
 
     case ActionType.INCREMENT_MISTAKES:
+      const mistakes = state.mistakes + action.payload;
+
+      if (mistakes >= MAX_MISTAKE_COUNT) {
+        return extend({}, initialState);
+      }
+
       return extend(state, {
         mistakes: state.mistakes + action.payload,
       });
