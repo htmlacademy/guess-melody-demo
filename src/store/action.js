8744fa2e656659952f0321f700809ec875ec6c49
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-15 12:36:52 +0300

    5.6. Добавит дополнительное действие
    Мы готовы воспользоваться подготовленным хранилищем для хранения состояния. Перед тем как это сделать, создадим дополнительное действие (action).
    
    Нам потребуется действие для сброса игры. Оно должно возвращать стейт к изначальному состоянию.

--- src/store/action.js
+++ src/store/action.js
@@ -1,4 +1,15 @@
 export const ActionType = {
   INCREMENT_MISTAKES: `INCREMENT_MISTAKES`,
   INCREMENT_STEP: `INCREMENT_STEP`,
+  RESET_GAME: `RESET_GAME`,
+};
+
+export const ActionCreator = {
+  incrementStep: () => ({
+    type: ActionType.INCREMENT_STEP,
+    payload: 1,
+  }),
+  resetGame: () => ({
+    type: ActionType.RESET_GAME,
+  })
 };
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -17,6 +17,9 @@ const reducer = (state = initialState, action) => {
       return extend(state, {
         mistakes: state.mistakes + action.payload,
       });
+
+    case ActionType.RESET_GAME:
+      return extend({}, initialState);
   }
 
   return state;
