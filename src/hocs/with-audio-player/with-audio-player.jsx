12e46dcc5914130af396f2bfb7c074e6d075f3e3
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-19 11:08:57 +0300

    6.3. Подключит компонент WithAudio в WithAudioPlayer
    Импортируем `WithAudio` в `WithActivePlayer`.

--- src/hocs/with-audio-player/with-audio-player.jsx
+++ src/hocs/with-audio-player/with-audio-player.jsx
@@ -1,5 +1,8 @@
 import React, {PureComponent} from 'react';
-import AudioPlayer from "../../components/audio-player/audio-player";
+import Player from "../../components/audio-player/audio-player";
+import withAudio from "../with-audio/with-audio";
+
+const AudioPlayer = withAudio(Player);
 
 const withActivePlayer = (Component) => {
   class WithActivePlayer extends PureComponent {
