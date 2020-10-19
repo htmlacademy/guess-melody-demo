065b7f692436428d0be00a7da921c245fadfd739
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-19 10:29:43 +0300

    6.1. Добавит новый HOC WithAudio.
    Начнём оптимизацию с переноса состояния в HOC-компоненты. Компонент
    `AudioPlayer` прекрасный для этого кандидат. У него есть собственное
    состояние и его можно вынести в HOC.
    
    Создадим новый HOC `WithAudio` и перенесём в него всё, что связано
    с воспроизведением мелодий: работа с элементом `audio` и состояние.

---
+++ src/hocs/with-audio/with-audio.jsx
@@ -0,0 +1,68 @@
+import React, {createRef, PureComponent} from "react";
+import PropTypes from "prop-types";
+
+const withAudio = (Component) => {
+  class WithAudio extends PureComponent {
+    constructor(props) {
+      super(props);
+
+      this._audioRef = createRef();
+
+      this.state = {
+        isLoading: true,
+      };
+    }
+
+    componentDidMount() {
+      const {src} = this.props;
+      const audio = this._audioRef.current;
+
+      audio.src = src;
+
+      audio.oncanplaythrough = () => this.setState({
+        isLoading: false,
+      });
+    }
+
+    componentDidUpdate() {
+      const audio = this._audioRef.current;
+
+      if (this.props.isPlaying) {
+        audio.play();
+      } else {
+        audio.pause();
+      }
+    }
+
+    componentWillUnmount() {
+      const audio = this._audioRef.current;
+
+      audio.oncanplaythrough = null;
+    }
+
+    render() {
+      const {isLoading} = this.state;
+
+      return (
+        <Component
+          {...this.props}
+          isLoading={isLoading}
+        >
+          <audio
+            ref={this._audioRef}
+          />
+        </Component>
+      );
+    }
+  }
+
+  WithAudio.propTypes = {
+    isPlaying: PropTypes.bool.isRequired,
+    onPlayButtonClick: PropTypes.func.isRequired,
+    src: PropTypes.string.isRequired,
+  };
+
+  return WithAudio;
+};
+
+export default withAudio;
