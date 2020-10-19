81eec869ef03874859af92fb69f027a795aa709e
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-19 12:03:29 +0300

    6.4. Добавит новый HOC WithUserAnswer
    В компоненте `GenreQuestionScreen` тоже есть стейт. Мы можем
    воспользоваться предыдущим приёмом и перенести его в HOC. Для этого
    создадим ещё один HOC `WithUserAnswer`.

---
+++ src/hocs/with-user-answer/with-user-answer.jsx
@@ -0,0 +1,68 @@
+import React, {PureComponent} from "react";
+import PropTypes from "prop-types";
+import {GameType} from "../../const";
+
+
+const withUserAnswer = (Component) => {
+  class WithUserAnswer extends PureComponent {
+    constructor(props) {
+      super(props);
+
+      this.state = {
+        answers: new Array(props.question.answers.length).fill(false),
+      };
+
+      this.handleAnswer = this.handleAnswer.bind(this);
+      this.handleChange = this.handleChange.bind(this);
+    }
+
+    handleAnswer() {
+      const {onAnswer, question} = this.props;
+      const {answers} = this.state;
+
+      onAnswer(question, answers);
+    }
+
+    handleChange(i, value) {
+      const {answers} = this.state;
+
+      const userAnswers = answers.slice(0);
+      userAnswers[i] = value;
+
+      this.setState({
+        answers: userAnswers,
+      });
+    }
+
+    render() {
+      const {answers} = this.state;
+
+      return (
+        <Component
+          {...this.props}
+          userAnswers={answers}
+          onAnswer={this.handleAnswer}
+          onChange={this.handleChange}
+        />
+      );
+    }
+  }
+
+  WithUserAnswer.propTypes = {
+    question: PropTypes.shape({
+      answers: PropTypes.arrayOf(PropTypes.shape({
+        src: PropTypes.string.isRequired,
+        genre: PropTypes.string.isRequired,
+      })).isRequired,
+      genre: PropTypes.string.isRequired,
+      type: PropTypes.oneOf([GameType.GENRE]).isRequired,
+    }).isRequired,
+    onAnswer: PropTypes.func.isRequired,
+  };
+
+
+  return WithUserAnswer;
+};
+
+
+export default withUserAnswer;
