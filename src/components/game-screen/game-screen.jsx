cc79101141d70d9814cdea4f801ebe8aed3a4eb4
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-19 15:07:27 +0300

    6.9. Обновит компонент GameScreen
    Теперь немного изменим логику работы компонента `GameScreen`.
    Добавим переходы к экранам `WinScreen` и `GameOverScreen`.
    
    Если превышено допустимое количество ошибок, то перенаправляем
    пользователя по маршруту `/lose`. Если кончились вопросы и остались
    попытки на ошибку, то перенаправляем на `WinScreen`.

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {Redirect} from 'react-router-dom';
 import {connect} from "react-redux";
 import {ActionCreator} from "../../store/action";
-import {GameType} from '../../const';
+import {GameType, MAX_MISTAKE_COUNT} from '../../const';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 import Mistakes from "../mistakes/mistakes";
@@ -21,16 +21,19 @@ const GameScreen = (props) => {
     questions,
     step,
     onUserAnswer,
-    resetGame,
     mistakes,
   } = props;
   const question = questions[step];
 
-  if (step >= questions.length || !question) {
-    resetGame();
-
+  if (mistakes >= MAX_MISTAKE_COUNT) {
     return (
-      <Redirect to="/" />
+      <Redirect to="/lose" />
+    );
+  }
+
+  if (step >= questions.length || !question) {
+    return (
+      <Redirect to="/result" />
     );
   }
 
@@ -63,7 +66,6 @@ GameScreen.propTypes = {
       PropTypes.oneOfType([artistQuestionProp, genreQuestionProp]).isRequired
   ),
   step: PropTypes.number.isRequired,
-  resetGame: PropTypes.func.isRequired,
   onUserAnswer: PropTypes.func.isRequired,
   mistakes: PropTypes.number.isRequired,
 };
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,7 +1,6 @@
 import {extend} from "../utils";
 import {ActionType} from "./action";
 import questions from "../mocks/questions";
-import {MAX_MISTAKE_COUNT} from "../const";
 
 const initialState = {
   mistakes: 0,
@@ -19,12 +18,6 @@ const reducer = (state = initialState, action) => {
       });
 
     case ActionType.INCREMENT_MISTAKES:
-      const mistakes = state.mistakes + action.payload;
-
-      if (mistakes >= MAX_MISTAKE_COUNT) {
-        return extend({}, initialState);
-      }
-
       return extend(state, {
         mistakes: state.mistakes + action.payload,
       });
