0123129172a98f89405278b4efd4e4a54dc66695
Igor Antonov <antonov.igor.khv@gmail.com>
2020-10-23 16:21:51 +0300

    7.9. Подключит redux-thunk при конфигурировании хранилища.
    Запросы готовы, а значит можно свести всё воедино и подключить `redux-thunk`
    во время конфигурирования хранилища.
    
    В `index.js` импортируем функцию `applyMiddleware` из `redux`, а затем
    обновим код инициализации хранилища.
    
    Пока откажемся от поддержки Redux DevTools (на время) и подключим
    redux-thunk. Предварительно не забыв создать экземпляр API.
    
    Затем выполним действия: получим с сервера список вопросов для игр и
    проверим наличие авторизации.

--- src/index.js
+++ src/index.js
@@ -1,14 +1,26 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import {createStore} from "redux";
+import {createStore, applyMiddleware} from "redux";
 import {Provider} from "react-redux";
+import thunk from "redux-thunk";
+import {createAPI} from "./services/api";
 import App from "./components/app/app";
 import rootReducer from "./store/reducers/root-reducer";
+import {requireAuthorization} from "./store/action";
+import {fetchQuestionList, checkAuth} from "./store/api-actions";
+import {AuthorizationStatus} from "./const";
+
+const api = createAPI(
+    () => store.dispatch(requireAuthorization(AuthorizationStatus.NO_AUTH))
+);
 
 const store = createStore(
     rootReducer,
-    window.__REDUX_DEVTOOLS_EXTENSION__ ? window.__REDUX_DEVTOOLS_EXTENSION__() : (f) => f
+    applyMiddleware(thunk.withExtraArgument(api))
 );
+
+store.dispatch(fetchQuestionList());
+store.dispatch(checkAuth());
 
 ReactDOM.render(
     <Provider store={store}>
