11bc6db9b9aab2ff1b903687e8ff35d9c1be179c
Igor Antonov <antonov.igor.khv@gmail.com>
2020-11-14 21:00:59 +0300

    8.12. Добавит новый тест для компонента WinScreen.
    Попробуем протестировать компонент `WinScreen`, который подключён к хранилищу. Для него уже есть несколько тестов, но в них мы работали с экземпляром компонента, который не подключён к хранилищу.
    
    Импортируем компонент `WinScreen`, который подключён к хранилищу и опишем несколько тестовых случаев. Начнём с конфигурирования. Опишем функцию `beforeEach`. В ней подготовим всё, что нам потребуется для тестирования. Эта функция будет выполнена перед запуском тестов текущей группы.
    
    Раз мы собираемся тестировать компонент, подключённый к хранилищу, то необходимо  замокать store. Сначала выполним конфигурирование (`configureStore`), а затем подготовим (`mockStore`) содержимое `store`.  Нас интересуют только данные для пространства `GAME`.
    
    Затем отрендерим компонент. Обратите внимание, теперь мы не передаём значения для пропсов `questionsCount` и  `mistakesCount`. Эти значения будут установлены из `store`.
    
    Тестовых случае будет два. С помощью первого проверим соответствие снепшоту. А во втором убедимся, что после выполнения клика по кнопке, `dispatch` был вызван один раз.

--- src/components/win-screen/win-screen.test.js
+++ src/components/win-screen/win-screen.test.js
@@ -1,6 +1,8 @@
 import React from "react";
 import renderer from "react-test-renderer";
-import {WinScreen} from "./win-screen";
+import {Provider} from "react-redux";
+import configureStore from "redux-mock-store";
+import WinScreen, {WinScreen as WinScreenWithoutStore} from "./win-screen";
 
 const noop = () => {};
 
@@ -9,7 +11,7 @@ describe(`Should WinScreen render correctly`, () => {
     it(`With 0 mistake`, () => {
       const tree = renderer
         .create(
-            <WinScreen
+            <WinScreenWithoutStore
               questionsCount={3}
               mistakesCount={0}
               onReplayButtonClick={noop}
@@ -25,7 +27,7 @@ describe(`Should WinScreen render correctly`, () => {
     it(`With 1 mistake`, () => {
       const tree = renderer
         .create(
-            <WinScreen
+            <WinScreenWithoutStore
               questionsCount={3}
               mistakesCount={1}
               onReplayButtonClick={noop}
@@ -43,7 +45,7 @@ describe(`Should WinScreen render correctly`, () => {
     it(`With 0 mistake`, () => {
       const tree = renderer
         .create(
-            <WinScreen
+            <WinScreenWithoutStore
               questionsCount={2}
               mistakesCount={0}
               onReplayButtonClick={noop}
@@ -58,7 +60,7 @@ describe(`Should WinScreen render correctly`, () => {
     it(`With 1 mistake`, () => {
       const tree = renderer
         .create(
-            <WinScreen
+            <WinScreenWithoutStore
               questionsCount={2}
               mistakesCount={1}
               onReplayButtonClick={noop}
@@ -70,4 +72,43 @@ describe(`Should WinScreen render correctly`, () => {
       expect(tree).toMatchSnapshot();
     });
   });
+});
+
+describe(`Render connected to store component`, () => {
+  const mockStore = configureStore([]);
+  let store = null;
+  let winScreenComponent = null;
+
+  beforeEach(() => {
+    store = mockStore({
+      GAME: {
+        step: 33,
+        mistakes: 2,
+      }
+    });
+
+    store.dispatch = jest.fn();
+
+    winScreenComponent = renderer.create(
+        <Provider store={store}>
+          <WinScreen
+            onReplayButtonClick={noop}
+            resetGameAction={noop}
+          />
+        </Provider>
+    );
+  });
+
+  it(`Should WinScreen connected to store render correctly`, () => {
+    expect(winScreenComponent.toJSON()).toMatchSnapshot();
+  });
+
+  it(`Should call dispatch when button click`, () => {
+    renderer.act(() => {
+      winScreenComponent.root.findByType(`button`).props.onClick();
+    });
+
+    expect(store.dispatch).toHaveBeenCalledTimes(1);
+  });
+
 });
