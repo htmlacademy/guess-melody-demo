1a134722218d75602e5ea4c6885b5f8abfcbde44
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-01 11:08:36 +0300

    1.3. Сконфигурирует webpack.
    Теперь займёмся webpack. Создадим файл `webpack.config.js` и опишем в
    нём конфигурацию.
    
    Для работы нам потребуется транспайлер (babel). Его тоже необходимо
    сконфигурировать. Опишем конфигурацию в `.babelrc`.
    
    Последний штрих: добавим в `.gitignore` исключение для бандлов,
    полученных в результате сборки проекта.

---
+++ webpack.config.js
@@ -0,0 +1,29 @@
+const path = require('path');
+
+module.exports = {
+  entry: './src/index.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'public')
+  },
+  devServer: {
+    contentBase: path.resolve(__dirname, 'public'),
+    open: false,
+    port: 1337,
+  },
+  module: {
+    rules: [
+      {
+        test: /\.(js|jsx)$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+        },
+      }
+    ],
+  },
+  resolve: {
+    extensions: ['.js', '.jsx']
+  },
+  devtool: 'source-map',
+};
