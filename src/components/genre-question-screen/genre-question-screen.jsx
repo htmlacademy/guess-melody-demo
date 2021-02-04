1280308185ff57292a7bdc41951eecf6f5d7acde
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-04 11:40:43 +0300

    2.4. Обновит ссылку для логотипа.
    Клик по логотипу перенаправляет пользователя на главный экран
    приложения. Сейчас эта задача решается за счёт компонента `<a>`. Обновим
    компоненты и заменим `<a>` на `<Link>` из пакета `react-router-dom`.

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
+import {Link} from 'react-router-dom';
 
 const GenreQuestionScreen = () => {
   return (
     <section className="game game--genre">
       <header className="game__header">
-        <a className="game__back" href="/">
+        <Link className="game__back" to="/">
           <span className="visually-hidden">Сыграть ещё раз</span>
           <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию"/>
-        </a>
+        </Link>
 
         <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
           <circle className="timer__line" cx="390" cy="390" r="370"
