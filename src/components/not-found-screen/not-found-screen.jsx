bee032e2c2eed2f2490127db2cb716712f31214d
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-04 11:01:16 +0300

    2.1. Добавит компоненты экранов.
    Создадим несколько новых компонентов: `ArtistQuestionScreen`,
    `AuthScreen`, `GameOverScreen`, `GenreQuestionScreen`, `WinScreen` и
    `NotFoundScreen`. Каждый компонент — отдельный экран приложения.
    Пока сделаем их максимально простыми, входных данных они принимать не
    будут.

---
+++ src/components/not-found-screen/not-found-screen.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+const NotFoundScreen = () => {
+  return (
+    <section className="game">
+      <header className="game__header">
+        <a className="game__back" href="/">
+          <span className="visually-hidden">Сыграть ещё раз</span>
+          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+        </a>
+
+        <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
+          <circle className="timer__line" cx="390" cy="390" r="370"
+            style={{filter: `url(#blur)`, transform: `rotate(-90deg) scaleY(-1)`, transformOrigin: `center`}}/>
+        </svg>
+      </header>
+
+      <section className="game__screen">
+        <h1>404. Page not found</h1>
+        <Link to="/">Вернуться на главную</Link>
+      </section>
+    </section>
+  );
+};
+
+export default NotFoundScreen;
