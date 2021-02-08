3a6d42f90fa76cd7cd7d4d7c612ee0e0a1316198
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-08 13:21:16 +0300

    3.5. Добавит стейт для компонента GenreQuestionScreen.
    Воспользуемся хуком `useState` и добавим с его помощью стейт для
    компонента `GenreQuestionScreen`.

--- src/components/genre-question-screen/genre-question-screen.jsx
+++ src/components/genre-question-screen/genre-question-screen.jsx
@@ -1,9 +1,10 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import {GameType} from '../../const';
 
 const GenreQuestionScreen = (props) => {
+  const [userAnswers, setUserAnswers] = useState([false, false, false, false]);
   const {question} = props;
   const {answers, genre} = question;
 
@@ -41,6 +42,11 @@ const GenreQuestionScreen = (props) => {
               <div className="game__answer">
                 <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${id}`}
                   id={`answer-${id}`}
+                  checked={userAnswers[id]}
+                  onChange={({target}) => {
+                    const value = target.checked;
+                    setUserAnswers([...userAnswers.slice(0, id), value, ...userAnswers.slice(id + 1)]);
+                  }}
                 />
                 <label className="game__check" htmlFor={`answer-${id}`}>Отметить</label>
               </div>
