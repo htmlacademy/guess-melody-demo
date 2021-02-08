9d8aa35fe769741b67b219c20ec463b2b503c2eb
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-08 12:57:16 +0300

    3.1. Добавит моки для игры «Угадай жанр».
    Для хранения тестовых данных заведём отдельную директорию `mocks`.
    Создадим в ней файл `questions.js` и опишем тестовые данные для игр.

---
+++ src/mocks/questions.js
@@ -0,0 +1,19 @@
+export default [
+  {
+    type: `genre`,
+    genre: `rock`,
+    answers: [{
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `blues`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `jazz`,
+    }, {
+      src: `https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg`,
+      genre: `rock`,
+    }],
+  }
+];
