271c74583a934a467d51417c8151ffc1ce5a7ef1
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-15 14:22:26 +0300

    4.4. Добавит новый компонент AudioPlayer.
    Для воспроизведения мелодий создадим новый компонент «AudioPlayer». Оба
    игровых экрана (`ArtistQuestionScreen`, `GenreQuestionScreen`) умеют
    воспроизводить мелодии. Поэтому функциональность для воспроизведения
    разумно вынести в отдельный компонент и переиспользовать многократно.
    
    Проигрывать мелодии будем с помощью объекта `Audio`. Поскольку в игре
    предусмотрена возможность приостанавливать и возобновлять воспроизведение
    мелодий, компоненту `AudioPlayer` потребуется состояние.
    
    В стейте сохраним информацию о готовности воспроизводить мелодию
    (`isLoading`), и статусу воспроизведения (`isPlaying`): проигрывается
    или приостановлено.
    
    Создадим экземпляр объекта `Audio` после монтирования компонента
    в DOM. Для этого воспользуемся хуком `useEffect`. В теле хука
    создадим экземпляр объекта и оформим подписки на события. Подробная
    информация о событиях объекта `Audio` доступна в MDN.

---
+++ src/components/audio-player/audio-player.jsx
@@ -0,0 +1,54 @@
+import React, {Fragment, useState, useEffect, useRef} from 'react';
+import PropTypes from 'prop-types';
+
+const AudioPlayer = ({defaultIsPlaying, src}) => {
+  const [isLoading, setIsLoading] = useState(true);
+  const [isPlaying, setIsPlaying] = useState(defaultIsPlaying);
+
+  const audioRef = useRef();
+
+  useEffect(() => {
+    audioRef.current = new Audio(src);
+    audioRef.current.oncanplaythrough = () => setIsLoading(false);
+    audioRef.current.onplay = () => setIsPlaying(true);
+    audioRef.current.onpause = () => setIsPlaying(false);
+
+    return () => {
+      audioRef.current.pause();
+      audioRef.current.oncanplaythrough = null;
+      audioRef.current.onplay = null;
+      audioRef.current.onpause = null;
+      audioRef.current = null;
+    };
+  }, [src]);
+
+  useEffect(() => {
+    if (isPlaying) {
+      audioRef.current.play();
+      return;
+    }
+
+    audioRef.current.pause();
+  }, [isPlaying]);
+
+  return (
+    <Fragment>
+      <button
+        className={`track__button track__button--${isPlaying ? `pause` : `play`}`}
+        type="button"
+        disabled={isLoading}
+        onClick={() => setIsPlaying(!isPlaying)}
+      />
+      <div className="track__status">
+        <audio />
+      </div>
+    </Fragment>
+  );
+};
+
+AudioPlayer.propTypes = {
+  defaultIsPlaying: PropTypes.bool.isRequired,
+  src: PropTypes.string.isRequired,
+};
+
+export default AudioPlayer;
