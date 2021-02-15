f6958d95ac85d006820aa8baa2db701028a4f0de
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-15 14:27:10 +0300

    4.5. Подключит компонент AudioPlayer в ArtistQuestionScreen.
    Воспользуемся компонентом `AudioPlayer` в `ArtistQuestionScreen`. Плееру
    требуется передать путь к мелодии, которую следует воспроизводить
    (`src`) и необходимость воспроизведения сразу как только мелодия будет
    готова для воспроизведения.

--- src/components/artist-question-screen/artist-question-screen.jsx
+++ src/components/artist-question-screen/artist-question-screen.jsx
@@ -2,6 +2,7 @@ import React from 'react';
 import {Link} from 'react-router-dom';
 import PropTypes from 'prop-types';
 import artistQuestionProp from './artist-question.prop';
+import AudioPlayer from '../audio-player/audio-player';
 
 const ArtistQuestionScreen = (props) => {
   const {onAnswer, question} = props;
@@ -31,12 +32,10 @@ const ArtistQuestionScreen = (props) => {
         <h2 className="game__title">Кто исполняет эту песню?</h2>
         <div className="game__track">
           <div className="track">
-            <button className="track__button track__button--play" type="button" />
-            <div className="track__status">
-              <audio
-                src={song.src}
-              />
-            </div>
+            <AudioPlayer
+              defaultIsPlaying={true}
+              src={song.src}
+            />
           </div>
         </div>
 
