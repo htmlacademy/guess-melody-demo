f9097fcd9ce0daa8f713c08002ba9d040bcae977
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-18 13:50:21 +0300

    5.10. Добавит действие `game/incrementMistake`.
    Следующим шагом создадим новое действие, в котором будем выполнять
    проверку правильности выбранного ответа и на основании проверки
    увеличивать счётчик ошибок.

--- project/src/store/action.js
+++ project/src/store/action.js
@@ -1,3 +1,6 @@
+import {isArtistAnswerCorrect, isGenreAnswerCorrect} from '../game';
+import {GameType} from '../const';
+
 export const ActionType = {
   INCREMENT_MISTAKES: 'game/incrementMistake',
   INCREMENT_STEP: 'game/incrementStep',
@@ -12,4 +15,23 @@ export const ActionCreator = {
   resetGame: () => ({
     type: ActionType.RESET_GAME,
   }),
+  incrementMistake: (question, userAnswer) => {
+    let answerIsCorrect;
+
+    switch (question.type) {
+      case GameType.ARTIST:
+        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+        break;
+      case GameType.GENRE:
+        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+        break;
+      default:
+        answerIsCorrect = false;
+    }
+
+    return {
+      type: ActionType.INCREMENT_MISTAKES,
+      payload: answerIsCorrect ? 0 : 1,
+    };
+  },
 };
