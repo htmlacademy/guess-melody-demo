8c095cd777c91df23a65be14fd70a25b8e2d8683
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-18 11:59:31 +0300

    5.3. Подключит redux к проекту.
    Следующим шагом подключим `redux` к проекту. Подключение выполним в
    файле `index.js`. Нам потребуется функция  `createStore` из пакета
    `redux`. С её помощью мы инициализируем хранилище. А также
    `Provider` из `react-redux`. Он упростит применение `redux` в React.

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import {createStore} from 'redux';
+import {Provider} from 'react-redux';
 import App from './components/app/app';
 import questions from './mocks/questions';
+import {reducer} from './store/reducer';
 
 const Setting = {
   ERRORS_COUNT: 3
 };
 
+const store = createStore(reducer);
+
 ReactDOM.render(
-    <App
-      errorsCount={Setting.ERRORS_COUNT}
-      questions={questions}
-    />,
+    <Provider store={store}>
+      <App
+        errorsCount={Setting.ERRORS_COUNT}
+        questions={questions}
+      />
+    </Provider>,
     document.querySelector(`#root`)
 );
