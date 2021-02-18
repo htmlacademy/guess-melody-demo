4a582cb26f48930e584c7c112750af23735f2ca0
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-18 13:58:44 +0300

    5.13. Добавит проверку на допустимое количество ошибок.

--- project/src/store/reducer.js
+++ project/src/store/reducer.js
@@ -1,23 +1,37 @@
 import {ActionType} from './action';
-import {FIRST_GAME_STEP} from '../const';
+import questions from '../mocks/questions';
+import {FIRST_GAME_STEP, MAX_MISTAKE_COUNT} from '../const';
 
 const initialState = {
   mistakes: 0,
   step: FIRST_GAME_STEP,
+  questions,
 };
 
 const reducer = (state = initialState, action) => {
   switch (action.type) {
-    case ActionType.INCREMENT_STEP:
+    case ActionType.INCREMENT_STEP: {
+      const nextStep = state.step + action.payload;
+
       return {
         ...state,
-        step: state.step + action.payload,
+        step: nextStep,
       };
-    case ActionType.INCREMENT_MISTAKES:
+    }
+    case ActionType.INCREMENT_MISTAKES: {
+      const mistakes = state.mistakes + action.payload;
+
+      if (mistakes >= MAX_MISTAKE_COUNT) {
+        return {
+          ...initialState,
+        };
+      }
+
       return {
         ...state,
         mistakes: state.mistakes + action.payload,
       };
+    }
     case ActionType.RESET_GAME:
       return {
         ...initialState,
