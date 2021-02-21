df532d2de5b7a2852da21dce077e105677072363
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-21 23:08:53 +0300

    6.3. Сконфигурирует пакет axios.
    Для отправки сетевых запросов воспользуемся пакетом `axios`.
    Он уже установлен. Создадим отдельный модуль и опишем в нём функцию
    `createAPI`. В ней сконфигурируем экземпляр `axios`.

---
+++ project/src/services/api.js
@@ -0,0 +1,31 @@
+import axios from 'axios';
+
+const BACKEND_URL = 'https://7.react.pages.academy/guess-melody';
+const REQUEST_TIMEOUT = 5000;
+
+const HttpCode = {
+  UNAUTHORIZED: 401,
+};
+
+export const createAPI = (onUnauthorized) => {
+  const api = axios.create({
+    baseURL: BACKEND_URL,
+    timeout: REQUEST_TIMEOUT,
+  });
+
+  const onSuccess = (response) => response;
+
+  const onFail = (err) => {
+    const {response} = err;
+
+    if (response.status === HttpCode.UNAUTHORIZED) {
+      onUnauthorized();
+    }
+
+    throw err;
+  };
+
+  api.interceptors.response.use(onSuccess, onFail);
+
+  return api;
+};
