3f43f154ab75ec2218af3fa004a080f638d1f236
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-21 23:04:00 +0300

    6.2. Добавит условие для перенаправления при достижении максимального количества ошибок.
    При достижении максимального количества ошибок необходимо
    выполнить перенаправление на страницу проигрыша. Добавим проверку в
    компоненте `GameScreen`. Если количество ошибок превышено — выполним
    перенаправление с помощью компонента `Redirect`.

--- src/components/game-screen/game-screen.jsx
+++ src/components/game-screen/game-screen.jsx
@@ -3,7 +3,7 @@ import PropTypes from 'prop-types';
 import {Redirect} from 'react-router-dom';
 import {connect} from 'react-redux';
 import {ActionCreator} from '../../store/action';
-import {GameType} from '../../const';
+import {GameType, MAX_MISTAKE_COUNT} from '../../const';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
 import artistQuestionProp from "../artist-question-screen/artist-question.prop";
@@ -19,9 +19,15 @@ const GameScreen = (props) => {
   const {questions, step, onUserAnswer, mistakes} = props;
   const question = questions[step];
 
+  if (mistakes >= MAX_MISTAKE_COUNT) {
+    return (
+      <Redirect to="/lose" />
+    );
+  }
+
   if (step >= questions.length || !question) {
     return (
-      <Redirect to="/" />
+      <Redirect to="/result" />
     );
   }
 
--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,6 +1,6 @@
 import {ActionType} from './action';
 import questions from '../mocks/questions';
-import {FIRST_GAME_STEP, MAX_MISTAKE_COUNT} from '../const';
+import {FIRST_GAME_STEP} from '../const';
 
 const initialState = {
   mistakes: 0,
@@ -19,14 +19,6 @@ const reducer = (state = initialState, action) => {
       };
 
     case ActionType.INCREMENT_MISTAKES:
-      const mistakes = state.mistakes + action.payload;
-
-      if (mistakes >= MAX_MISTAKE_COUNT) {
-        return {
-          ...initialState
-        };
-      }
-
       return {
         ...state,
         mistakes: state.mistakes + action.payload
