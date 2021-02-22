20852d9ad0e578b45da129501b7cd9eef6298ef6
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-22 10:53:56 +0300

    6.7. Добавит асинхронные действия.
    Создадим отдельный модуль в котором опишем асинхронные действия. В этих
    действиях будем выполнять запросы к серверу.
    
    На данном этапе нам потребуются следующие действия: для загрузки списка
    вопросов, проверки наличия авторизации и отправки данных для прохождения
    аутентификации.

---
+++ src/store/api-actions.js
@@ -0,0 +1,18 @@
+import {ActionCreator} from "./action";
+import {AuthorizationStatus} from "../const";
+
+export const fetchQuestionList = () => (dispatch, _getState, api) => (
+  api.get(`/questions`)
+    .then(({data}) => dispatch(ActionCreator.loadQuestions(data)))
+);
+
+export const checkAuth = () => (dispatch, _getState, api) => (
+  api.get(`/login`)
+    .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
+    .catch(() => {})
+);
+
+export const login = ({login: email, password}) => (dispatch, _getState, api) => (
+  api.post(`/login`, {email, password})
+    .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
+);
