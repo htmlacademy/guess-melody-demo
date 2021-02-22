096373c3aa04e8ffef82e7db602f8bf6bf377606
Igor Antonov <antonov.igor.khv@gmail.com>
2021-02-22 12:35:47 +0300

    6.10. Добавит компонент приватного маршрута.
    Перехватчики (интерсепторы), которые мы описали в одном из прошлых шагов,
    решают проблему, перенаправления, когда запрос был отправлен: если сервер
    вернул `401`, то они выполнят перенаправление на маршруту `/login`.
    
    Но как быть, если необходимо решить эту же задачу на клиенте? Для этого
    нам потребуется реализовать компонент для описания приватных маршрутов, а
    затем воспользоваться им для всех маршрутов, доступ к которым возможен
    после предварительной авторизации.

---
+++ src/components/private-route/private-route.jsx
@@ -0,0 +1,37 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import {Route, Redirect} from 'react-router-dom';
+import {connect} from 'react-redux';
+import {AuthorizationStatus} from '../../const';
+
+
+const PrivateRoute = ({render, path, exact, authorizationStatus}) => {
+  return (
+    <Route
+      path={path}
+      exact={exact}
+      render={(routeProps) => {
+        return (
+          authorizationStatus === AuthorizationStatus.AUTH
+            ? render(routeProps)
+            : <Redirect to={`/login`} />
+        );
+      }}
+    />
+  );
+};
+
+PrivateRoute.propTypes = {
+  authorizationStatus: PropTypes.string.isRequired,
+  exact: PropTypes.bool.isRequired,
+  path: PropTypes.string.isRequired,
+  render: PropTypes.func.isRequired,
+};
+
+const mapStateToProps = (state) => ({
+  authorizationStatus: state.authorizationStatus,
+});
+
+
+export {PrivateRoute};
+export default connect(mapStateToProps)(PrivateRoute);
