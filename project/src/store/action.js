f02b97da7054f81c5e1a1b8d1a13d3c4aa13fc26
Igor Antonov <antonov.igor.khv@gmail.com>
2021-03-01 13:24:04 +0300

    7.5. Рефакторит действия (actions). WIP
    Паттерн ActionCreator не обязывает держать все действия в одном в
    объекте. Для небольших приложений это удобно, но если действий много,
    то работать с таким объектом становится сложней. Банально неудобно
    каждый раз импортировать объект со всеми действиями.
    
    Разобьём их на отдельные функции. Если действий станет ещё больше,
    то эти функции легко разбросать по отдельным модулям. Сами
    модули создать в соответствии с предметной областью действий.
    
    WIP. Проект не работает. Действия отрефакторили, но не изменили
    обращение к действиям в других частях проекта.

--- project/src/store/action.js
+++ project/src/store/action.js
@@ -11,46 +11,50 @@ export const ActionType = {
   REDIRECT_TO_ROUTE: 'game/redirectToRoute',
 };
 
-export const ActionCreator = {
-  incrementStep: () => ({
-    type: ActionType.INCREMENT_STEP,
-    payload: 1,
-  }),
-  resetGame: () => ({
-    type: ActionType.RESET_GAME,
-  }),
-  incrementMistake: (question, userAnswer) => {
-    let answerIsCorrect;
+export const incrementStep = () => ({
+  type: ActionType.INCREMENT_STEP,
+  payload: 1,
+});
 
-    switch (question.type) {
-      case GameType.ARTIST:
-        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
-        break;
-      case GameType.GENRE:
-        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
-        break;
-      default:
-        answerIsCorrect = false;
-    }
+export const resetGame = () => ({
+  type: ActionType.RESET_GAME,
+});
 
-    return {
-      type: ActionType.INCREMENT_MISTAKES,
-      payload: answerIsCorrect ? 0 : 1,
-    };
-  },
-  loadQuestions: (questions) => ({
-    type: ActionType.LOAD_QUESTIONS,
-    payload: questions,
-  }),
-  requireAuthorization: (status) => ({
-    type: ActionType.REQUIRED_AUTHORIZATION,
-    payload: status,
-  }),
-  logout: () => ({
-    type: ActionType.LOGOUT,
-  }),
-  redirectToRoute: (url) => ({
-    type: ActionType.REDIRECT_TO_ROUTE,
-    payload: url,
-  }),
+export const incrementMistake = (question, userAnswer) => {
+  let answerIsCorrect;
+
+  switch (question.type) {
+    case GameType.ARTIST:
+      answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+      break;
+    case GameType.GENRE:
+      answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+      break;
+    default:
+      answerIsCorrect = false;
+  }
+
+  return {
+    type: ActionType.INCREMENT_MISTAKES,
+    payload: answerIsCorrect ? 0 : 1,
+  };
 };
+
+export const loadQuestions = (questions) => ({
+  type: ActionType.LOAD_QUESTIONS,
+  payload: questions,
+});
+
+export const requireAuthorization = (status) => ({
+  type: ActionType.REQUIRED_AUTHORIZATION,
+  payload: status,
+});
+
+export const logout = () => ({
+  type: ActionType.LOGOUT,
+});
+
+export const redirectToRoute = (url) => ({
+  type: ActionType.REDIRECT_TO_ROUTE,
+  payload: url,
+});
--- project/src/store/api-actions.js
+++ project/src/store/api-actions.js
@@ -1,26 +1,26 @@
-import {ActionCreator} from './action';
+import {loadQuestions, requireAuthorization, logout as closeSession, redirectToRoute} from './action';
 import {AuthorizationStatus, AppRoute, APIRoute} from '../const';
 
 export const fetchQuestionList = () => (dispatch, _getState, api) => (
   api.get(APIRoute.QUESTIONS)
-    .then(({data}) => dispatch(ActionCreator.loadQuestions(data)))
+    .then(({data}) => dispatch(loadQuestions(data)))
 );
 
 export const checkAuth = () => (dispatch, _getState, api) => (
   api.get(APIRoute.LOGIN)
-    .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
+    .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
     .catch(() => {})
 );
 
 export const login = ({login: email, password}) => (dispatch, _getState, api) => (
   api.post(APIRoute.LOGIN, {email, password})
     .then(({data}) => localStorage.setItem('token', data.token))
-    .then(() => dispatch(ActionCreator.requireAuthorization(AuthorizationStatus.AUTH)))
-    .then(() => dispatch(ActionCreator.redirectToRoute(AppRoute.RESULT)))
+    .then(() => dispatch(requireAuthorization(AuthorizationStatus.AUTH)))
+    .then(() => dispatch(redirectToRoute(AppRoute.RESULT)))
 );
 
 export const logout = () => (dispatch, _getState, api) => (
   api.delete(APIRoute.LOGOUT)
     .then(() => localStorage.removeItem('token'))
-    .then(() => dispatch(ActionCreator.logout()))
+    .then(() => dispatch(closeSession()))
 );
