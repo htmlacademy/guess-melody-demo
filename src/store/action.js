9fda06587789366f00c22dd2aaea8dcc4bf6bb08
Igor Antonov <antonov.igor.khv@gmail.com>
2021-03-01 13:24:04 +0300

    7.5. Рефакторит действия (actions). WIP
    Паттерн ActionCreator не обязывает держать все действия в одном в
    объекте. Для небольших приложений это удобно, но если действий много,
    то работать с таким объектом становится сложней. Банально неудобно
    каждый раз импортировать объект со всеми действиями.
    
    Разобьём их на отдельные функции. Если действий станет ещё больше,
    то эти функции легко разбросать по отдельным модулям. Сами
    модули создать в соответствии с предметной областью действий.
    
    WIP. Проект не работает. Действия отрефакторили, но не изменили
    обращение к действиям в других частях проекта.

--- src/store/action.js
+++ src/store/action.js
@@ -10,41 +10,44 @@ export const ActionType = {
   REQUIRED_AUTHORIZATION: `user/requiredAuthorization`,
 };
 
-export const ActionCreator = {
-  incrementStep: () => ({
-    type: ActionType.INCREMENT_STEP,
-    payload: 1,
-  }),
-  resetGame: () => ({
-    type: ActionType.RESET_GAME,
-  }),
-  incrementMistake: (question, userAnswer) => {
-    let answerIsCorrect = false;
+export const incrementStep = () => ({
+  type: ActionType.INCREMENT_STEP,
+  payload: 1,
+});
 
-    switch (question.type) {
-      case GameType.ARTIST:
-        answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
-        break;
-      case GameType.GENRE:
-        answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
-        break;
-    }
+export const resetGame = () => ({
+  type: ActionType.RESET_GAME,
+});
 
-    return {
-      type: ActionType.INCREMENT_MISTAKES,
-      payload: answerIsCorrect ? 0 : 1,
-    };
-  },
-  loadQuestions: (questions) => ({
-    type: ActionType.LOAD_QUESTIONS,
-    payload: questions
-  }),
-  requireAuthorization: (status) => ({
-    type: ActionType.REQUIRED_AUTHORIZATION,
-    payload: status,
-  }),
-  redirectToRoute: (url) => ({
-    type: ActionType.REDIRECT_TO_ROUTE,
-    payload: url,
-  })
+export const incrementMistake = (question, userAnswer) => {
+  let answerIsCorrect = false;
+
+  switch (question.type) {
+    case GameType.ARTIST:
+      answerIsCorrect = isArtistAnswerCorrect(question, userAnswer);
+      break;
+    case GameType.GENRE:
+      answerIsCorrect = isGenreAnswerCorrect(question, userAnswer);
+      break;
+  }
+
+  return {
+    type: ActionType.INCREMENT_MISTAKES,
+    payload: answerIsCorrect ? 0 : 1,
+  };
 };
+
+export const loadQuestions = (questions) => ({
+  type: ActionType.LOAD_QUESTIONS,
+  payload: questions
+});
+
+export const requireAuthorization = (status) => ({
+  type: ActionType.REQUIRED_AUTHORIZATION,
+  payload: status,
+});
+
+export const redirectToRoute = (url) => ({
+  type: ActionType.REDIRECT_TO_ROUTE,
+  payload: url,
+});
