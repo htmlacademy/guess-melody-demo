c0b61fa26195527d37832f83b92cb192193d5c08
Igor Antonov <antonov.igor.khv@gmail.com>
2021-03-01 13:37:03 +0300

    7.7. Рефакторит редьюсер. WIP
    Чем больше действий, тем не поворотливей становится редьюсер. В
    демо-проекте действий немного, но редьюсер всё равно стал разрастаться.
    
    Можно продолжать поддерживать один редьюсер, превратив его в
    универсальный редьюсер, но это плохая идея. Рано или поздно в редьюсере
    станет трудно ориентироваться. Лучше разбить его в соответствии
    с предметной области. Таким образом получим несколько редьюсеров,
    а потом объединим их с помощью `combineReducers`.
    
    WIP: Приложение не работает, из-за рефакторинга редьюсера. Редьюсер
    перестроен, нужно обновить компоненты, которые подключены к хранилищу.

---
+++ project/src/store/user/user.js
@@ -0,0 +1,25 @@
+import {ActionType} from '../action';
+import {AuthorizationStatus} from '../../const';
+
+const initialState = {
+  authorizationStatus: AuthorizationStatus.UNKNOWN,
+};
+
+const user = (state = initialState, action) => {
+  switch (action.type) {
+    case ActionType.REQUIRED_AUTHORIZATION:
+      return {
+        ...state,
+        authorizationStatus: action.payload,
+      };
+    case ActionType.LOGOUT:
+      return {
+        ...state,
+        authorizationStatus: AuthorizationStatus.NO_AUTH,
+      };
+    default:
+      return state;
+  }
+};
+
+export {user};
