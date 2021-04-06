AlexBizPlus/guess-melody-5

8.10. Добавит тесты для редьюсеров и асинхронных операций.
Тестировать можно не только компоненты, но и редьюсеры, действия,
асинхронные операции и так далее.

При тестировании редьюсеров мы должны убедиться, что диспатч действия
приводит к ожидаемому обновлению хранилища.

С асинхронными операциями немного сложней. Мы должны замокать работу с
сервером. Для этого установим дополнительный пакет для `axios`. При
тестировании асинхронных операций мы должны убедиться, что при успешной
обработке запроса, будут выполнены соответствующие действия.

src/store/reducers/user/user-reducer.test.js
@@ -1,0 +1,73 @@
+import MockAdapter from "axios-mock-adapter";
+import {createAPI} from "../../../services/api";
+import {user} from "./user";
+import {ActionType} from "../../action";
+import {checkAuth, login} from "../../api-actions";
+import {APIRoute, AppRoute} from "../../../const";
+import {AuthorizationStatus} from "../../../const";
+
+const api = createAPI(() => {});
+
+it(`Reducer without additional parameters should return initial state`, () => {
+  expect(user(void 0, {})).toEqual({
+    authorizationStatus: AuthorizationStatus.NO_AUTH,
+  });
+});
+
+it(`Reducer should update authorizationStatus to "auth"`, () => {
+  expect(user({
+    authorizationStatus: AuthorizationStatus.NO_AUTH,
+  }, {
+    type: ActionType.REQUIRED_AUTHORIZATION,
+    payload: AuthorizationStatus.AUTH
+  })).toEqual({
+    authorizationStatus: AuthorizationStatus.AUTH
+  });
+});
+
+describe(`Async operation work correctly`, () => {
+  it(`Should make a correct API call to /login`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const questionLoader = checkAuth();
+
+    apiMock
+      .onGet(APIRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return questionLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+      });
+  });
+
+  it(`Should make a correct API call to /login`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeUser = {login: `test@test.ru`, password: `123456`};
+    const questionLoader = login(fakeUser);
+
+    apiMock
+      .onPost(APIRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return questionLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(2);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: ActionType.REDIRECT_TO_ROUTE,
+          payload: AppRoute.RESULT,
+        });
+
+      });
+  });
+
+});

htmlacademy/guess-melody-demo

src/store/reducers/user/user-reducer.test.js
@@ -1,0 +1,73 @@
+import MockAdapter from "axios-mock-adapter";
+import {createAPI} from "../../../services/api";
+import {user} from "./user";
+import {ActionType} from "../../action";
+import {checkAuth, login} from "../../api-actions";
+import {APIRoute, AppRoute} from "../../../const";
+import {AuthorizationStatus} from "../../../const";
+
+const api = createAPI(() => {});
+
+it(`Reducer without additional parameters should return initial state`, () => {
+  expect(user(void 0, {})).toEqual({
+    authorizationStatus: AuthorizationStatus.NO_AUTH,
+  });
+});
+
+it(`Reducer should update authorizationStatus to "auth"`, () => {
+  expect(user({
+    authorizationStatus: AuthorizationStatus.NO_AUTH,
+  }, {
+    type: ActionType.REQUIRED_AUTHORIZATION,
+    payload: AuthorizationStatus.AUTH
+  })).toEqual({
+    authorizationStatus: AuthorizationStatus.AUTH
+  });
+});
+
+describe(`Async operation work correctly`, () => {
+  it(`Should make a correct API call to /login`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const checkAuthLoader = checkAuth();
+
+    apiMock
+      .onGet(APIRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return checkAuthLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+      });
+  });
+
+  it(`Should make a correct API call to /login`, () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeUser = {email: `test@test.ru`, password: `123456`};
+    const loginLoader = login(fakeUser);
+
+    apiMock
+      .onPost(APIRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return loginLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(2);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: ActionType.REDIRECT_TO_ROUTE,
+          payload: AppRoute.RESULT,
+        });
+
+      });
+  });
+
+});