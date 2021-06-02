e754eb24ecf9d6c38c95595bc02d1e1df8a5a2da
kam4atka <sledsib@ya.ru>
2021-06-03 01:26:41 +0300

    2.3. Добавит компонент `Logo`.
    В компонентах `ArtistQuestionScreen`, `GenreQuestionScreen` и
    `NotFoundScreen` повторяется блок разметки, отвечающий за вывод логотипа
    приложения. Чтобы избежать дублирования, эту часть разметки можно
    вынести в отдельный компонент `Logo`.

---
+++ project/src/components/logo/logo.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import {Link} from 'react-router-dom';
+
+function Logo() {
+  return (
+    <Link className="game__back" to="/">
+      <span className="visually-hidden">Сыграть ещё раз</span>
+      <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
+    </Link>
+  );
+}
+
+export default Logo;
