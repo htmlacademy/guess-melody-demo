3fcf094bb56348a41eee367a9067dbf9230ba60f
kam4atka <sledsib@ya.ru>
2021-06-07 13:32:30 +0300

    3.4. Создаст отдельные модули для описания валидации пропсов.
    В модуле `const.js` опишем константу `GameType` (перечисление) для
    хранения типа игры.
    
    Создадим отдельные модули для компонентов `GenreQuestionScreen` и
    `ArtistQuestionScreen`, и добавим в них описание пропсов.

---
+++ project/src/components/artist-question-screen/artist-question.prop.js
@@ -0,0 +1,14 @@
+import PropTypes from 'prop-types';
+import {GameType} from '../../const';
+
+export default PropTypes.shape({
+  answers: PropTypes.arrayOf(PropTypes.shape({
+    artist: PropTypes.string.isRequired,
+    picture: PropTypes.string.isRequired,
+  })).isRequired,
+  song: PropTypes.shape({
+    artist: PropTypes.string.isRequired,
+    src: PropTypes.string.isRequired,
+  }).isRequired,
+  type: PropTypes.oneOf([GameType.ARTIST]).isRequired,
+}).isRequired;
--- project/src/const.js
+++ project/src/const.js
@@ -1,3 +1,8 @@
+export const GameType = {
+  ARTIST: 'artist',
+  GENRE: 'genre',
+};
+
 export const AppRoute = {
   LOGIN: '/login',
   LOSE: '/lose',
