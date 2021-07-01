49ca56016bcf9c6225e59f9ec6cf91bad230c60b
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-01 12:30:31 +0300

    9.9. Добавит тесты для асинхронных операций.
    Аналогичным образом добавим недостающие тесты для асинхронных операций.

--- project/src/store/api-actions.test.ts
+++ project/src/store/api-actions.test.ts
@@ -3,10 +3,12 @@ import thunk, {ThunkDispatch} from 'redux-thunk';
 import MockAdapter from 'axios-mock-adapter';
 import {configureMockStore} from '@jedmao/redux-mock-store';
 import {createAPI} from '../services/api';
-import {checkAuthAction} from './api-actions';
-import {APIRoute, AuthorizationStatus} from '../const';
+import {checkAuthAction, loginAction, fetchQuestionAction, logoutAction} from './api-actions';
+import {APIRoute, AppRoute, AuthorizationStatus} from '../const';
 import {State} from '../types/state';
-import {requireAuthorization} from './action';
+import {requireAuthorization, redirectToRoute, loadQuestions, requireLogout} from './action';
+import {AuthData} from '../types/auth-data';
+import {makeFakeGenreQuestion, makeFakeArtistQuestion} from '../utils/mocks';
 
 describe('Async actions', () => {
   const onFakeUnauthorized = jest.fn();
@@ -33,5 +35,54 @@ describe('Async actions', () => {
     expect(store.getActions()).toEqual([
       requireAuthorization(AuthorizationStatus.Auth),
     ]);
+  });
+
+  it('should dispatch RequriedAuthorization and RedirectToRoute when POST /login', async () => {
+    const fakeUser: AuthData = {login: 'test@test.ru', password: '123456'};
+    mockAPI
+      .onPost(APIRoute.Login)
+      .reply(200, {token: 'secret'});
+
+    const store = mockStore();
+    Storage.prototype.setItem = jest.fn();
+
+    await store.dispatch(loginAction(fakeUser));
+
+    expect(store.getActions()).toEqual([
+      requireAuthorization(AuthorizationStatus.Auth),
+      redirectToRoute(AppRoute.Result),
+    ]);
+
+    expect(Storage.prototype.setItem).toBeCalledTimes(1);
+    expect(Storage.prototype.setItem).toBeCalledWith('guess-melody-token', 'secret');
+  });
+
+  it('should dispatch Load_Questions when GET /questions', async () => {
+    const mockQuestions = [makeFakeArtistQuestion(), makeFakeGenreQuestion()];
+    mockAPI
+      .onGet(APIRoute.Questions)
+      .reply(200, mockQuestions);
+
+    const store = mockStore();
+    await store.dispatch(fetchQuestionAction());
+
+    expect(store.getActions()).toEqual([
+      loadQuestions(mockQuestions),
+    ]);
+  });
+
+  it('should dispatch Logout when Delete /logout', async () => {
+    mockAPI
+      .onDelete(APIRoute.Logout)
+      .reply(204);
+
+    const store = mockStore();
+    Storage.prototype.removeItem = jest.fn();
+
+    await store.dispatch(logoutAction());
+
+    expect(store.getActions()).toEqual([requireLogout()]);
+    expect(Storage.prototype.removeItem).toBeCalledTimes(1);
+    expect(Storage.prototype.removeItem).toBeCalledWith('guess-melody-token');
   });
 });
