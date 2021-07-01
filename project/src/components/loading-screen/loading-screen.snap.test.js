df0d9c79d22c909e620c00870d4b3770a0c23e04
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-01 13:19:22 +0300

    8.5. Добавит примеры снепшот-тестов.
    Один из способов протестировать React-компоненты — снепшоты. Снепшоты
    содержат результат отрисовки компонента (разметку). При запуске тестов
    выполняется сравнение. Если разметка компонента отличается от
    сохранённой, тест упадёт.

---
+++ project/src/components/loading-screen/loading-screen.snap.test.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import {render} from '@testing-library/react';
+import LoadingScreen from './loading-screen';
+
+describe('Component: LoadingScreen', () => {
+  test('should render correctly', () => {
+    const {container} = render(<LoadingScreen />);
+    expect(container).toMatchSnapshot();
+  });
+});
