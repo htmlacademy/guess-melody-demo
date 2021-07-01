3e7522925abc3184c11284cb7bcb9542e81b8ef2
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-01 12:30:31 +0300

    8.4. Добавит тесты для асинхронных операций.
    В модуле `api-actions.test.js` опишем тесты для асинхронных операций.
    Для написания тестов потребуется воспользоваться `MockAdapter`.
    Настоящие запросы к серверу мы делать не будем.

---
+++ project/src/store/api-actions.test.js
@@ -0,0 +1,77 @@
+import MockAdapter from 'axios-mock-adapter';
+import {createAPI} from '../services/api';
+import {ActionType} from './action';
+import {checkAuth, login, fetchQuestionList} from './api-actions';
+import {APIRoute, AppRoute, AuthorizationStatus} from '../const';
+
+let api = null;
+
+describe('Async operations', () => {
+  beforeAll(() => {
+    api = createAPI(() => {});
+  });
+
+  it('should make a correct API call to GET /login', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const checkAuthLoader = checkAuth();
+
+    apiMock
+      .onGet(APIRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return checkAuthLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+      });
+  });
+
+  it('should make a correct API call to POST /login', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const fakeUser = {email: 'test@test.ru', password: '123456'};
+    const loginLoader = login(fakeUser);
+
+    apiMock
+      .onPost(APIRoute.LOGIN)
+      .reply(200, [{fake: true}]);
+
+    return loginLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(2);
+
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.REQUIRED_AUTHORIZATION,
+          payload: AuthorizationStatus.AUTH,
+        });
+
+        expect(dispatch).toHaveBeenNthCalledWith(2, {
+          type: ActionType.REDIRECT_TO_ROUTE,
+          payload: AppRoute.RESULT,
+        });
+      });
+  });
+
+  it('should make a correct API call to GET /questions', () => {
+    const apiMock = new MockAdapter(api);
+    const dispatch = jest.fn();
+    const questionLoader = fetchQuestionList();
+
+    apiMock
+      .onGet(APIRoute.QUESTIONS)
+      .reply(200, [{fake: true}]);
+
+    return questionLoader(dispatch, () => {}, api)
+      .then(() => {
+        expect(dispatch).toHaveBeenCalledTimes(1);
+        expect(dispatch).toHaveBeenNthCalledWith(1, {
+          type: ActionType.LOAD_QUESTIONS,
+          payload: [{fake: true}],
+        });
+      });
+  });
+});
