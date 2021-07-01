6790f504994085cc83a23c6368e939542501c072
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-01 14:11:27 +0300

    9.11. Добавит пример теста компонента без использования снепшотов.
    Тестирование снепшотами — не единственный вариант. Можно проверять,
    что в результате отрисовки компонента, действительно были созданы
    соответствующие элементы интерфейса и пользователь может с ними
    взаимодействовать.
    
    Рассмотрим на примере компонента «NotFoundScreen». Компонент работает
    правильно, если пользователь видит текст «404. Page not found».

---
+++ project/src/pages/not-found-screen/not-found-screen.test.tsx
@@ -0,0 +1,25 @@
+import {render, screen} from '@testing-library/react';
+import {HelmetProvider} from 'react-helmet-async';
+import {createMemoryHistory} from 'history';
+import HistoryRouter from '../../components/history-route/history-route';
+import NotFoundScreen from './not-found-screen';
+
+describe('Component: NotFoundScreen', () => {
+  it('should render correctly', () => {
+    const history = createMemoryHistory();
+
+    render(
+      <HistoryRouter history={history}>
+        <HelmetProvider>
+          <NotFoundScreen />
+        </HelmetProvider>
+      </HistoryRouter>,
+    );
+
+    const headerElement = screen.getByText('404. Page not found');
+    const linkElement = screen.getByText('Вернуться на главную');
+
+    expect(headerElement).toBeInTheDocument();
+    expect(linkElement).toBeInTheDocument();
+  });
+});
