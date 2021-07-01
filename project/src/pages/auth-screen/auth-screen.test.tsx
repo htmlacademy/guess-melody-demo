38e3430e584ab40e147053a6a83e829543e8f2ea
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-01 14:33:32 +0300

    9.12. Добавит пример теста для компонента подключенного к хранилищу.
    Рассмотрим более сложный тест. Некоторые компоненты подключены к
    хранилищу. Нам нужно это учесть при тестировании, и не забыть обернуть
    проверяемый компонент в Provider.  Помимо этого, также потребуется
    замокать хранилище. Для этого воспользуемся пакетом `redux-mock-store`.

---
+++ project/src/pages/auth-screen/auth-screen.test.tsx
@@ -0,0 +1,38 @@
+import {render, screen} from '@testing-library/react';
+import {HelmetProvider} from 'react-helmet-async';
+import {createMemoryHistory} from 'history';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {Provider} from 'react-redux';
+import userEvent from '@testing-library/user-event';
+import HistoryRouter from '../../components/history-route/history-route';
+import AuthScreen from './auth-screen';
+
+const mockStore = configureMockStore();
+
+describe('Component: AuthScreen', () => {
+  it('should render "AuthScreen" when user navigate to "login" url', async () => {
+    const history = createMemoryHistory();
+    history.push('/login');
+
+    render(
+      <Provider store={mockStore({})}>
+        <HistoryRouter history={history}>
+          <HelmetProvider>
+            <AuthScreen />
+          </HelmetProvider>
+        </HistoryRouter>
+      </Provider>,
+    );
+
+    expect(screen.getByText(/Сыграть ещё раз/i)).toBeInTheDocument();
+    expect(screen.getByText(/Хотите узнать свой результат\? Представьтесь!/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Логин/i)).toBeInTheDocument();
+    expect(screen.getByLabelText(/Пароль/i)).toBeInTheDocument();
+
+    await userEvent.type(screen.getByTestId('login'), 'keks');
+    await userEvent.type(screen.getByTestId('password'), '123456');
+
+    expect(screen.getByDisplayValue(/keks/i)).toBeInTheDocument();
+    expect(screen.getByDisplayValue(/123456/i)).toBeInTheDocument();
+  });
+});
