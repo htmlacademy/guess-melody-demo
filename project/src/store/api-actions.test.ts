f2e814f8ab805a04d34a552872cdf001ae4aa425
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-01 12:30:31 +0300

    9.9. Добавит тесты для асинхронных операций.
    Аналогичным образом добавим недостающие тесты для асинхронных операций.

--- project/src/store/api-actions.test.ts
+++ project/src/store/api-actions.test.ts
@@ -3,9 +3,12 @@ import thunk, {ThunkDispatch} from 'redux-thunk';
 import MockAdapter from 'axios-mock-adapter';
 import {configureMockStore} from '@jedmao/redux-mock-store';
 import {createAPI} from '../services/api';
-import {checkAuthAction} from './api-actions';
+import {checkAuthAction, loginAction, fetchQuestionAction, logoutAction} from './api-actions';
 import {APIRoute} from '../const';
 import {State} from '../types/state';
+import {AuthData} from '../types/auth-data';
+import {makeFakeGenreQuestion, makeFakeArtistQuestion} from '../utils/mocks';
+import {redirectToRoute} from './action';
 
 describe('Async actions', () => {
   const api = createAPI();
@@ -34,5 +37,69 @@ describe('Async actions', () => {
       checkAuthAction.pending.type,
       checkAuthAction.fulfilled.type
     ]);
+  });
+
+  it('should dispatch RequriedAuthorization and RedirectToRoute when POST /login', async () => {
+    const fakeUser: AuthData = {login: 'test@test.ru', password: '123456'};
+
+    mockAPI
+      .onPost(APIRoute.Login)
+      .reply(200, {token: 'secret'});
+
+
+    const store = mockStore();
+    Storage.prototype.setItem = jest.fn();
+
+    await store.dispatch(loginAction(fakeUser));
+
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toEqual([
+      loginAction.pending.type,
+      redirectToRoute.type,
+      loginAction.fulfilled.type
+    ]);
+
+    expect(Storage.prototype.setItem).toBeCalledTimes(1);
+    expect(Storage.prototype.setItem).toBeCalledWith('guess-melody-token', 'secret');
+  });
+
+  it('should dispatch Load_Questions when GET /questions', async () => {
+    const mockQuestions = [makeFakeArtistQuestion(), makeFakeGenreQuestion()];
+    mockAPI
+      .onGet(APIRoute.Questions)
+      .reply(200, mockQuestions);
+
+    const store = mockStore();
+
+    await store.dispatch(fetchQuestionAction());
+
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toEqual([
+      fetchQuestionAction.pending.type,
+      fetchQuestionAction.fulfilled.type
+    ]);
+  });
+
+  it('should dispatch Logout when Delete /logout', async () => {
+    mockAPI
+      .onDelete(APIRoute.Logout)
+      .reply(204);
+
+    const store = mockStore();
+    Storage.prototype.removeItem = jest.fn();
+
+    await store.dispatch(logoutAction());
+
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toEqual([
+      logoutAction.pending.type,
+      logoutAction.fulfilled.type
+    ]);
+
+    expect(Storage.prototype.removeItem).toBeCalledTimes(1);
+    expect(Storage.prototype.removeItem).toBeCalledWith('guess-melody-token');
   });
 });
