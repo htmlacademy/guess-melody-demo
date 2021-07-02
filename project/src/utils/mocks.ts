ded53039907c1bd7e88dfb06b6738f578903b46a
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-02 18:12:22 +0300

    9.4. Добавит тесты для isGenreAnswerCorrect.
    Аналогичным образом протестируем `isGenreAnswerCorrect`.
    
    Специально для этого добавим функцию `makeFakeGenreQuestion`.

--- project/src/utils/mocks.ts
+++ project/src/utils/mocks.ts
@@ -1,6 +1,6 @@
-import {system, name, internet} from 'faker';
+import {music, system, name, internet} from 'faker';
 import {GameType} from '../const';
-import {QuestionArtist} from '../types/question';
+import {QuestionArtist, QuestionGenre} from '../types/question';
 
 export const makeFakeArtistQuestion = (): QuestionArtist => ({
   type: GameType.Artist,
@@ -12,3 +12,11 @@ export const makeFakeArtistQuestion = (): QuestionArtist => ({
     { picture: internet.avatar(), artist: name.title() }
   )),
 } as QuestionArtist);
+
+export const makeFakeGenreQuestion = (): QuestionGenre => ({
+  type: GameType.Genre,
+  genre: music.genre(),
+  answers: new Array(4).fill(null).map(() => (
+    { src: system.filePath(), genre: music.genre() }),
+  ),
+} as QuestionGenre);
