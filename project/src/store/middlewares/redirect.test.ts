238972fc171977ea4957fbf0fd6823930a60b64a
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-02 17:17:22 +0300

    9.7. Добавит тест для middleware.
    Для тестирования middleware необходимо подготовить окружение. В данном
    контексте под окружением подразумеваются моки для имитации методов
    хранилища: `dispatch`, `getState`, а также функция для вызова `middleware`.
    
    Для решения этой задачи опишем функцию `mockRedux`. Она вернёт объект
    со всеми необходимыми методами, включая функции `next` и `invoke`.
    Последняя потребуется для вызова `middleware`.
    
    Тестовых кейсов будет три. В первом убедимся, что функция `next` вызывается
    с аргументом `action` (объект с описанием действия).
    
    Во втором кейсе проверим, что middleware использует `history` и вызывает
    метод `push`. При вызове метод `push` должен измениться
    `location.pathname`. Настоящего объекта `history` у нас нет, поэтому
    замокаем модуль `browser-history.js`. Вместо сконфигурированного
    `history` воспользуемся фиктивной реализацией (см. `fakeHistory`).
    
    В третьем кейсе убедимся, что редирект не сработает, если передать
    неправильный объект с действием.

---
+++ project/src/store/middlewares/redirect.test.ts
@@ -0,0 +1,38 @@
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {AnyAction} from 'redux';
+import {redirect} from './redirect';
+import {redirectToRoute} from '../action';
+import {AppRoute} from '../../const';
+import {State} from '../../types/state';
+
+const fakeHistory = {
+  location: {pathname: ''},
+  push(path: string) {
+    this.location.pathname = path;
+  },
+};
+
+jest.mock('../../browser-history', () => fakeHistory);
+
+const middlewares = [redirect];
+const mockStore = configureMockStore<State, AnyAction>(middlewares);
+const store = mockStore();
+
+describe('Middleware: redirect', () => {
+  beforeEach(() => {
+    fakeHistory.push('');
+  });
+
+  it('should be redirect to /login', () => {
+    store.dispatch(redirectToRoute(AppRoute.Login));
+    expect(fakeHistory.location.pathname).toBe(AppRoute.Login);
+    expect(store.getActions()).toEqual([
+      redirectToRoute(AppRoute.Login),
+    ]);
+  });
+
+  it('should not to be redirect /lose because bad action', () => {
+    store.dispatch({type: 'UNKNOWN_ACTION', payload: AppRoute.Lose});
+    expect(fakeHistory.location.pathname).not.toBe(AppRoute.Lose);
+  });
+});
