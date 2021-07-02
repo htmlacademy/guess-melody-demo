143b08aa9ca8aeeeb1fde3daa7c13620f8414c01
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-02 18:12:22 +0300

    9.4. Добавит тесты для isGenreAnswerCorrect.
    Аналогичным образом протестируем `isGenreAnswerCorrect`.
    
    Специально для этого добавим функцию `makeFakeGenreQuestion`.

--- project/src/game.test.ts
+++ project/src/game.test.ts
@@ -1,18 +1,37 @@
-import {isArtistAnswerCorrect} from './game';
-import {makeFakeArtistQuestion} from './utils/mocks';
+import {isArtistAnswerCorrect, isGenreAnswerCorrect} from './game';
+import {makeFakeArtistQuestion, makeFakeGenreQuestion} from './utils/mocks';
 
 const mockArtistQuestion = makeFakeArtistQuestion();
+const mockGenreQuestion = makeFakeGenreQuestion();
 
-describe('Function: isArtistAnswerCorrect', () => {
-  it('should return "true" when answer is correct', () => {
-    const {artist: correctAnswer} = mockArtistQuestion.song;
-    expect(isArtistAnswerCorrect(mockArtistQuestion, correctAnswer))
-      .toBe(true);
+describe('Business Logiс: check user\'s answer', () => {
+  describe('Function: isArtistAnswerCorrect', () => {
+    it('should return "true" when answer is correct', () => {
+      const {artist: correctAnswer} = mockArtistQuestion.song;
+      expect(isArtistAnswerCorrect(mockArtistQuestion, correctAnswer))
+        .toBe(true);
+    });
+
+    it('should return "false" when answer is incorrect', () => {
+      const incorrectAnswer = 'unknown';
+      expect(isArtistAnswerCorrect(mockArtistQuestion, incorrectAnswer))
+        .toBe(false);
+    });
   });
 
-  it('should return "false" when answer is incorrect', () => {
-    const incorrectAnswer = 'unknown';
-    expect(isArtistAnswerCorrect(mockArtistQuestion, incorrectAnswer))
-      .toBe(false);
+  describe('Function: isGenreAnswerCorrect', () => {
+    it('should return "true" when answer is correct', () => {
+      const {answers} = mockGenreQuestion;
+      const correctAnswer = answers.map((answer) => answer.genre === mockGenreQuestion.genre);
+      expect(isGenreAnswerCorrect(mockGenreQuestion, correctAnswer))
+        .toBe(true);
+    });
+
+    it('should be return "false" when answer is incorrect', () => {
+      const {answers} = mockGenreQuestion;
+      const incorrectAnswer = answers.map((answer) => answer.genre !== mockGenreQuestion.genre);
+      expect(isGenreAnswerCorrect(mockGenreQuestion, incorrectAnswer))
+        .toBe(false);
+    });
   });
 });
