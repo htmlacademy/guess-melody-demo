d4cd4e46359b9d7f83cfb3100441313fc3649735
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-02 20:05:37 +0300

    9.14. Добавит тест для компонента PrivateRoute.
    Для тестирования компонента `PrivateRoute` вновь потребуется подготовить
    обёртку из `Router` и `Provider`.
    
    Сами тесты максимально простые. Определим два маршрута: публичный и
    приватный. Статус пользователя будем регулировать изменением
    соответствующего поля в хранилище.
    
    В тестовых случаях будем искать текст. Для публичного маршрута
    рендерится заголовок «Public Route», а для приватного «Private Route».
    
    Обратите внимание на применение методов поиска. Мы используем
    `getByText` и `queryByText`.
    
    У `getByText` есть особенность — если он не находит элемент с текстом,
    то возникнет исключение и тест упадёт.
    
    В некоторых случаях такое поведение подходит. Например, если мы не
    нашли элемент с нужным текстом. Но, а что если требуется сделать
    проверку наоборот. Например: убедиться, что отсутствует элемент с
    определённым текстом.
    
    Для таких случаев пригодятся методы начинающиеся с префикса `query`.
    Например, `queryByText`. Если элемент не будет найден, то метод
    вернёт `null`.

---
+++ project/src/components/private-route/private-route.test.tsx
@@ -0,0 +1,60 @@
+import {Router, Route} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import {render, screen} from '@testing-library/react';
+import {Provider} from 'react-redux';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {AuthorizationStatus} from '../../const';
+import PrivateRoute from './private-route';
+
+const mockStore = configureMockStore();
+const history = createMemoryHistory();
+
+describe('Component: PrivateRouter', () => {
+  beforeEach(() => {
+    history.push('/private');
+  });
+
+  it('should render component for public route, when user not authorized', () => {
+    const store = mockStore({
+      USER: {authorizationStatus: AuthorizationStatus.NoAuth},
+    });
+
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Route exact path="/login"><h1>Public Route</h1></Route>
+          <PrivateRoute
+            exact
+            path="/private"
+            render={() => (<h1>Private Route</h1>)}
+          />
+        </Router>
+      </Provider>,
+    );
+
+    expect(screen.getByText(/Public Route/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Private Route/i)).not.toBeInTheDocument();
+  });
+
+  it('should render component for private route, when user authorized', () => {
+    const store = mockStore({
+      USER: {authorizationStatus: AuthorizationStatus.Auth},
+    });
+
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Route exact path="/login"><h1>Public Route</h1></Route>
+          <PrivateRoute
+            exact
+            path="/private"
+            render={() => (<h1>Private Route</h1>)}
+          />
+        </Router>
+      </Provider>,
+    );
+
+    expect(screen.getByText(/Private Route/i)).toBeInTheDocument();
+    expect(screen.queryByText(/Public Route/i)).not.toBeInTheDocument();
+  });
+});
