af311d1c973c640660e197136180411b493ff446
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-03 19:38:18 +0300

    9.18. Добавит тест для компонента Logo.
    Компонент `Logo` тестируется без особых сложностей. Нам нужно убедиться,
    что по клику выполняется переход на главную страницу. Воспользуемся
    уже знакомым подходом: обернём компонент в `Router`. Вместо главной
    страницы сделаем заглушку.

---
+++ project/src/components/logo/logo.test.tsx
@@ -0,0 +1,44 @@
+import {render, screen} from '@testing-library/react';
+import {Routes, Route} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import HistoryRouter from '../history-route/history-route';
+import Logo from './logo';
+import userEvent from '@testing-library/user-event';
+
+const history = createMemoryHistory();
+
+describe('Component: Logo', () => {
+  it('should render correctly', () => {
+    render(
+      <HistoryRouter history={history}>
+        <Logo />
+      </HistoryRouter>);
+
+    expect(screen.getByAltText(/Угадай мелодию/i)).toBeInTheDocument();
+    expect(screen.getByRole('link')).toBeInTheDocument();
+  });
+
+  it('should redirect to root url when user clicked to link', async () => {
+    history.push('/fake');
+
+    render(
+      <HistoryRouter history={history}>
+        <Routes>
+          <Route
+            path="/"
+            element={<h1>This is main page</h1>}
+          />
+          <Route
+            path='*'
+            element={<Logo />}
+          />
+        </Routes>
+      </HistoryRouter>);
+
+    expect(screen.queryByText(/This is main page/i)).not.toBeInTheDocument();
+
+    await userEvent.click(screen.getByRole('link'));
+
+    expect(screen.getByText(/This is main page/i)).toBeInTheDocument();
+  });
+});
