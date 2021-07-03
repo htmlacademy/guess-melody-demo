509e542ab615ffe87da7e3b3a01860b327df4519
Igor Antonov <antonov.igor.khv@gmail.com>
2021-07-03 14:42:41 +0300

    9.7. Добавит тест для компонента WinScreen.
    Аналогичным образом протестируем компонент `WinScreen`. Подход остаётся
    точно таким же как в случае с `GameOverScreen`.

---
+++ project/src/components/win-screen/win-screen.test.js
@@ -0,0 +1,66 @@
+import React from 'react';
+import {render, screen} from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+import {Router, Switch, Route} from 'react-router-dom';
+import {createMemoryHistory} from 'history';
+import configureStore from 'redux-mock-store';
+import {Provider} from 'react-redux';
+import WinScreen from './win-screen';
+import {AppRoute} from '../../const.js';
+
+const mockStore = configureStore({});
+let history;
+let store;
+
+describe('Component: WinScreen', () => {
+  beforeEach(() => {
+    history = createMemoryHistory();
+    history.push(AppRoute.RESULT);
+
+    store = mockStore({
+      GAME: {step: 10, mistakes: 2},
+    });
+  });
+
+  it('should render correctly', () => {
+    const correctlyQuestionsCount = 8;
+    const mistakes = 2;
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <WinScreen
+            onReplayButtonClick={jest.fn()}
+          />
+        </Router>
+      </Provider>,
+    );
+
+    expect(screen.getByText(/Вы настоящий меломан!/i)).toBeInTheDocument();
+    expect(screen.getByText(/Вы ответили правильно/i)).toHaveTextContent(correctlyQuestionsCount);
+    expect(screen.getByText(/Вы ответили правильно/i)).toHaveTextContent(mistakes);
+  });
+
+  it('should redirect when user click "Replay Button"', () => {
+    const replayButtonClickHandle = jest.fn(
+      () => history.push(AppRoute.GAME));
+
+    render(
+      <Provider store={store}>
+        <Router history={history}>
+          <Switch>
+            <Route exact path={AppRoute.RESULT}>
+              <WinScreen
+                onReplayButtonClick={replayButtonClickHandle}
+              />
+            </Route>
+            <Route exact path={AppRoute.GAME}><h1>Game Screen</h1></Route>
+          </Switch>
+        </Router>
+      </Provider>,
+    );
+
+    userEvent.click(screen.getByText(/Сыграть ещё раз/i));
+    expect(replayButtonClickHandle).toBeCalled();
+    expect(screen.getByText('Game Screen')).toBeInTheDocument();
+  });
+});
