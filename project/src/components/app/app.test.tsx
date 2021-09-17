3827b39040ac41ebd93945456032bdf31e536d20
kam4atka <sledsib@ya.ru>
2021-09-17 19:26:10 +0300

    2.3. Добавит React-компоненты.
    Создадим первый компонент для экрана приветствия. Все компоненты будем
    размещать в директории `components`. Для каждого компонента создадим
    отдельную директорию.
    
    Затем изменим второй компонент — `App`. Он будет отрисовывать компонент
    `WelcomeScreen`.
    
    Последним шагом отредактируем файл `index.tsx`. Передадим компоненту
    `App` его первый пропс.

--- project/src/components/app/app.test.tsx
+++
@@ -1,8 +0,0 @@
-import {render, screen} from '@testing-library/react';
-import App from './app';
-
-test('Renders app-component', () => {
-  render(<App />);
-  const textElement = screen.getByText(/Hello, world!/i);
-  expect(textElement).toBeInTheDocument();
-});
--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,8 +2,14 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './components/app/app';
 
+const Setting = {
+  ERRORS_COUNT: 3,
+};
+
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <App
+      errorsCount = {Setting.ERRORS_COUNT}
+    />
   </React.StrictMode>,
   document.getElementById('root'));
