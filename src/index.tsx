cd3af095992c0f7f37b7a8bb134ef842559937fd
kam4atka <sledsib@ya.ru>
2021-09-20 17:41:30 +0300

    4.3. Подключит моки в файл `index.tsx`. WIP
    Подключим модуль с тестовыми данными в файл `index.tsx`, а затем
    передадим данные через пропсы в компонент `App`.
    
    WIP: Линтер выдает ошибку о неиспользуемой переменной.

--- src/index.tsx
+++ src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 import {Setting} from './const';
+import {questions} from './mocks/questions';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
@@ -11,6 +12,7 @@ root.render(
   <React.StrictMode>
     <App
       errorsCount = {Setting.ErrorsCount}
+      questions = {questions}
     />
   </React.StrictMode>,
 );
