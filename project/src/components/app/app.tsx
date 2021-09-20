650f2320f972ad1e00a5320781f587fb295b7a78
kam4atka <sledsib@ya.ru>
2021-09-20 17:41:30 +0300

    4.3. Подключит моки в файл `index.tsx`.
    Подключим модуль с тестовыми данными в файл `index.tsx`, а затем
    передадим данные через пропсы в компонент `App`.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -9,12 +9,14 @@ import GameOverScreen from '../../pages/game-over-screen/game-over-screen';
 import WinScreen from '../../pages/win-screen/win-screen';
 import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 import PrivateRoute from '../private-route/private-route';
+import {Questions} from '../../types/question';
 
 type AppScreenProps = {
   errorsCount: number;
+  questions: Questions;
 }
 
-function App({errorsCount}: AppScreenProps): JSX.Element {
+function App({errorsCount, questions}: AppScreenProps): JSX.Element {
   return (
     <HelmetProvider>
       <BrowserRouter>
