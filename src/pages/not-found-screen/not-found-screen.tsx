b92d06ba27f265e64059194bebc4caec7f2a7079
kam4atka <sledsib@ya.ru>
2021-09-20 15:43:31 +0300

    3.3. Обновит ссылку для логотипа.
    Заменим код разметки логотипа в компонентах `ArtistQuestionScreen`,
    `GenreQuestionScreen` и `NotFoundScreen` на вызов компонента `Logo`,
    созданного в предыдущем коммите.
    
    Обратите внимание, клик по ссылке "Вернуться на главную" в компоненте
    `NotFoundScreen` перенаправляет пользователя на главный экран приложения.
    Сейчас эта задача решается за счёт компонента `<a>`. Обновим компонент и
    заменим `<a>` на `<Link>` из пакета `react-router-dom`.
    
    Аналогичные изменения необходимо повторить в компоненте `WinScreen`.

--- src/pages/not-found-screen/not-found-screen.tsx
+++ src/pages/not-found-screen/not-found-screen.tsx
@@ -1,11 +1,11 @@
+import {Link} from 'react-router-dom';
+import Logo from '../../components/logo/logo';
+
 function NotFoundScreen(): JSX.Element {
   return (
     <section className="game">
       <header className="game__header">
-        <a className="game__back" href="/">
-          <span className="visually-hidden">Сыграть ещё раз</span>
-          <img className="game__logo" src="img/melody-logo-ginger.png" alt="Угадай мелодию" />
-        </a>
+        <Logo />
 
         <svg xmlns="http://www.w3.org/2000/svg" className="timer" viewBox="0 0 780 780">
           <circle className="timer__line" cx="390" cy="390" r="370"
@@ -16,7 +16,7 @@ function NotFoundScreen(): JSX.Element {
 
       <section className="game__screen">
         <h1>404. Page not found</h1>
-        <a href="/">Вернуться на главную</a>
+        <Link to="/">Вернуться на главную</Link>
       </section>
     </section>
   );
--- src/pages/win-screen/win-screen.tsx
+++ src/pages/win-screen/win-screen.tsx
@@ -1,8 +1,10 @@
+import {Link} from 'react-router-dom';
+
 function WinScreen(): JSX.Element {
   return (
     <section className="result">
       <div className="result-logout__wrapper">
-        <a className="result-logout__link" href="/">Выход</a>
+        <Link className="result-logout__link" to="/">Выход</Link>
       </div>
       <div className="result__logo">
         <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
