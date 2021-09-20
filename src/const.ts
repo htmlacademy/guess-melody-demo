1c5d130e4145ed3a3f96c15e4d3af5763ba180fa
kam4atka <sledsib@ya.ru>
2021-09-20 15:11:45 +0300

    3.1. Добавит маршрутизацию.
    На этом шаге добавим маршрутизацию в приложение. Для этого в модуле
    компонента `App` подключим несколько компонентов из пакета
    `react-router-dom` (`Routes`, `Route`, `BrowserRouter`), а затем
    обновим код самого компонента: добавим маршрутизацию.
    
    Обратите внимание: маршруты `/dev-artist`, `/dev-genre` нужны только
    на этапе разработки. Позже мы избавимся от них, заменив маршрутом
    `/game`.

--- src/const.ts
+++ src/const.ts
@@ -1,3 +1,13 @@
 export const Setting = {
   ErrorsCount: 3
 };
+
+export enum AppRoute {
+  Login = '/login',
+  Lose = '/lose',
+  Result = '/result',
+  Root = '/',
+  DevArtist = '/dev-artist',
+  DevGenre = '/dev-genre'
+}
+
