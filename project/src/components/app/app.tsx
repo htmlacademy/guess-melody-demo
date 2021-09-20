b58f88dd994e6383464e3d7f57f7aec89eeae049
kam4atka <sledsib@ya.ru>
2021-09-20 15:11:45 +0300

    3.1. Добавит маршрутизацию.
    На этом шаге добавим маршрутизацию в приложение. Для этого в модуле
    компонента `App` подключим несколько компонентов из пакета
    `react-router-dom` (`Routes`, `Route`, `BrowserRouter`), а затем
    обновим код самого компонента: добавим маршрутизацию.
    
    Обратите внимание: маршруты `/dev-artist`, `/dev-genre` нужны только
    на этапе разработки. Позже мы избавимся от них, заменив маршрутом
    `/game`.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Route, BrowserRouter, Routes} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import WelcomeScreen from '../../pages/welcome-screen/welcome-screen';
+import ArtistQuestionScreen from '../../pages/artist-question-screen/artist-question-screen';
+import GenreQuestionScreen from '../../pages/genre-question-screen/genre-question-screen';
+import AuthScreen from '../../pages/auth-screen/auth-screen';
+import GameOverScreen from '../../pages/game-over-screen/game-over-screen';
+import WinScreen from '../../pages/win-screen/win-screen';
+import NotFoundScreen from '../../pages/not-found-screen/not-found-screen';
 
 type AppScreenProps = {
   errorsCount: number;
@@ -6,7 +14,38 @@ type AppScreenProps = {
 
 function App({errorsCount}: AppScreenProps): JSX.Element {
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Routes>
+        <Route
+          path={AppRoute.Root}
+          element={<WelcomeScreen errorsCount={errorsCount} />}
+        />
+        <Route
+          path={AppRoute.DevArtist}
+          element={<ArtistQuestionScreen />}
+        />
+        <Route
+          path={AppRoute.DevGenre}
+          element={<GenreQuestionScreen />}
+        />
+        <Route
+          path={AppRoute.Login}
+          element={<AuthScreen />}
+        />
+        <Route
+          path={AppRoute.Result}
+          element={<WinScreen />}
+        />
+        <Route
+          path={AppRoute.Lose}
+          element={<GameOverScreen />}
+        />
+        <Route
+          path="*"
+          element={<NotFoundScreen />}
+        />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
