1b69df787ac650f484d738c9cadd9b0e656926cf
kam4atka <sledsib@ya.ru>
2021-09-20 16:30:27 +0300

    4.1. Добавит моки для игры «Угадай жанр».
    Для хранения тестовых данных заведём отдельную директорию `mocks`.
    Создадим в ней модуль `questions.ts` и опишем тестовые данные для игр.
    Начнём с игры «Угадай жанр».
    
    Паралельно с этим опишем типы для тестовых данных.

---
+++ project/src/mocks/questions.ts
@@ -0,0 +1,21 @@
+import {QuestionGenre} from '../types/question';
+
+export const questions: QuestionGenre[] = [
+  {
+    type: 'genre',
+    genre: 'rock',
+    answers: [{
+      src: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg',
+      genre: 'rock',
+    }, {
+      src: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg',
+      genre: 'blues',
+    }, {
+      src: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg',
+      genre: 'jazz',
+    }, {
+      src: 'https://upload.wikimedia.org/wikipedia/commons/4/4e/BWV_543-fugue.ogg',
+      genre: 'rock',
+    }],
+  },
+];
