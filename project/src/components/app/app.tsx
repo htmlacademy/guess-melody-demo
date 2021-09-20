59a14b99934c2e2af034eb66d43996d14e931662
kam4atka <sledsib@ya.ru>
2021-09-20 15:11:45 +0300

    3.1. Добавит маршрутизацию.
    На этом шаге добавим маршрутизацию в приложение. Для этого в модуле
    компонента `App` подключим несколько компонентов из пакета
    `react-router-dom` (`Switch`, `Route`, `BrowserRouter`), а затем
    обновим код самого компонента: добавим маршрутизацию.
    
    Компонент `Route` принимает проп `exact`. Он позволяет установить
    точное соответствие маршруту и `location.pathname`.
    
    Обратите внимание: маршруты `/dev-artist`, `/dev-genre` нужны только
    на этапе разработки. Позже мы избавимся от них, заменив маршрутом
    `/game`.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,4 +1,12 @@
+import {Switch, Route, BrowserRouter} from 'react-router-dom';
+import {AppRoute} from '../../const';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
+import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
+import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+import AuthScreen from '../auth-screen/auth-screen';
+import GameOverScreen from '../game-over-screen/game-over-screen';
+import WinScreen from '../win-screen/win-screen';
+import NotFoundScreen from '../not-found-screen/not-found-screen';
 
 type AppScreenProps = {
   errorsCount: number;
@@ -6,7 +14,33 @@ type AppScreenProps = {
 
 function App({errorsCount}: AppScreenProps): JSX.Element {
   return (
-    <WelcomeScreen errorsCount={errorsCount} />
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={AppRoute.Root}>
+          <WelcomeScreen
+            errorsCount={errorsCount}
+          />
+        </Route>
+        <Route exact path={AppRoute.DevArtist}>
+          <ArtistQuestionScreen />
+        </Route>
+        <Route exact path={AppRoute.DevGenre}>
+          <GenreQuestionScreen />
+        </Route>
+        <Route exact path={AppRoute.Login}>
+          <AuthScreen />
+        </Route>
+        <Route exact path={AppRoute.Result}>
+          <WinScreen />
+        </Route>
+        <Route exact path={AppRoute.Lose}>
+          <GameOverScreen />
+        </Route>
+        <Route>
+          <NotFoundScreen />
+        </Route>
+      </Switch>
+    </BrowserRouter>
   );
 }
 
