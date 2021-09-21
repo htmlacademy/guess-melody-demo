9039e40947632522b7198c605450913b305d2336
kam4atka <sledsib@ya.ru>
2021-09-21 12:58:48 +0300

    4.6. Добавит обработчик отправки формы в GenreQuestionScreen. WIP
    Опишем обработчик отправки формы. В нём вызовем функцию, переданную в
    проп `onAnswer`. В будущем эта функция будет отвечать за проверку
    ответа на вопрос. Передадим ей вопрос и ответы пользователя.
    
    Обратите внимание, на данном этапе, функция обработчика отправки формы
    генерирует ошибку. Это временное решение и в дальнейшем данный
    обработчик будет совершать полезную работу. А пока, если мы попробуем
    отправить ответ в компоненте `GenreQuestionScreen`, проект завершится
    ошибкой.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -34,6 +34,9 @@ function App({errorsCount, questions}: AppScreenProps): JSX.Element {
           element={
             <GenreQuestionScreen
               question={firstQuestion as QuestionGenre}
+              onAnswer={() => {
+                throw new Error('Function \'onAnswer\' isn\'t implemented.');
+              }}
             />
           }
         />
