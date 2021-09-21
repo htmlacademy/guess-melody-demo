0db9849cd2550a18b04733a9d5538b637a5f8384
kam4atka <sledsib@ya.ru>
2021-09-21 19:45:57 +0300

    4.9. Добавит маршрут /game.
    Добавим маршрут `/game`. В компоненте `WelcomeScreen` добавим действие
    для кнопки запуска игры. При нажатии на кнопку пользователь
    перенаправляется по маршруту `/game`.

--- src/pages/welcome-screen/welcome-screen.tsx
+++ src/pages/welcome-screen/welcome-screen.tsx
@@ -1,10 +1,14 @@
+import {useNavigate} from 'react-router-dom';
 import {Helmet} from 'react-helmet-async';
+import {AppRoute} from '../../const';
 
 type WelcomeScreenProps = {
   errorsCount: number;
 }
 
 function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
+  const navigate = useNavigate();
+
   return (
     <section className="welcome">
       <Helmet>
@@ -13,7 +17,14 @@ function WelcomeScreen({errorsCount}: WelcomeScreenProps): JSX.Element {
       <div className="welcome__logo">
         <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83"/>
       </div>
-      <button className="welcome__button"><span className="visually-hidden">Начать игру</span></button>
+      <button
+        className="welcome__button"
+        onClick={() => navigate(AppRoute.Game)}
+      >
+        <span className="visually-hidden">
+          Начать игру
+        </span>
+      </button>
       <h2 className="welcome__rules-title">Правила игры</h2>
       <p className="welcome__text">Правила просты:</p>
       <ul className="welcome__rules-list">
