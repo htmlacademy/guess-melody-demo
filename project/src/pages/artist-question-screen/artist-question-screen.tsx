8058c8dec5118825b9219dce18ecd194312450ec
kam4atka <sledsib@ya.ru>
2021-09-21 19:04:25 +0300

    4.7. Обновит компонент ArtistQuestionScreen. WIP
    Произведём аналогичные действия с компонентом `ArtistQuestionScreen`.
    Стейт нам не потребуется, так как ответ может быть только один.

--- project/src/pages/artist-question-screen/artist-question-screen.tsx
+++ project/src/pages/artist-question-screen/artist-question-screen.tsx
@@ -1,7 +1,17 @@
+import {ChangeEvent} from 'react';
 import {Helmet} from 'react-helmet-async';
 import Logo from '../../components/logo/logo';
+import {QuestionArtist, UserArtistQuestionAnswer} from '../../types/question';
 
-function ArtistQuestionScreen(): JSX.Element {
+type ArtistQuestionScreenProps = {
+  question: QuestionArtist;
+  onAnswer: (question: QuestionArtist, answer: UserArtistQuestionAnswer) => void;
+};
+
+function ArtistQuestionScreen(props: ArtistQuestionScreenProps): JSX.Element {
+  const {question, onAnswer} = props;
+  const {answers, song} = question;
+
   return (
     <section className="game game--artist">
       <Helmet>
@@ -29,35 +39,33 @@ function ArtistQuestionScreen(): JSX.Element {
           <div className="track">
             <button className="track__button track__button--play" type="button" />
             <div className="track__status">
-              <audio></audio>
+              <audio
+                src={song.src}
+              />
             </div>
           </div>
         </div>
 
         <form className="game__artist">
-          <div className="artist">
-            <input className="artist__input visually-hidden" type="radio" name="answer" value="artist-1" id="answer-1" />
-            <label className="artist__name" htmlFor="answer-1">
-              <img className="artist__picture" src="img/placeholder.jpg" alt="Пелагея" />
-              Пелагея
-            </label>
-          </div>
-
-          <div className="artist">
-            <input className="artist__input visually-hidden" type="radio" name="answer" value="artist-2" id="answer-2" />
-            <label className="artist__name" htmlFor="answer-2">
-              <img className="artist__picture" src="img/placeholder.jpg" alt="Краснознаменная дивизия имени моей бабушки" />
-              Краснознаменная дивизия имени моей бабушки
-            </label>
-          </div>
-
-          <div className="artist">
-            <input className="artist__input visually-hidden" type="radio" name="answer" value="artist-3" id="answer-3" />
-            <label className="artist__name" htmlFor="answer-3">
-              <img className="artist__picture" src="img/placeholder.jpg" alt="Lordi" />
-              Lordi
-            </label>
-          </div>
+          {answers.map((answer, id) => (
+            <div key={answer.artist} className="artist">
+              <input
+                className="artist__input visually-hidden"
+                type="radio"
+                name="answer"
+                value={`answer-${id}`}
+                id={`answer-${id}`}
+                onChange={(evt: ChangeEvent<HTMLInputElement>) => {
+                  evt.preventDefault();
+                  onAnswer(question, answer.artist);
+                }}
+              />
+              <label className="artist__name" htmlFor={`answer-${id}`}>
+                <img className="artist__picture" src={answer.picture} alt={answer.artist} />
+                {answer.artist}
+              </label>
+            </div>
+          ))}
         </form>
       </section>
     </section>
