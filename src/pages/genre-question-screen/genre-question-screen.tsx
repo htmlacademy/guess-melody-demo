c8f930e3bc26f2c945b201d6203ea85fd07fe227
kam4atka <sledsib@ya.ru>
2021-09-22 15:14:28 +0300

    5.8. Уберёт подключение AudioPlayer из GenreQuestionScreen. WIP
    Компонент `AudioPlayer` больше не нужен в `GenreQuestionScreen`. Для
    этого мы создали HOC. Следовательно, подключение компонента необходимо
    удалить, а заодно убрать из стейта `activePlayer`.

--- src/pages/genre-question-screen/genre-question-screen.tsx
+++ src/pages/genre-question-screen/genre-question-screen.tsx
@@ -1,20 +1,19 @@
 import {useState, FormEvent, ChangeEvent} from 'react';
 import {Helmet} from 'react-helmet-async';
 import Logo from '../../components/logo/logo';
-import AudioPlayer from '../../components/audio-player/audio-player';
 import {QuestionGenre, UserGenreQuestionAnswer} from '../../types/question';
 
 type GenreQuestionScreenProps = {
   question: QuestionGenre;
   onAnswer: (question: QuestionGenre, answers: UserGenreQuestionAnswer) => void;
+  renderPlayer: (src: string, playerIndex: number) => JSX.Element;
 };
 
 function GenreQuestionScreen(props: GenreQuestionScreenProps): JSX.Element {
-  const {question, onAnswer} = props;
+  const {question, onAnswer, renderPlayer} = props;
   const {answers, genre} = question;
 
   const [userAnswers, setUserAnswers] = useState([false, false, false, false]);
-  const [activePlayer, setActivePlayer] = useState(0);
 
   return (
     <section className="game game--genre">
@@ -50,11 +49,7 @@ function GenreQuestionScreen(props: GenreQuestionScreenProps): JSX.Element {
             const keyValue = `${id}-${answer.src}`;
             return (
               <div key={keyValue} className="track">
-                <AudioPlayer
-                  isPlaying={id === activePlayer}
-                  src={answer.src}
-                  onPlayButtonClick = {() => setActivePlayer(activePlayer === id ? -1 : id)}
-                />
+                {renderPlayer(answer.src, id)}
                 <div className="game__answer">
                   <input className="game__input visually-hidden" type="checkbox" name="answer" value={`answer-${id}`}
                     id={`answer-${id}`}
