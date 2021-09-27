21f9b54ca3df030e42d0773f8c8ff8ce6d0ceab4
kam4atka <sledsib@ya.ru>
2021-09-27 17:03:00 +0300

    1.1. Инициализирует проект с помощью CRA.
    Подготовить проект к разработке можно несколькими способами. Первый
    заключается в самостоятельной настройке всех необходимых компонентов:
    конфигурирование сборщика, настройка линтера, подбор пакетов и так далее.
    Второй вариант — воспользоваться специальным инструментом.
    
    Мы воспользуемся вторым вариантом. Создадим заготовку проекта при
    помощи CRA (Create React App). Это официальный инструмент для подготовки
    основы для проектов.
    
    Из коробки CRA предоставляет готовый шаблон, но одним им дело не ограничивается.
    На npmjs.com можно найти десятки стартовых шаблонов от различных компаний.
    Мы воспользуемся шаблоном @htmlacademy/cra-template-typescript
    (https://www.npmjs.com/package/@htmlacademy/cra-template-typescript).
    
    Для создания проекта достаточно ввести одну единственную команду:
    `npx create-react-app project --template @htmlacademy/cra-template-typescript`.

---
+++ project/src/components/app/app.test.tsx
@@ -0,0 +1,8 @@
+import {render, screen} from '@testing-library/react';
+import App from './app';
+
+test('Renders app-component', () => {
+  render(<App />);
+  const textElement = screen.getByText(/Hello, world!/i);
+  expect(textElement).toBeInTheDocument();
+});
