eb7692db4be881fc84ca89dd0ff68a8b2dab296e
kam4atka <sledsib@ya.ru>
2021-10-01 14:49:12 +0300

    3.5. Заменит Route на PrivateRoute.
    Защитим маршрут `/result` при помощи компонента `PrivateRoute`.
    Если пользователь прошёл авторизацию, то он попадёт на маршрут `/result`.
    Если нет, то будет перенаправлен на `/login`.
    
    На данном этапе будем передавать статичное значение для пропса
    `authorizationStatus`. Пользователь всегда будет считаться
    неавторизованным. Мы исправим это, когда научимся взаимодействовать
    с сервером.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -1,5 +1,5 @@
 import {Switch, Route, BrowserRouter} from 'react-router-dom';
-import {AppRoute} from '../../const';
+import {AppRoute, AuthorizationStatus} from '../../const';
 import WelcomeScreen from '../welcome-screen/welcome-screen';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
@@ -7,6 +7,7 @@ import AuthScreen from '../auth-screen/auth-screen';
 import GameOverScreen from '../game-over-screen/game-over-screen';
 import WinScreen from '../win-screen/win-screen';
 import NotFoundScreen from '../not-found-screen/not-found-screen';
+import PrivateRoute from '../private-route/private-route';
 
 type AppScreenProps = {
   errorsCount: number;
@@ -30,9 +31,13 @@ function App({errorsCount}: AppScreenProps): JSX.Element {
         <Route exact path={AppRoute.Login}>
           <AuthScreen />
         </Route>
-        <Route exact path={AppRoute.Result}>
-          <WinScreen />
-        </Route>
+        <PrivateRoute
+          exact
+          path={AppRoute.Result}
+          render={() => <WinScreen />}
+          authorizationStatus={AuthorizationStatus.NoAuth}
+        >
+        </PrivateRoute>
         <Route exact path={AppRoute.Lose}>
           <GameOverScreen />
         </Route>
