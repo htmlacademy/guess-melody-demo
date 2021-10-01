d626ebb454d87a6dc1b233bf96a260577f88e717
kam4atka <sledsib@ya.ru>
2021-10-01 14:48:27 +0300

    3.4. Добавит компонент приватного маршрута.
    Добавим компонент для описания приватных маршрутов.

---
+++ project/src/components/private-route/private-route.tsx
@@ -0,0 +1,19 @@
+import {Navigate} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
+
+type PrivateRouteProps = {
+  authorizationStatus: AuthorizationStatus;
+  children: JSX.Element;
+}
+
+function PrivateRoute(props: PrivateRouteProps): JSX.Element {
+  const {authorizationStatus, children} = props;
+
+  return (
+    authorizationStatus === AuthorizationStatus.Auth
+      ? children
+      : <Navigate to={AppRoute.Login} />
+  );
+}
+
+export default PrivateRoute;
