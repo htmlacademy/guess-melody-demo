0097eb43a011dbf3241c13106a43ebff09c3a8c8
kam4atka <sledsib@ya.ru>
2021-10-01 14:48:27 +0300

    3.4. Добавит компонент приватного маршрута.
    Добавим компонент для описания приватных маршрутов.

---
+++ project/src/components/private-route/private-route.tsx
@@ -0,0 +1,26 @@
+import {Route, Redirect} from 'react-router-dom';
+import {RouteProps} from 'react-router-dom';
+import {AppRoute, AuthorizationStatus} from '../../const';
+
+type PrivateRouteProps = RouteProps & {
+  render: () => JSX.Element;
+  authorizationStatus: AuthorizationStatus;
+}
+
+function PrivateRoute(props: PrivateRouteProps): JSX.Element {
+  const {exact, path, render, authorizationStatus} = props;
+
+  return (
+    <Route
+      exact={exact}
+      path={path}
+      render={() => (
+        authorizationStatus === AuthorizationStatus.Auth
+          ? render()
+          : <Redirect to={AppRoute.Login} />
+      )}
+    />
+  );
+}
+
+export default PrivateRoute;
