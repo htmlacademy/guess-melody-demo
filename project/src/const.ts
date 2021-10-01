7bded6812c990469633323dfcbd96d1c5ff2db30
kam4atka <sledsib@ya.ru>
2021-10-01 14:48:27 +0300

    3.4. Добавит компонент приватного маршрута.
    Добавим компонент для описания приватных маршрутов.

--- project/src/const.ts
+++ project/src/const.ts
@@ -6,3 +6,9 @@ export enum AppRoute {
   DevArtist = '/dev-artist',
   DevGenre = '/dev-genre'
 }
+
+export enum AuthorizationStatus {
+  Auth = 'AUTH',
+  NoAuth = 'NO_AUTH',
+  Unknown = 'UNKNOWN',
+}
