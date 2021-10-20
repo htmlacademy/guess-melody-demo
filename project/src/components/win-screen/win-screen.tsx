75977abb85794e6360113ddd6f24c1d842f39959
kam4atka <sledsib@ya.ru>
2021-10-21 01:39:58 +0300

    7.12. Обновит компонент WinScreen.
    Теперь, при нажатии на кн. "Сыграть ещё раз", в компоненте `WinScreen`,
    происходит повторный запуск игры.

--- project/src/components/win-screen/win-screen.tsx
+++ project/src/components/win-screen/win-screen.tsx
@@ -1,6 +1,10 @@
 import {Link} from 'react-router-dom';
+import {useHistory} from 'react-router-dom';
+import {AppRoute} from '../../const';
 
 function WinScreen(): JSX.Element {
+  const history = useHistory();
+
   return (
     <section className="result">
       <div className="result-logout__wrapper">
@@ -11,7 +15,13 @@ function WinScreen(): JSX.Element {
       </div>
       <h2 className="result__title">Вы настоящий меломан!</h2>
       <p className="result__total">Вы ответили правильно на 6 вопросов и совершили 2 ошибки</p>
-      <button className="replay" type="button">Сыграть ещё раз</button>
+      <button
+        onClick={() => history.push(AppRoute.Game)}
+        className="replay"
+        type="button"
+      >
+        Сыграть ещё раз
+      </button>
     </section>
   );
 }
