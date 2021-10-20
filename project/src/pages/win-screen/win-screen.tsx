0e13de8c29f766885592bb11f92a1e2725762f41
kam4atka <sledsib@ya.ru>
2021-10-21 01:39:58 +0300

    7.17. Отрефакторит экраны WinScreen и GameOverScreen.
    Добавим необходимые действия для кнопок "Сыграть ещё раз", в компоненте
    `WinScreen`, и "Попробовать еще раз", в компоненте `GameOverScreen`.
    Теперь клип по этим кнопкам приводит в повторному запуску игры.
    
    Также поправим отображение информации о результатах игры в компоненте
    `WinScreen`.

--- project/src/pages/win-screen/win-screen.tsx
+++ project/src/pages/win-screen/win-screen.tsx
@@ -1,6 +1,16 @@
-import {Link} from 'react-router-dom';
+import {Link, useNavigate} from 'react-router-dom';
+import {useAppSelector, useAppDispatch} from '../../hooks';
+import {resetGame} from '../../store/action';
+import {AppRoute} from '../../const';
 
 function WinScreen(): JSX.Element {
+  const {step, mistakes} = useAppSelector((state) => state);
+
+  const dispatch = useAppDispatch();
+  const navigate = useNavigate();
+
+  const correctlyQuestionsCount = step - mistakes;
+
   return (
     <section className="result">
       <div className="result-logout__wrapper">
@@ -10,8 +20,17 @@ function WinScreen(): JSX.Element {
         <img src="img/melody-logo.png" alt="Угадай мелодию" width="186" height="83" />
       </div>
       <h2 className="result__title">Вы настоящий меломан!</h2>
-      <p className="result__total">Вы ответили правильно на 6 вопросов и совершили 2 ошибки</p>
-      <button className="replay" type="button">Сыграть ещё раз</button>
+      <p className="result__total">Вы ответили правильно на {correctlyQuestionsCount} вопросов и совершили {mistakes} ошибки</p>
+      <button
+        onClick={() => {
+          dispatch(resetGame());
+          navigate(AppRoute.Game);
+        }}
+        className="replay"
+        type="button"
+      >
+        Сыграть ещё раз
+      </button>
     </section>
   );
 }
