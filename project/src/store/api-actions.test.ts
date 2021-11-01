75e34291baba184ae60dc3882adc2d6e13aa9091
kam4atka <sledsib@ya.ru>
2021-11-01 14:41:31 +0300

    9.8. Добавит тест для асинхронных операций.
    В модуле `api-actions.test.js` опишем тест для асинхронных операций.
    Для написания тестов потребуется воспользоваться `MockAdapter`.
    Настоящие запросы к серверу мы делать не будем.
    
    Проверим, что при обращению к серверy, возвращается ответ 200 и
    статус `auth`.

---
+++ project/src/store/api-actions.test.ts
@@ -0,0 +1,36 @@
+import {Action} from 'redux';
+import thunk, {ThunkDispatch} from 'redux-thunk';
+import MockAdapter from 'axios-mock-adapter';
+import {configureMockStore} from '@jedmao/redux-mock-store';
+import {createAPI} from '../services/api';
+import {checkAuthAction} from './api-actions';
+import {requireAuthorization} from './user-process/user-process';
+import {APIRoute} from '../const';
+import {State} from '../types/state';
+
+describe('Async actions', () => {
+  const api = createAPI();
+  const mockAPI = new MockAdapter(api);
+  const middlewares = [thunk.withExtraArgument(api)];
+
+  const mockStore = configureMockStore<
+      State,
+      Action,
+      ThunkDispatch<State, typeof api, Action>
+    >(middlewares);
+
+  it('should authorization status is «auth» when server return 200', async () => {
+    const store = mockStore();
+    mockAPI
+      .onGet(APIRoute.Login)
+      .reply(200, []);
+
+    expect(store.getActions()).toEqual([]);
+
+    await store.dispatch(checkAuthAction());
+
+    const actions = store.getActions().map(({type}) => type);
+
+    expect(actions).toContain(requireAuthorization.toString());
+  });
+});
