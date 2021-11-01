2565ea4ae9e996ff3cb22b85bb56ee227a79e345
kam4atka <sledsib@ya.ru>
2021-11-01 13:40:13 +0300

    9.2. Добавит описание тестового случая.
    Попробуем написать первый тестовый случай. Убедимся, что функция
    `isArtistAnswerCorrect` вернёт `true`, когда ответ на вопрос правильный.
    В качестве вопроса мы будем использовать тестовые данные.
    
    Специально для этого мы подготовили функцию `mockFakeArtistQuestion`.

--- project/src/game.test.ts
+++ project/src/game.test.ts
@@ -1,5 +1,12 @@
+import {isArtistAnswerCorrect} from './game';
+import {makeFakeArtistQuestion} from './utils/mocks';
+
+const mockArtistQuestion = makeFakeArtistQuestion();
+
 describe('Function: isArtistAnswerCorrect', () => {
   it('should return "true" when answer is correct', () => {
-    // Тестовый случай
+    const {artist: correctAnswer} = mockArtistQuestion.song;
+    expect(isArtistAnswerCorrect(mockArtistQuestion, correctAnswer))
+      .toBe(true);
   });
 });
