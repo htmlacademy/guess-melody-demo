b4b45e89b96c49d8d903e26afbc79acdde73031f
Igor Antonov <antonov.igor.khv@gmail.com>
2022-02-25 16:37:02 +0300

    6.9. Добавит действие `checkUserAnswer`.
    Следующим шагом создадим новое действие, в котором будем выполнять
    проверку правильности выбранного ответа и на основании проверки
    увеличивать счётчик ошибок.

--- src/store/action.ts
+++ src/store/action.ts
@@ -1,5 +1,8 @@
 import {createAction} from '@reduxjs/toolkit';
+import {Question, UserAnswer} from '../types/question';
 
 export const incrementStep = createAction('game/incrementStep');
+
+export const checkUserAnswer = createAction<{question: Question; userAnswer: UserAnswer}>('game/checkUserAnswer');
 
 export const resetGame = createAction('game/reset');
