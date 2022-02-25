f7747f3b9a6b4f424c5b28789990cf5bf090ba51
Igor Antonov <antonov.igor.khv@gmail.com>
2022-02-25 15:00:20 +0300

    6.3. Добавит действие для перезапуска игры.
    Мы готовы воспользоваться подготовленным хранилищем. Но перед тем как
    это сделать, создадим ещё одно действие (action). Пользователь может
    захотеть сыграть в игру повторно, поэтому нам потребуется
    действие для сброса игры. Оно должно возвращать состояние приложения к
    к изначальному виду.

--- project/src/store/reducer.ts
+++ project/src/store/reducer.ts
@@ -1,5 +1,5 @@
 import {createReducer} from '@reduxjs/toolkit';
-import {incrementStep} from './action';
+import {incrementStep, resetGame} from './action';
 import {FIRST_GAME_STEP} from '../const';
 
 const STEP_COUNT = 1;
@@ -13,6 +13,10 @@ const reducer = createReducer(initialState, (builder) => {
   builder
     .addCase(incrementStep, (state) => {
       state.step = state.step + STEP_COUNT;
+    })
+    .addCase(resetGame, (state) => {
+      state.mistakes = 0;
+      state.step = FIRST_GAME_STEP;
     });
 });
 
