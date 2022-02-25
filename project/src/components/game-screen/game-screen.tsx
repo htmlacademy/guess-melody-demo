f63ba1f3cb3ae0ce069b69feeeec3fa2c88c649e
Igor Antonov <antonov.igor.khv@gmail.com>
2022-02-25 16:23:07 +0300

    6.7. Добавит вывод ошибок в игровые экраны.
    Теперь подключим компонент `Mistakes` в `GameScreen`. Этот компонент мы
    будем передавать в `GenreQuestionScreen` и `ArtistQuestionScreen` в виде
    дочернего компонента.
    
    Количество возможных ошибок хранится в хранилище, в поле `mistakes`.
    Извлечём значение с помощью `useAppSelector`.

--- project/src/components/game-screen/game-screen.tsx
+++ project/src/components/game-screen/game-screen.tsx
@@ -4,6 +4,7 @@ import {incrementStep} from '../../store/action';
 import {AppRoute, GameType} from '../../const';
 import ArtistQuestionScreen from '../artist-question-screen/artist-question-screen';
 import GenreQuestionScreen from '../genre-question-screen/genre-question-screen';
+import Mistakes from '../mistakes/mistakes';
 import {QuestionArtist, QuestionGenre, Questions} from '../../types/question';
 import withAudioPlayer from '../../hocs/with-audio-player/with-audio-player';
 
@@ -15,7 +16,7 @@ type GameScreenProps = {
 };
 
 function GameScreen({questions}: GameScreenProps): JSX.Element {
-  const step = useAppSelector((state) => state.step);
+  const {step, mistakes} = useAppSelector((state) => state);
 
   const question = questions[step];
 
@@ -32,7 +33,9 @@ function GameScreen({questions}: GameScreenProps): JSX.Element {
           key={step}
           question={question as QuestionArtist}
           onAnswer={() => dispatch(incrementStep())}
-        />
+        >
+          <Mistakes count={mistakes} />
+        </ArtistQuestionScreenWrapped>
       );
     case GameType.Genre:
       return (
@@ -40,7 +43,9 @@ function GameScreen({questions}: GameScreenProps): JSX.Element {
           key={step}
           question={question as QuestionGenre}
           onAnswer={() => dispatch(incrementStep())}
-        />
+        >
+          <Mistakes count={mistakes} />
+        </GenreQuestionScreenWrapped>
       );
     default:
       return <Navigate to={AppRoute.Root} />;
