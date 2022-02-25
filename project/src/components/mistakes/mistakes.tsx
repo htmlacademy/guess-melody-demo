6cbef29bd267bf8d5e3c2d4c78dd551c11b17da1
Igor Antonov <antonov.igor.khv@gmail.com>
2022-02-25 15:43:56 +0300

    6.5. Добавит компонент для отображения ошибок.
    Во время игры пользователь может совершить несколько ошибок. Эту
    информацию мы должны выводить в виде изображений с нотами. Создадим для
    этого отдельный компонент `Mistakes`.

---
+++ project/src/components/mistakes/mistakes.tsx
@@ -0,0 +1,18 @@
+type MistakesProps = {
+  count: number;
+};
+
+function Mistakes({count}: MistakesProps): JSX.Element {
+  const mistakes = Array.from({length: count}, () => '');
+
+  return (
+    <div className="game__mistakes">
+      {mistakes.map((_item, index) => {
+        const keyValue = `mistake-${index}`;
+        return <div key={keyValue} className="wrong" />;
+      })}
+    </div>
+  );
+}
+
+export default Mistakes;
