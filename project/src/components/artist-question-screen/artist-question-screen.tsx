8dd18b1d3a50b2bc09d366ac7842ae8f1099c627
Igor Antonov <antonov.igor.khv@gmail.com>
2022-02-25 16:03:44 +0300

    6.6. Удалит разметку отрисовки ошибок из игровых экранов.
    В прошлом шаге мы создали отдельный компонент для отрисовки ошибок.
    Следовательно, аналогичный JSX внутри компонентов `GenreQuestionScreen`
    и `ArtistQuestionScreen` нам не нужен.
    
    Удалим лишний JSX и воспользуемся служебным пропсом `children` для
    отрисовки компонента с количеством ошибок.
    
    Поскольку для описания типов компонентов `ArtistQuestionScreen` и
    `GenreQuestionScreen` мы воспользовались типом `PropsWithChildren`,
    то ошибки при запуске проекта мы не получим. Тип `PropsWithChildren`
    описывает пропс `children` как опциональный (Optional Properties).

--- project/src/components/artist-question-screen/artist-question-screen.tsx
+++ project/src/components/artist-question-screen/artist-question-screen.tsx
@@ -1,15 +1,15 @@
-import {ChangeEvent} from 'react';
+import {ChangeEvent, PropsWithChildren} from 'react';
 import Logo from '../logo/logo';
 import {QuestionArtist, UserArtistQuestionAnswer} from '../../types/question';
 
-type ArtistQuestionScreenProps = {
+type ArtistQuestionScreenProps = PropsWithChildren<{
   question: QuestionArtist,
   onAnswer: (question: QuestionArtist, answer: UserArtistQuestionAnswer) => void;
   renderPlayer: (src: string, playerIndex: number) => JSX.Element;
-};
+}>;
 
 function ArtistQuestionScreen(props: ArtistQuestionScreenProps): JSX.Element {
-  const {question, onAnswer, renderPlayer} = props;
+  const {question, onAnswer, renderPlayer, children} = props;
   const {answers, song} = question;
 
   return (
@@ -23,11 +23,7 @@ function ArtistQuestionScreen(props: ArtistQuestionScreenProps): JSX.Element {
           />
         </svg>
 
-        <div className="game__mistakes">
-          <div className="wrong" />
-          <div className="wrong" />
-          <div className="wrong" />
-        </div>
+        {children}
       </header>
 
       <section className="game__screen">
--- project/src/components/genre-question-screen/genre-question-screen.tsx
+++ project/src/components/genre-question-screen/genre-question-screen.tsx
@@ -1,15 +1,15 @@
-import {useState, FormEvent, ChangeEvent} from 'react';
+import {useState, FormEvent, ChangeEvent, PropsWithChildren} from 'react';
 import Logo from '../logo/logo';
 import {QuestionGenre, UserGenreQuestionAnswer} from '../../types/question';
 
-type GenreQuestionScreenProps = {
+type GenreQuestionScreenProps = PropsWithChildren<{
   question: QuestionGenre;
   onAnswer: (question: QuestionGenre, answers: UserGenreQuestionAnswer) => void;
   renderPlayer: (src: string, playerIndex: number) => JSX.Element;
-};
+}>;
 
 function GenreQuestionScreen(props: GenreQuestionScreenProps): JSX.Element {
-  const {question, onAnswer, renderPlayer} = props;
+  const {question, onAnswer, renderPlayer, children} = props;
   const {answers, genre} = question;
 
   const [userAnswers, setUserAnswers] = useState([false, false, false, false]);
@@ -25,11 +25,7 @@ function GenreQuestionScreen(props: GenreQuestionScreenProps): JSX.Element {
           />
         </svg>
 
-        <div className="game__mistakes">
-          <div className="wrong"/>
-          <div className="wrong"/>
-          <div className="wrong"/>
-        </div>
+        {children}
       </header>
 
       <section className="game__screen">
