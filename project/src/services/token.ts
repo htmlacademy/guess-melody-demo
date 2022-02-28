fef4db7732e156603573c85068958f556aaa3c00
kam4atka <sledsib@ya.ru>
2022-02-28 13:26:55 +0300

    7.3. Добавит модуль для работы с токеном.
    Для обращения к некоторым ресурсам сервера нам потребуется
    авторизационный токен. Чтобы его получить мы будем обращаться к
    определённому ресурсу.
    
    Токен — это по факту строка и её нужно где-то хранить. Хранить, чтобы
    была возможность добавлять её к очередному запросу к серверу.
    Мы можем завести для этого отдельную переменную, но этот вариант не
    очень. Если обновить страницу, то токен пропадёт и его придётся
    запрашивать заново. Поэтому нужно подумать о более надёжном хранилище.
    
    Один вариантов решения этой задачи — воспользоваться `localStorage`.
    После получения сохраним токен в хранилище, а когда потребуется им
    воспользоваться — извлечём. Для взаимодействия с `localStorage` создадим
    отдельный модуль и опишем несколько функций. В будущем это упростит
    нам написание автоматизированных тестов.

---
+++ project/src/services/token.ts
@@ -0,0 +1,16 @@
+const AUTH_TOKEN_KEY_NAME = 'guess-melody-token';
+
+export type Token = string;
+
+export const getToken = (): Token => {
+  const token = localStorage.getItem(AUTH_TOKEN_KEY_NAME);
+  return token ?? '';
+};
+
+export const saveToken = (token: Token): void => {
+  localStorage.setItem(AUTH_TOKEN_KEY_NAME, token);
+};
+
+export const dropToken = (): void => {
+  localStorage.removeItem(AUTH_TOKEN_KEY_NAME);
+};
