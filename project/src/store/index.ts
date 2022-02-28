318fd30e6257f37b116d90b5085f08d6286bcd6a
kam4atka <sledsib@ya.ru>
2022-02-28 18:24:05 +0300

    7.21. Добавит middleware и добавит новое действие.
    Мы сконфигурировали историю и теперь надо понять как будем выполнять
    перенаправление. Самый простой способ: подключить наш объект в нужное
    место и при необходимости вызывать метод `push`.
    
    Способ рабочий, но не очень удобен. Было бы здорово завести для этого
    отдельное действие и использовать точно также как и другие действия.
    
    Чтобы добиться такого поведения, создадим новую middleware, которую
    назовём `redirect`, а затем подключим её в список middleware при
    конфигурировании хранилища.
    
    После этого добавим новое действие — `redirectToRoute` и сразу опишем
    для него функцию (в соответствии с паттерном «action creator»). Напомним,
    при выполнении этого действия, будет выполняться перенаправление
    пользователя на нужный маршрут.
    
    После описания действия, обновим асинхронное действие `loginAction`.
    В случае успешной аутентификации перенаправим пользователя на маршрут
    `/result`.

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -1,6 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {reducer} from './reducer';
 import {createAPI} from '../services/api';
+import {redirect} from './middlewares/redirect';
 
 export const api = createAPI();
 
@@ -11,5 +12,5 @@ export const store = configureStore({
       thunk: {
         extraArgument: api,
       },
-    }),
+    }).concat(redirect),
 });
---
+++ project/src/store/middlewares/redirect.ts
@@ -0,0 +1,17 @@
+import {PayloadAction} from '@reduxjs/toolkit';
+import browserHistory from '../../browser-history';
+import {Middleware} from 'redux';
+import {reducer} from '../reducer';
+
+type Reducer = ReturnType<typeof reducer>;
+
+export const redirect: Middleware<unknown, Reducer> =
+  (_store) =>
+    (next) =>
+      (action: PayloadAction<string>) => {
+        if (action.type === 'game/redirectToRoute') {
+          browserHistory.push(action.payload);
+        }
+
+        return next(action);
+      };
