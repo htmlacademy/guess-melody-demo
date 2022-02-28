d54801d99f51b1201ceaf49f34cf7c9463d6d61c
kam4atka <sledsib@ya.ru>
2022-02-28 16:32:43 +0300

    7.10. Подключит redux-thunk и добавит загрузку вопросов.
    Начнём с конфигурирования хранилища. Подключим `redux-thunk` в список
    middlewares. Аргументом для `thunk` передадим сконфигурированный
    экземпляр `axios`, чтобы была возможность обратиться к нему из действия.
    
    После конфигурирования хранилища вызовем действие для загрузки списка
    игр с удалённого сервера.
    
    В объект начального состояния добавим новое поле `isDataLoaded`.
    Значением по умолчанию станет `false`. Менять значение будем после
    выполнения загрузки списка вопросов с сервера.
    
    Добавим новый компонент «Спиннер». Сделаем его в виде обычного текста
    «Loading…».

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -4,6 +4,10 @@ import {Provider} from 'react-redux';
 import App from './components/app/app';
 import ErrorMessage from './components/error-message/errorMessage';
 import {store} from './store';
+import {fetchQuestionAction, checkAuthAction} from './store/api-actions';
+
+store.dispatch(fetchQuestionAction());
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
