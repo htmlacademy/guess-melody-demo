cb340dabed95120013ca9981a78b3679a5a7063c
kam4atka <sledsib@ya.ru>
2022-02-28 13:27:53 +0300

    7.4. Добавим поддержку токенов.
    Мы определись с местом хранения токена, теперь нужно обновить модуль,
    где происходит конфигурирование axios. Добавим извлечение токена из
    `localStorage` и вставим его в заголовки. Для этого нам потребуется
    воспользоваться перехватчиками, а именно перехватчиком запроса.
    
    Настраиваем перехватчик, который будет вызывать формирования запроса,
    но до его отправки серверу. Здесь мы сможем модифицировать
    конфигурацию axios.

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,4 +1,5 @@
-import axios, {AxiosInstance} from 'axios';
+import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
+import {getToken} from './token';
 
 const BACKEND_URL = 'https://13.design.pages.academy/guess-melody';
 const REQUEST_TIMEOUT = 5000;
@@ -8,6 +9,18 @@ export const createAPI = (): AxiosInstance => {
     baseURL: BACKEND_URL,
     timeout: REQUEST_TIMEOUT,
   });
+
+  api.interceptors.request.use(
+    (config: AxiosRequestConfig) => {
+      const token = getToken();
+
+      if (token && config.headers) {
+        config.headers['x-token'] = token;
+      }
+
+      return config;
+    },
+  );
 
   return api;
 };
