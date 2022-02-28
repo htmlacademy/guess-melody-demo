cf161b0bc9a257dac59caa8ebfe08ebafb4f600b
kam4atka <sledsib@ya.ru>
2022-02-28 18:24:05 +0300

    7.16. Добавит middleware и добавит новое действие.
    Мы сконфигурировали историю и теперь надо понять как будем выполнять
    перенаправление. Самый простой способ: подключить наш объект в нужное
    место и при необходимости вызывать метод `push`.
    
    Способ рабочий, но не очень удобен. Было бы здорово завести для этого
    отдельное действие и использовать точно также как и другие действия.
    
    Чтобы добиться такого поведения, создадим новую middleware, которую
    назовём `redirect`, а затем подключим её в список middleware при
    конфигурировании хранилища.
    
    После этого добавим новое действие — `redirectToRoute` и сразу опишем
    для него функцию (в соответствии с паттерном «action creator»). Напомним,
    при выполнении этого действия, будет выполняться перенаправление
    пользователя на нужный маршрут.
    
    После описания действия, обновим асинхронное действие `loginAction`.
    В случае успешной аутентификации перенаправим пользователя на маршрут
    `/result`.

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,6 +1,6 @@
 import {createAction} from '@reduxjs/toolkit';
 import {Question, Questions, UserAnswer} from '../types/question';
-import {AuthorizationStatus} from '../const';
+import {AppRoute, AuthorizationStatus} from '../const';
 
 export const incrementStep = createAction('game/incrementStep');
 
@@ -13,3 +13,5 @@ export const loadQuestions = createAction<Questions>('data/loadQuestions');
 export const requireAuthorization = createAction<AuthorizationStatus>('user/requireAuthorization');
 
 export const setError = createAction<string>('game/setError');
+
+export const redirectToRoute = createAction<AppRoute>('game/redirectToRoute');
