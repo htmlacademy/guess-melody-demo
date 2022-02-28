e49b0076f2ee6ce430ea05437b3eaa9e5948405d
kam4atka <sledsib@ya.ru>
2022-02-28 17:33:59 +0300

    7.14. Добавит компонент-обертку HistoryRouter над Router.

---
+++ project/src/components/history-route/history-route.tsx
@@ -0,0 +1,35 @@
+import {useState, useLayoutEffect} from 'react';
+import {Router} from 'react-router-dom';
+import type {BrowserHistory} from 'history';
+
+export interface HistoryRouterProps {
+  history: BrowserHistory
+  basename?: string
+  children?: React.ReactNode
+}
+
+function HistoryRouter({
+  basename,
+  children,
+  history,
+}: HistoryRouterProps) {
+  const [state, setState] = useState({
+    action: history.action,
+    location: history.location,
+  });
+
+  useLayoutEffect(() => history.listen(setState), [history]);
+
+  return (
+    <Router
+      basename={basename}
+      location={state.location}
+      navigationType={state.action}
+      navigator={history}
+    >
+      {children}
+    </Router>
+  );
+}
+
+export default HistoryRouter;
