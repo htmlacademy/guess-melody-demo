442d322291201c657c2b309dff9396fac03c375d
kam4atka <sledsib@ya.ru>
2022-02-28 13:50:24 +0300

    7.5. Добавит новое действие загрузки вопросов.
    Опишем в новое действие для загрузки вопросов (`loadQuestions`)
    и обновим функцию-редьюсер.

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,8 +1,10 @@
 import {createAction} from '@reduxjs/toolkit';
-import {Question, UserAnswer} from '../types/question';
+import {Question, Questions, UserAnswer} from '../types/question';
 
 export const incrementStep = createAction('game/incrementStep');
 
 export const checkUserAnswer = createAction<{question: Question; userAnswer: UserAnswer}>('game/checkUserAnswer');
 
 export const resetGame = createAction('game/reset');
+
+export const loadQuestions = createAction<Questions>('data/loadQuestions');
