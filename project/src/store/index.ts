8095b35b720434b32e2cf4afc8299bdf7533ca5a
kam4atka <sledsib@ya.ru>
2022-02-28 16:23:26 +0300

    7.8. Добавит асинхронные действия.
    Создадим отдельный модуль в котором опишем асинхронные действия. В этих
    действиях будем выполнять запросы к серверу.
    
    На данном этапе нам потребуются следующие действия: для загрузки списка
    вопросов, проверки наличия авторизации и отправки данных для прохождения
    аутентификации, отправки запроса на выход из приложения.

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -1,4 +1,7 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {reducer} from './reducer';
+import {createAPI} from '../services/api';
+
+export const api = createAPI();
 
 export const store = configureStore({reducer});
