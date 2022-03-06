df8560bb32b3afbb168618ff88ea5ac9ab6140e1
kam4atka <sledsib@ya.ru>
2022-03-06 14:17:58 +0300

    7.14. Добавит обработку ошибок взаимодействия с сервером
    С помощью интерсептора мы добавляем обработку ответов от сервера.
    В случае, если мы получаем соответствующую ошибку, мы вызываем функцию
    `processErrorHandle` и передаем ей сообщение ошибки. Таким образом,
    мы связали все элементы системы обработки ошибок ответа от сервера.

--- src/index.tsx
+++ src/index.tsx
@@ -4,6 +4,9 @@ import {Provider} from 'react-redux';
 import App from './components/app/app';
 import ErrorMessage from './components/error-message/error-message';
 import {store} from './store';
+import {checkAuthAction} from './store/api-actions';
+
+store.dispatch(checkAuthAction());
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
