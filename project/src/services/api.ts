24269f2ac5a36bc858d8afcede46e09aadbb7186
kam4atka <sledsib@ya.ru>
2022-03-06 14:17:58 +0300

    7.14. Добавит обработку ошибок взаимодействия с сервером
    С помощью интерсептора мы добавляем обработку ответов от сервера.
    В случае, если мы получаем соответствующую ошибку, мы вызываем функцию
    `processErrorHandle` и передаем ей сообщение ошибки. Таким образом,
    мы связали все элементы системы обработки ошибок ответа от сервера.

--- project/src/services/api.ts
+++ project/src/services/api.ts
@@ -1,5 +1,15 @@
-import axios, {AxiosInstance, AxiosRequestConfig} from 'axios';
+import axios, {AxiosInstance, AxiosRequestConfig, AxiosResponse, AxiosError} from 'axios';
+import {StatusCodes} from 'http-status-codes';
 import {getToken} from './token';
+import {processErrorHandle} from './process-error-handle';
+
+const StatusCodeMapping: Record<number, boolean> = {
+  [StatusCodes.BAD_REQUEST]: true,
+  [StatusCodes.UNAUTHORIZED]: true,
+  [StatusCodes.NOT_FOUND]: true
+};
+
+const shouldDisplayError = (response: AxiosResponse) => !!StatusCodeMapping[response.status];
 
 const BACKEND_URL = 'https://10.react.pages.academy/guess-melody';
 const REQUEST_TIMEOUT = 5000;
@@ -20,6 +30,17 @@ export const createAPI = (): AxiosInstance => {
 
       return config;
     },
+  );
+
+  api.interceptors.response.use(
+    (response) => response,
+    (error: AxiosError) => {
+      if (error.response && shouldDisplayError(error.response)) {
+        processErrorHandle(error.response.data.error);
+      }
+
+      throw error;
+    }
   );
 
   return api;
