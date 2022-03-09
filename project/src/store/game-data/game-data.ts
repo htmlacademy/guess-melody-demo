e6b09609b7a9a59b3cf5a91e7acacb9230e6f58c
Igor Antonov <antonov.igor.khv@gmail.com>
2022-03-09 19:01:32 +0300

    8.7. Создаём новые слайсы. Продолжаем разбивать редьюсер. WIP
    Проделаем аналогичную процедуру для остальной части редьюсера. Создадим
    отдельные слайсы: `GameData` и `GameProcess`.
    
    Исходный файл `reducer.ts` удалим. Больше он нам не потребуется. В
    файле `action.ts` оставим только действие `redirectToRouter`.
    
    WIP. Проект по-прежнему не запускается. Слайсы созданы, но не подключены.

---
+++ project/src/store/game-data/game-data.ts
@@ -0,0 +1,25 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {NameSpace} from '../../const';
+import {GameData} from '../../types/state';
+import {fetchQuestionAction} from '../api-actions';
+
+const initialState: GameData = {
+  questions: [],
+  isQuestionsDataLoading: false,
+};
+
+export const gameData = createSlice({
+  name: NameSpace.Data,
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(fetchQuestionAction.pending, (state) => {
+        state.isQuestionsDataLoading = true;
+      })
+      .addCase(fetchQuestionAction.fulfilled, (state, action) => {
+        state.questions = action.payload;
+        state.isQuestionsDataLoading = false;
+      });
+  }
+});
