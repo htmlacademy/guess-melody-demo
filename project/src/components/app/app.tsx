40eae6b2b125536d5957a1d85506798a9d356bc8
Igor Antonov <antonov.igor.khv@gmail.com>
2022-03-09 19:13:33 +0300

    8.9. Обновит компоненты
    До окончательного перехода на слайсы остаётся обновить компоненты:
    изменить путь к действиям и имя при извлечении данных из хранилища.

--- project/src/components/app/app.tsx
+++ project/src/components/app/app.tsx
@@ -12,9 +12,12 @@ import LoadingScreen from '../loading-screen/loading-screen';
 import HistoryRouter from '../history-route/history-route';
 import {isCheckedAuth} from '../../game';
 import browserHistory from '../../browser-history';
+import {getAuthorizationStatus} from '../../store/user-process/selectors';
+import {getLoadedDataStatus} from '../../store/game-data/selectors';
 
 function App(): JSX.Element {
-  const {authorizationStatus, isDataLoaded} = useAppSelector((state) => state);
+  const authorizationStatus = useAppSelector(getAuthorizationStatus);
+  const isDataLoaded = useAppSelector(getLoadedDataStatus);
 
   if (isCheckedAuth(authorizationStatus) || !isDataLoaded) {
     return (
