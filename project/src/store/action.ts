35b64feb65b5e49f5c0e15aea35ef20e394cf797
Igor Antonov <antonov.igor.khv@gmail.com>
2022-03-09 19:01:32 +0300

    8.7. Создаём новые слайсы. Продолжаем разбивать редьюсер. WIP
    Проделаем аналогичную процедуру для остальной части редьюсера. Создадим
    отдельные слайсы: `GameData` и `GameProcess`.
    
    Исходный файл `reducer.ts` удалим. Больше он нам не потребуется. В
    файле `action.ts` оставим только действие `redirectToRouter`.
    
    WIP. Проект по-прежнему не запускается. Слайсы созданы, но не подключены.

--- project/src/store/action.ts
+++ project/src/store/action.ts
@@ -1,15 +1,4 @@
 import {createAction} from '@reduxjs/toolkit';
-import {Question, Questions, UserAnswer} from '../types/question';
 import {AppRoute} from '../const';
-
-export const incrementStep = createAction('game/incrementStep');
-
-export const checkUserAnswer = createAction<{question: Question; userAnswer: UserAnswer}>('game/checkUserAnswer');
-
-export const resetGame = createAction('game/reset');
-
-export const loadQuestions = createAction<Questions>('data/loadQuestions');
-
-export const setQuestionsDataLoadingStatus = createAction<boolean>('data/setQuestionsDataLoadingStatus');
 
 export const redirectToRoute = createAction<AppRoute>('game/redirectToRoute');
