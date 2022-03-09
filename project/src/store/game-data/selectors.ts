22f763a2914f49dd2990e0df4032d587feb25469
Igor Antonov <antonov.igor.khv@gmail.com>
2022-03-09 19:05:16 +0300

    8.8. Объединит слайсы в один редьюсер. WIP
    Объединим слайсы в общий редьюсер. Для этого создадим файл
    `root-reducer.ts`. Затем подключим его при инициализации хранилища.
    
    Сделаем ещё один небольшой рефакторинг. Вместо прямого обращения к
    полям хранилища воспользуемся селекторами.
    
    Селектор — обычная функция, которая возвращает значение нужного
    поля хранилища.
    
    WIP.  Проект по-прежнему не работает. Мы подготовили всё необходимое,
    но не обновили компоненты.

---
+++ project/src/store/game-data/selectors.ts
@@ -0,0 +1,6 @@
+import {NameSpace} from '../../const';
+import {State} from '../../types/state';
+import {Questions} from '../../types/question';
+
+export const getQuestions = (state: State): Questions => state[NameSpace.Data].questions;
+export const getQuestionsDataLoadingStatus = (state: State): boolean => state[NameSpace.Data].isQuestionsDataLoading;
---
+++ project/src/store/user-process/selectors.ts
@@ -0,0 +1,6 @@
+import {NameSpace} from '../../const';
+import {State} from '../../types/state';
+import {AuthorizationStatus} from '../../const';
+
+export const getAuthorizationStatus = (state: State): AuthorizationStatus => state[NameSpace.User].authorizationStatus;
+export const getAuthCheckedStatus = (state: State): boolean => state[NameSpace.User].authorizationStatus !== AuthorizationStatus.Unknown;
