6d687ad19e73b01fe5c18a860663a2a7bc6fb227
Igor Antonov <antonov.igor.khv@gmail.com>
2022-03-09 18:53:36 +0300

    8.6. Добавляем первый слайс userProcess. WIP
    Наш редьюсер стал большим. В нём обрабатываются абсолютно все действия.
    Для оптимизации мы можем разбить редьюсер на отдельные части. Для этого
    воспользуемся функцией `createSlice` из пакета `RTK`. Вынесем в
    отдельный слайс всё, что необходимо для поддержки доменной области `user`.
    
    После создания слайса, нет необходимости держать `action creator` для
    определённого действия. Его можно получить через слайс.
    
    WIP: добавили слайс, но пока не подключили. Проект не запускается.

---
+++ project/src/store/user-process/user-process.ts
@@ -0,0 +1,32 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {NameSpace, AuthorizationStatus} from '../../const';
+import {UserProcess} from '../../types/state';
+import {checkAuthAction, loginAction, logoutAction} from '../api-actions';
+
+const initialState: UserProcess = {
+  authorizationStatus: AuthorizationStatus.Unknown,
+};
+
+export const userProcess = createSlice({
+  name: NameSpace.User,
+  initialState,
+  reducers: {},
+  extraReducers(builder) {
+    builder
+      .addCase(checkAuthAction.fulfilled, (state) => {
+        state.authorizationStatus = AuthorizationStatus.Auth;
+      })
+      .addCase(checkAuthAction.rejected, (state) => {
+        state.authorizationStatus = AuthorizationStatus.NoAuth;
+      })
+      .addCase(loginAction.fulfilled, (state) => {
+        state.authorizationStatus = AuthorizationStatus.Auth;
+      })
+      .addCase(loginAction.rejected, (state) => {
+        state.authorizationStatus = AuthorizationStatus.NoAuth;
+      })
+      .addCase(logoutAction.fulfilled, (state, action) => {
+        state.authorizationStatus = AuthorizationStatus.NoAuth;
+      });
+  }
+});
