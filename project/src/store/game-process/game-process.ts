d73844a2f188869d66905d2e2988425c14b51868
Igor Antonov <antonov.igor.khv@gmail.com>
2022-03-09 19:01:32 +0300

    8.7. Создаём новые слайсы. Продолжаем разбивать редьюсер. WIP
    Проделаем аналогичную процедуру для остальной части редьюсера. Создадим
    отдельные слайсы: `GameData` и `GameProcess`.
    
    Исходный файл `reducer.ts` удалим. Больше он нам не потребуется. В
    файле `action.ts` оставим только действие `redirectToRouter`.
    
    WIP. Проект по-прежнему не запускается. Слайсы созданы, но не подключены.

---
+++ project/src/store/game-process/game-process.ts
@@ -0,0 +1,32 @@
+import {createSlice} from '@reduxjs/toolkit';
+import {isAnswerCorrect} from '../../game';
+import {NameSpace, FIRST_GAME_STEP} from '../../const';
+import {GameProcess} from '../../types/state';
+
+const initialState: GameProcess = {
+  mistakes: 0,
+  step: FIRST_GAME_STEP,
+};
+
+const STEP_COUNT = 1;
+
+export const gameProcess = createSlice({
+  name: NameSpace.Game,
+  initialState,
+  reducers: {
+    incrementStep: (state) => {
+      state.step = state.step + STEP_COUNT;
+    },
+    checkUserAnswer: (state, action) => {
+      const {question, userAnswer} = action.payload;
+
+      state.mistakes += Number(!isAnswerCorrect(question, userAnswer));
+    },
+    resetGame: (state) => {
+      state.mistakes = 0;
+      state.step = FIRST_GAME_STEP;
+    },
+  },
+});
+
+export const {incrementStep, checkUserAnswer, resetGame} = gameProcess.actions;
