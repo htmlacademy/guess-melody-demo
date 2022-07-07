3f586ff507095357b6c8263cdb8cd25d7b8436ee
Igor Antonov <antonov.igor.khv@gmail.com>
2022-07-07 12:28:49 +0300

    2.1. Добавит React-компоненты.
    Создадим первый компонент для экрана приветствия. Это компонент «Страницы», поэтому разместим его в директории `pages`. Для каждого компонента создадим отдельную директорию.
    
    Затем изменим второй компонент — `App`. Он будет отрисовывать компонент
    `WelcomeScreen`.
    
    Последним шагом отредактируем файл `index.tsx`. Передадим компоненту
    `App` его первый пропс.

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,12 +2,18 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './components/app/app';
 
+const Setting = {
+  ErrorsCount: 3,
+} as const;
+
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement,
 );
 
 root.render(
   <React.StrictMode>
-    <App />
+    <App
+      errorsCount = {Setting.ErrorsCount}
+    />
   </React.StrictMode>,
 );
