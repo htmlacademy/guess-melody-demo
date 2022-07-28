514e5a2928af4f235cf5d66421a0812a582118c8
kam4atka <sledsib@ya.ru>
2022-07-28 13:15:15 +0300

    7.12. - Создаст компонент для отображения ошибки и подключит его.
    Для сохранения и удаления текста ошибки у нас все готово. Однако как же
    показать этот текст пользователю?
    
    Для этого создадим новый компонент `error-message.tsx` и подключим его
    к глобальному хранилищу. Сам компонент подключим в файле `index.tsx`

--- project/src/index.tsx
+++ project/src/index.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import {Provider} from 'react-redux';
 import App from './components/app/app';
+import ErrorMessage from './components/error-message/error-message';
 import {store} from './store';
 
 const root = ReactDOM.createRoot(
@@ -11,6 +12,7 @@ const root = ReactDOM.createRoot(
 root.render(
   <React.StrictMode>
     <Provider store = {store}>
+      <ErrorMessage />
       <App />
     </Provider>
   </React.StrictMode>,
