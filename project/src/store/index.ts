683ea75f56077c0312d5d26554e3bbd15034a9de
kam4atka <sledsib@ya.ru>
2022-07-28 12:45:05 +0300

    7.2. - Подключим `redux-thunk`.
    Cконфигурируем хранилище. Подключим `redux-thunk` в список middlewares.
    Аргументом для `thunk` передадим сконфигурированный экземпляр `axios`,
    чтобы была возможность обратиться к нему из действия.

--- project/src/store/index.ts
+++ project/src/store/index.ts
@@ -1,4 +1,15 @@
 import {configureStore} from '@reduxjs/toolkit';
 import {reducer} from './reducer';
+import {createAPI} from '../services/api';
 
-export const store = configureStore({reducer});
+export const api = createAPI();
+
+export const store = configureStore({
+  reducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      thunk: {
+        extraArgument: api,
+      },
+    }),
+});
