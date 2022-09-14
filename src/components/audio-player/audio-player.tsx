dd45251b321985d65ed18b1fbcbff19785c725bd
Lydia Zakharova <jahontis@gmail.com>
2022-09-14 11:40:31 +0300

    5.2. Добавит classnames.
    Воспользуемся пакетом `classnames`, чтобы изменять классы элемента
    в зависимости от состояния.

--- src/components/audio-player/audio-player.tsx
+++ src/components/audio-player/audio-player.tsx
@@ -1,4 +1,5 @@
 import {Fragment, useState, useEffect, useRef} from 'react';
+import cn from 'classnames';
 
 type AudioPlayerProps = {
   autoPlay: boolean;
@@ -47,7 +48,11 @@ function AudioPlayer({autoPlay, src}: AudioPlayerProps): JSX.Element {
   return (
     <Fragment>
       <button
-        className="track__button track__button--play"
+        className={cn(
+          'track__button',
+          {'track__button--play': !isPlaying},
+          {'track__button--pause': isPlaying}
+        )}
         type="button"
         disabled={!isLoaded}
         onClick={() => setIsPlaying(!isPlaying)}
