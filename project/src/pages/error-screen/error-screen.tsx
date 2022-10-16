c9835c0af4d25df2694aeac96531a53a606e4333
Lydia Zakharova <jahontis@gmail.com>
2022-10-16 13:06:07 +0300

    8.11. Добавит компонент ErrorScreen.
    Создадим новый компонент «Ошибка запроса» с кнопкой, кликнув на которую,
    пользователь сможет отправить повторный запрос на сервер.

---
+++ project/src/pages/error-screen/error-screen.tsx
@@ -0,0 +1,23 @@
+import {useAppDispatch} from '../../hooks';
+import {fetchQuestionAction} from '../../store/api-actions';
+
+function ErrorScreen(): JSX.Element {
+  const dispatch = useAppDispatch();
+
+  return (
+    <>
+      <p className="error__text">Не удалось загрузить вопросы</p>
+      <button
+        onClick={() => {
+          dispatch(fetchQuestionAction());
+        }}
+        className="replay replay--error"
+        type="button"
+      >
+        Попробовать ещё раз
+      </button>
+    </>
+  );
+}
+
+export default ErrorScreen;
