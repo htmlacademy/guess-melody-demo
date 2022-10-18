ArtyomNelyub/guess-melody-9

9.28. Добавит тест для HOC withAudioPlayer.
Тестирование HOC мало, чем отличается от тестирования обычного
компонента. Достаточно обернуть компонент в HOC и убедиться, что он
рендерится верно.

Однако, в нашем случае есть нюанс. HOC `withAudioPlayer` подключает
компонент `AudioPlayer`, который в свою очередь использует объект
`audio`. Чтобы не мокать элемент `audio`, мы замокаем компонент
`AudioPlayer`.

В первом тест-кейсе убедимся, что применение HOC ничего не ломает
и базовый компонент по-прежнему корректно рендерится.

Во втором тестовом случае попробуем поэкспериментировать на компоненте
`ArtistQuestionScreen`. Подготовим для него все необходимые пропсы.
Затем проверим корректность рендеринга.

project/src/hocs/with-audio-player/with-audio-player.test.tsx
@@ -1,0 +1,47 @@
+import {render, screen} from '@testing-library/react';
+import {createMemoryHistory} from 'history';
+import HistoryRouter from '../../components/history-route/history-route';
+import withAudioPlayer from './with-audio-player';
+import ArtistQuestionScreen from '../../components/artist-question-screen/artist-question-screen';
+import {makeFakeArtistQuestion} from '../../utils/mocks';
+
+jest.mock('../../components/audio-player/audio-player', () => {
+  const mockAudioPlayer = () => <>This is mock AudioPlayer</>;
+
+  return {
+    __esModule: true,
+    default: mockAudioPlayer,
+  };
+});
+
+describe('HOC: withAudioPlayer', () => {
+  it('base component should correct rendering when use with HOC', () => {
+    const BaseComponentWrapped = withAudioPlayer(() => <h1>withAudioPlayer</h1>);
+
+    render(<BaseComponentWrapped />);
+
+    expect(screen.getByText(/withAudioPlayer/i)).toBeInTheDocument();
+  });
+
+  it('base component should correct rendering another component with render-prop', () => {
+    const mockQuestion = makeFakeArtistQuestion();
+    const history = createMemoryHistory();
+
+    const BaseComponentWrapped = withAudioPlayer(ArtistQuestionScreen);
+
+    render(
+      <HistoryRouter history={history}>
+        <BaseComponentWrapped
+          onAnswer={jest.fn()}
+          question={mockQuestion}
+        >
+          <p>This is children component</p>
+        </BaseComponentWrapped>
+      </HistoryRouter>,
+    );
+
+    expect(screen.getByText(/This is children component/i)).toBeInTheDocument();
+    expect(screen.getByText(/This is mock AudioPlayer/i)).toBeInTheDocument();
+    expect(screen.getByText(/Кто исполняет эту песню/i)).toBeInTheDocument();
+  });
+});

htmlacademy/guess-melody-demo

project/src/hocs/with-audio-player/with-audio-player.test.tsx
@@ -1,0 +1,50 @@
+import {render, screen} from '@testing-library/react';
+import {HelmetProvider} from 'react-helmet-async';
+import {createMemoryHistory} from 'history';
+import HistoryRouter from '../../components/history-route/history-route';
+import withAudioPlayer from './with-audio-player';
+import ArtistQuestionScreen from '../../pages/artist-question-screen/artist-question-screen';
+import {makeFakeArtistQuestion} from '../../utils/mocks';
+
+jest.mock('../../components/audio-player/audio-player', () => {
+  const mockAudioPlayer = () => <>This is mock AudioPlayer</>;
+
+  return {
+    __esModule: true,
+    default: mockAudioPlayer,
+  };
+});
+
+describe('HOC: withAudioPlayer', () => {
+  it('base component should correct rendering when use with HOC', () => {
+    const BaseComponentWrapped = withAudioPlayer(() => <h1>withAudioPlayer</h1>);
+
+    render(<BaseComponentWrapped />);
+
+    expect(screen.getByText(/withAudioPlayer/i)).toBeInTheDocument();
+  });
+
+  it('base component should correct rendering another component with render-prop', () => {
+    const mockQuestion = makeFakeArtistQuestion();
+    const history = createMemoryHistory();
+
+    const BaseComponentWrapped = withAudioPlayer(ArtistQuestionScreen);
+
+    render(
+      <HistoryRouter history={history}>
+        <HelmetProvider>
+          <BaseComponentWrapped
+            onAnswer={jest.fn()}
+            question={mockQuestion}
+          >
+            <p>This is children component</p>
+          </BaseComponentWrapped>
+        </HelmetProvider>
+      </HistoryRouter>,
+    );
+
+    expect(screen.getByText(/This is children component/i)).toBeInTheDocument();
+    expect(screen.getByText(/This is mock AudioPlayer/i)).toBeInTheDocument();
+    expect(screen.getByText(/Кто исполняет эту песню/i)).toBeInTheDocument();
+  });
+});