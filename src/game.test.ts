17d1c751a01bba7e5c52fa7f049e50c6718d7d24
Igor Antonov <antonov.igor.khv@gmail.com>
2023-08-03 17:53:21 +0300

    9.4. Добавит тесты для `isGenreAnswerCorrect`.
    Аналогичным образом протестируем `isGenreAnswerCorrect`.
    
    Специально для этого добавим функцию `makeFakeGenreQuestion`.

--- src/game.test.ts
+++ src/game.test.ts
@@ -1,28 +1,56 @@
-import { isArtistAnswerCorrect } from './game';
-import { makeFakeArtistQuestion } from './utils/mocks';
+import { isArtistAnswerCorrect, isGenreAnswerCorrect } from './game';
+import { makeFakeArtistQuestion, makeFakeGenreQuestion } from './utils/mocks';
 
-describe('Function: isArtistAnswerCorrect', () => {
-  it('should return "true" when answer is correct', () => {
-    // Arrange
-    const mockArtiestQuestion = makeFakeArtistQuestion();
-    const { artist: correctAnswer } = mockArtiestQuestion.song;
+describe('Business Logic: check user\'s answer', () => {
+  describe('Function: isArtistAnswerCorrect', () => {
+    it('should return "true" when answer is correct', () => {
+      // Arrange
+      const mockArtiestQuestion = makeFakeArtistQuestion();
+      const { artist: correctAnswer } = mockArtiestQuestion.song;
 
-    // Act
-    const result = isArtistAnswerCorrect(mockArtiestQuestion, correctAnswer);
+      // Act
+      const result = isArtistAnswerCorrect(mockArtiestQuestion, correctAnswer);
 
-    // Assert
-    expect(result).toBe(true);
+      // Assert
+      expect(result).toBe(true);
+    });
+
+    it('should return "false" when answer is incorrect', () => {
+      // Arrange
+      const mockArtiestQuestion = makeFakeArtistQuestion();
+      const incorrectAnswer = 'unknown';
+
+      // Act
+      const result = isArtistAnswerCorrect(mockArtiestQuestion, incorrectAnswer);
+
+      // Assert
+      expect(result).toBe(false);
+    });
   });
 
-  it('should return "false" when answer is incorrect', () => {
-    // Arrange
-    const mockArtiestQuestion = makeFakeArtistQuestion();
-    const incorrectAnswer = 'unknown';
+  describe('Function: isGenreAnswerCorrect', () => {
+    it('should return "true" when answer is correct', () => {
+      // Arrange
+      const mockGenreQuestion = makeFakeGenreQuestion();
+      const { answers } = mockGenreQuestion;
+      const correctAnswer = answers.map((answer) => answer.genre === mockGenreQuestion.genre);
 
-    // Act
-    const result = isArtistAnswerCorrect(mockArtiestQuestion, incorrectAnswer);
+      // Act
+      const result = isGenreAnswerCorrect(mockGenreQuestion, correctAnswer);
 
-    // Assert
-    expect(result).toBe(false);
+      // Assert
+      expect(result).toBe(true);
+    });
+
+    it('should be return "false" when answer is incorrect', () => {
+      const mockGenreQuestion = makeFakeGenreQuestion();
+      const { answers } = mockGenreQuestion;
+      const incorrectAnswer = answers.map((answer) => answer.genre !== mockGenreQuestion.genre);
+
+      const result = isGenreAnswerCorrect(mockGenreQuestion, incorrectAnswer);
+
+      expect(result).toBe(false);
+    });
   });
 });
+
