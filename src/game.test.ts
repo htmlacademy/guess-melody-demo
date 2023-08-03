eb4058b43c91a29fcc2d4ebb1d12bf7c832ef737
Igor Antonov <antonov.igor.khv@gmail.com>
2023-08-03 17:40:07 +0300

    9.3. Добавит описание негативного тестового кейса.
    В прошлом шаге мы проверили функцию на успех. Если ответ верный, функция должна вернуть `true`. Проверим обратную ситуацию.
    Если функции передать неправильный ответ, то результатом выполнения
    станет `false`.

--- src/game.test.ts
+++ src/game.test.ts
@@ -13,4 +13,16 @@ describe('Function: isArtistAnswerCorrect', () => {
     // Assert
     expect(result).toBe(true);
   });
+
+  it('should return "false" when answer is incorrect', () => {
+    // Arrange
+    const mockArtiestQuestion = makeFakeArtistQuestion();
+    const incorrectAnswer = 'unknown';
+
+    // Act
+    const result = isArtistAnswerCorrect(mockArtiestQuestion, incorrectAnswer);
+
+    // Assert
+    expect(result).toBe(false);
+  });
 });
