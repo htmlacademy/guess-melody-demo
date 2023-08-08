00a86946426ea129c292700620bb074dfe09e5d5
Igor Antonov <antonov.igor.khv@gmail.com>
2023-08-08 16:00:59 +0300

    9.6. Добавит тесты для селекторов `GameData`.

--- src/store/game-data/selectors.ts
+++ src/store/game-data/selectors.ts
@@ -2,6 +2,6 @@ import {NameSpace} from '../../const';
 import {State} from '../../types/state';
 import {Questions} from '../../types/question';
 
-export const getQuestions = (state: State): Questions => state[NameSpace.Data].questions;
-export const getQuestionsDataLoadingStatus = (state: State): boolean => state[NameSpace.Data].isQuestionsDataLoading;
-export const getErrorStatus = (state: State): boolean => state[NameSpace.Data].hasError;
+export const getQuestions = (state: Pick<State, NameSpace.Data>): Questions => state[NameSpace.Data].questions;
+export const getQuestionsDataLoadingStatus = (state: Pick<State, NameSpace.Data>): boolean => state[NameSpace.Data].isQuestionsDataLoading;
+export const getErrorStatus = (state: Pick<State, NameSpace.Data>): boolean => state[NameSpace.Data].hasError;
