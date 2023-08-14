128fd1e376252cf3617ba9f1c2e22246d69062d3
Igor Antonov <antonov.igor.khv@gmail.com>
2023-08-14 19:03:39 +0300

    9.13. Тестируем компоненты с React Router.
    Некоторые компоненты нельзя отрисовать, если не предоставить им все необходимые зависимости. Один из таких случаев: компоненты, которые используют React Router, а точней говоря его компоненты.
    
    Например, в компоненте `Logo` используется компонент `Link` из пакета `react-router-dom`.
    
    Мы не можем использовать такие компоненты вне React Router. Чтобы решить эту проблему, создадим функцию `withHistory`, которая будет добавлять обёртку из необходимых компонентов.

---
+++ src/utils/mock-component.tsx
@@ -0,0 +1,15 @@
+import { MemoryHistory, createMemoryHistory } from 'history';
+import HistoryRouter from '../components/history-route/history-route';
+import { HelmetProvider } from 'react-helmet-async';
+
+export function withHistory(component: JSX.Element, history?: MemoryHistory) {
+  const memoryHistory = history ?? createMemoryHistory();
+
+  return (
+    <HistoryRouter history={memoryHistory}>
+      <HelmetProvider>
+        {component}
+      </HelmetProvider>
+    </HistoryRouter>
+  );
+}
