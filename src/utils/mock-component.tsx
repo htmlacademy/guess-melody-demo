0380220f7a745040c886e4936571dc1e23b8dace
Igor Antonov <antonov.igor.khv@gmail.com>
2023-08-14 19:25:03 +0300

    9.14. Добавит функцию `withStore`.
    При тестировании компонентов, подключённых к Redux, нам также потребуется обёртка для компонента в виде `Provider`.
    
    Для подготовки этого поведения создадим отдельную функцию `withStore`.

--- src/utils/mock-component.tsx
+++ src/utils/mock-component.tsx
@@ -1,6 +1,14 @@
 import { MemoryHistory, createMemoryHistory } from 'history';
 import HistoryRouter from '../components/history-route/history-route';
 import { HelmetProvider } from 'react-helmet-async';
+import { MockStore, configureMockStore } from '@jedmao/redux-mock-store';
+import MockAdapter from 'axios-mock-adapter';
+import { State } from '../types/state';
+import { createAPI } from '../services/api';
+import thunk from 'redux-thunk';
+import { Action } from 'redux';
+import { AppThunkDispatch } from './mocks';
+import { Provider } from 'react-redux';
 
 export function withHistory(component: JSX.Element, history?: MemoryHistory) {
   const memoryHistory = history ?? createMemoryHistory();
@@ -12,4 +20,27 @@ export function withHistory(component: JSX.Element, history?: MemoryHistory) {
       </HelmetProvider>
     </HistoryRouter>
   );
+}
+
+type ComponentWithMockStore = {
+  withStoreComponent: JSX.Element;
+  mockStore: MockStore;
+  mockAxiosAdapter: MockAdapter;
+}
+
+export function withStore(
+  component: JSX.Element,
+  initialState: Partial<State> = {},
+): ComponentWithMockStore {
+  const axios = createAPI();
+  const mockAxiosAdapter = new MockAdapter(axios);
+  const middleware = [thunk.withExtraArgument(axios)];
+  const mockStoreCreator = configureMockStore<State, Action<string>, AppThunkDispatch>(middleware);
+  const mockStore = mockStoreCreator(initialState);
+
+  return ({
+    withStoreComponent: <Provider store={mockStore}>{component}</Provider>,
+    mockStore,
+    mockAxiosAdapter,
+  });
 }
